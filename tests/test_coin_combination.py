ce374788e54c9f69ce1e8d5bc29f13553ca4e7b5
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-02-03 15:20:21 +0200

    'Solution'

--- tests/test_coin_combination.py
+++ tests/test_coin_combination.py
@@ -1,3 +1,17 @@
 from app.coin_combination import get_coin_combination
 
-# write your tests here
+
+def test_zero_cents():
+    assert get_coin_combination(0) == [0, 0, 0, 0]
+
+
+def test_all_types_coins():
+    assert get_coin_combination(42) == [2, 1, 1, 1]
+
+
+def test_one_coin():
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+
+
+
+
