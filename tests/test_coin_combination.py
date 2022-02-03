ed4ea8114f422d12758a14df1d5929642de18216
Nazar-kob <kobrinnazar05@mail.com>
2022-02-03 15:39:32 +0200

    Solution

--- tests/test_coin_combination.py
+++ tests/test_coin_combination.py
@@ -1,3 +1,36 @@
+import pytest
+
 from app.coin_combination import get_coin_combination
 
-# write your tests here
+
+def test_get_penny():
+    assert get_coin_combination(4) == [4, 0, 0, 0]
+
+
+def test_get_nickel():
+    assert get_coin_combination(5) == [0, 1, 0, 0]
+
+
+def test_get_dime():
+    assert get_coin_combination(20) == [0, 0, 2, 0]
+
+
+def test_get_quarter():
+    assert get_coin_combination(100) == [0, 0, 0, 4]
+
+
+def test_when_coin_is_zero():
+    assert get_coin_combination(0) == [0, 0, 0, 0]
+
+
+@pytest.mark.parametrize(
+    'coin, expected',
+    [
+        (17, [2, 1, 1, 0]),
+        (43, [3, 1, 1, 1]),
+        (67, [2, 1, 1, 2]),
+        (49, [4, 0, 2, 1])
+    ]
+)
+def test_when_get_mix_or_all_coins(coin, expected):
+    assert get_coin_combination(coin) == expected
