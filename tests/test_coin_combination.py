bb19b9aaac6d15130f985ff9d47d02a1aea7ea02
Kalyta Valerii <deptovka@gmail.com>
2022-02-03 16:41:48 +0200

    'Solution'

--- tests/test_coin_combination.py
+++ tests/test_coin_combination.py
@@ -1,3 +1,18 @@
 from app.coin_combination import get_coin_combination
+import pytest
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "coins, values",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
+        (99, [4, 0, 2, 3]),
+        (299, [4, 0, 2, 11]),
+
+    ],
+)
+def test_with_different_number_of_coins(coins, values):
+    assert get_coin_combination(coins) == values
