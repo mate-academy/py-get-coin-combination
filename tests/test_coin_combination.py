33dc04e015659615a4f03308e1b718bda5c2362e
nikon4uk <nikonchukmax4@gmail.com>
2022-02-03 19:39:54 +0200

    Solution

--- tests/test_coin_combination.py
+++ tests/test_coin_combination.py
@@ -1,3 +1,31 @@
 from app.coin_combination import get_coin_combination
 
-# write your tests here
+
+def test_nil_coins():
+    actual = get_coin_combination(0)
+    return actual == [0, 0, 0, 0]
+
+
+def test_pennies():
+    actual = get_coin_combination(3)
+    assert actual == [3, 0, 0, 0]
+
+
+def test_nickels():
+    actual = get_coin_combination(6)
+    assert actual == [1, 1, 0, 0]
+
+
+def test_dimes():
+    actual = get_coin_combination(17)
+    assert actual == [2, 1, 1, 0]
+
+
+def test_quarters():
+    actual = get_coin_combination(50)
+    assert actual == [0, 0, 0, 2]
+
+
+def test_all_combination():
+    actual = get_coin_combination(67)
+    assert actual == [2, 1, 1, 2]
