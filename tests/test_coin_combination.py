fefe518d6dd4ec20b5f14a5340b071c6b005cf1d
TJgkOsVP <sasha06052001@gmail.com>
2022-02-03 15:18:28 +0200

    'Solution'

--- tests/test_coin_combination.py
+++ tests/test_coin_combination.py
@@ -1,3 +1,29 @@
 from app.coin_combination import get_coin_combination
 
-# write your tests here
+
+def test_nothing_to_combine():
+    goals = get_coin_combination(0)
+    assert goals == [0, 0, 0, 0]
+
+
+def test_nickels_may_be_confused_with_pennies():
+    goals = get_coin_combination(65)
+    assert goals == [0, 1, 1, 2]
+
+
+def test_dimes_may_be_confused_with_nickles():
+    goals = get_coin_combination(47)
+    assert goals == [2, 0, 2, 1]
+
+
+def test_quarters_may_be_confused_with_dimes():
+    goals = get_coin_combination(80)
+    assert goals == [0, 1, 0, 3]
+
+
+def test_different_values():
+    goals = get_coin_combination(99)
+    assert goals == [4, 0, 2, 3]
+
+    goals = get_coin_combination(68)
+    assert goals == [3, 1, 1, 2]
