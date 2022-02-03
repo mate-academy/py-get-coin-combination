fc71417ff098586015dc81d8b1b3d2ea3241def4
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-02-03 19:08:32 +0200

    'Solution'

--- tests/test_coin_combination.py
+++ tests/test_coin_combination.py
@@ -1,3 +1,31 @@
 from app.coin_combination import get_coin_combination
 
-# write your tests here
+
+def test_quarter():
+    goals = get_coin_combination(25)
+
+    assert goals[3] == 1
+
+
+def test_dime():
+    goals = get_coin_combination(10)
+
+    assert goals[2] == 1
+
+
+def test_nickel():
+    goals = get_coin_combination(5)
+
+    assert goals[1] == 1
+
+
+def test_penny():
+    goals = get_coin_combination(1)
+
+    assert goals[0] == 1
+
+
+def test_big_value():
+    goals = get_coin_combination(46364)
+
+    assert goals == [4, 0, 1, 1854]
