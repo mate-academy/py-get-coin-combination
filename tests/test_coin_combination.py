b566185b525bedbcb5dfee3e5f9088590ecdcc61
Rybak Dmytro <codingclaud@gmail.com>
2022-02-03 15:16:38 +0200

    'Solution'

--- tests/test_coin_combination.py
+++ tests/test_coin_combination.py
@@ -1,3 +1,10 @@
 from app.coin_combination import get_coin_combination
 
-# write your tests here
+
+def test_check_right_count_of_coins():
+    assert get_coin_combination(17) == [2, 1, 1, 0]  # here we've checked pennies, nickels and dime
+    assert get_coin_combination(50) == [0, 0, 0, 2]  # here quarters were checked
+
+
+def test_check_zero_coins():
+    assert get_coin_combination(0) == [0, 0, 0, 0]  # 0 cents
