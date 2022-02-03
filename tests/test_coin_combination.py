61d15c964c9cb4085191b3d90bcdf3d1fe5cb033
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-02-03 16:19:56 +0200

    'Solution'

--- tests/test_coin_combination.py
+++ tests/test_coin_combination.py
@@ -1,3 +1,27 @@
 from app.coin_combination import get_coin_combination
 
-# write your tests here
+
+def test_zero_cents():
+    assert get_coin_combination(0) == [0, 0, 0, 0]
+
+
+def test_pennies():
+    assert get_coin_combination(2) == [2, 0, 0, 0]
+
+
+def test_nickels():
+    assert get_coin_combination(5) == [0, 1, 0, 0]
+
+
+def test_dimes():
+    assert get_coin_combination(20) == [0, 0, 2, 0]
+
+
+def test_quarters():
+    assert get_coin_combination(75) == [0, 0, 0, 3]
+
+
+def test_mixed_combination():
+    assert get_coin_combination(93) == [3, 1, 1, 3]
+
+
