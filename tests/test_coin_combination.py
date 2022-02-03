cc69b0d8130ce1bcd0ebeee6c3b3bfa171913b11
Denys Lipin <lipin.transstream@gmail.com>
2022-02-03 15:57:56 +0200

    'Solution'

--- tests/test_coin_combination.py
+++ tests/test_coin_combination.py
@@ -1,3 +1,17 @@
+import random
+
 from app.coin_combination import get_coin_combination
 
-# write your tests here
+
+def test_should_return_zeros_if_cents_equal_zero():
+    assert get_coin_combination(0) == [0, 0, 0, 0]
+
+
+def test_should_return_one_for_each_coin_in_list():
+    assert get_coin_combination(41) == [1, 1, 1, 1]
+
+
+def test_should_return_right_result_for_default_value():
+    cents = random.randint(42, 1000)
+    res = get_coin_combination(cents)
+    assert res[0] * 1 + res[1] * 5 + res[2] * 10 + res[3] * 25 == cents
