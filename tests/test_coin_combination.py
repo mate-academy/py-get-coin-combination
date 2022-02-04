0167a5e66318b6cd9ca32e21134b756a7a2b0949
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-02-04 20:47:27 +0200

    'Solution'

--- tests/test_coin_combination.py
+++ tests/test_coin_combination.py
@@ -5,23 +5,21 @@ def test_zero_cents():
     assert get_coin_combination(0) == [0, 0, 0, 0]
 
 
-def test_pennies():
+def test_correct_determine_pennies():
     assert get_coin_combination(2) == [2, 0, 0, 0]
 
 
-def test_nickels():
+def test_correct_determine_nickels():
     assert get_coin_combination(5) == [0, 1, 0, 0]
 
 
-def test_dimes():
+def test_correct_determine_dimes():
     assert get_coin_combination(20) == [0, 0, 2, 0]
 
 
-def test_quarters():
+def test_correct_determine_quarters():
     assert get_coin_combination(75) == [0, 0, 0, 3]
 
 
 def test_mixed_combination():
     assert get_coin_combination(93) == [3, 1, 1, 3]
-
-
