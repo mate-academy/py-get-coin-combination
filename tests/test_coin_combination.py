fe5674595b3586e782547240b922f52b216cf738
nikon4uk <nikonchukmax4@gmail.com>
2022-02-04 18:49:33 +0200

    was refactored

--- tests/test_coin_combination.py
+++ tests/test_coin_combination.py
@@ -2,30 +2,24 @@ from app.coin_combination import get_coin_combination
 
 
 def test_nil_coins():
-    actual = get_coin_combination(0)
-    return actual == [0, 0, 0, 0]
+    return get_coin_combination(0) == [0, 0, 0, 0]
 
 
 def test_pennies():
-    actual = get_coin_combination(3)
-    assert actual == [3, 0, 0, 0]
+    assert get_coin_combination(3) == [3, 0, 0, 0]
 
 
 def test_nickels():
-    actual = get_coin_combination(6)
-    assert actual == [1, 1, 0, 0]
+    assert get_coin_combination(6) == [1, 1, 0, 0]
 
 
 def test_dimes():
-    actual = get_coin_combination(17)
-    assert actual == [2, 1, 1, 0]
+    assert get_coin_combination(17) == [2, 1, 1, 0]
 
 
 def test_quarters():
-    actual = get_coin_combination(50)
-    assert actual == [0, 0, 0, 2]
+    assert get_coin_combination(50) == [0, 0, 0, 2]
 
 
 def test_all_combination():
-    actual = get_coin_combination(67)
-    assert actual == [2, 1, 1, 2]
+    assert get_coin_combination(67) == [2, 1, 1, 2]
