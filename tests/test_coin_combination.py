5b14e93e06e79497cfd8b2663fae37aa7800b4e6
Anita Posledova <anitaposliedova@gmail.com>
2022-02-07 18:57:23 +0200

    Solution

--- tests/test_coin_combination.py
+++ tests/test_coin_combination.py
@@ -5,11 +5,31 @@ import pytest
 @pytest.mark.parametrize(
     "input_amount_of_cents,result_coin_combination",
     [
-        (0, [0, 0, 0, 0]),
-        (4, [4, 0, 0, 0]),
-        (8, [3, 1, 0, 0]),
-        (17, [2, 1, 1, 0]),
-        (84, [4, 5, 0, 3]),
+        pytest.param(
+                    0,
+                    [0, 0, 0, 0],
+                    id="should return all zeroes when variable `cents` is equal to zero"
+        ),
+        pytest.param(
+                    4,
+                    [4, 0, 0, 0],
+                    id="should return four and zeroes when variable `cents` is equal to four"
+        ),
+        pytest.param(
+                    8,
+                    [3, 1, 0, 0],
+                    id="should return three, one, double zeroes when variable `cents` is equal to eight"
+        ),
+        pytest.param(
+                    17,
+                    [2, 1, 1, 0],
+                    id="should return two, double ones, zero when variable `cents` is equal to seventeen"
+        ),
+        pytest.param(
+                    84,
+                    [4, 1, 0, 3],
+                    id="should return four, five, zero, three when variable `cents` is equal to eighty-four"
+        ),
     ]
 )
 def test_is_result_correct(input_amount_of_cents, result_coin_combination):
