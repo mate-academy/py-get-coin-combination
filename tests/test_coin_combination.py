6ea19c26e6d82328073852a53078c1d2bf0eb0fd
Zikkolion <nmadaraosnova835@gmail.com>
2022-02-07 09:37:30 +0200

    'Solution'

--- tests/test_coin_combination.py
+++ tests/test_coin_combination.py
@@ -1,3 +1,19 @@
+import pytest
 from app.coin_combination import get_coin_combination
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    'coins,result',
+    [
+        [1, [1, 0, 0, 0]],
+        [6, [1, 1, 0, 0]],
+        [17, [2, 1, 1, 0]],
+        [50, [0, 0, 0, 2]]
+    ]
+)
+def test_different_coins(coins, result):
+    assert get_coin_combination(coins) == result
+
+
+def test_zero():
+    assert get_coin_combination(0) == [0, 0, 0, 0]
