c0b9cd2256d0bb0e90ef207c7ca7e72f3731693b
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-02-08 07:05:56 +0200

    'Solution'

--- tests/test_coin_combination.py
+++ tests/test_coin_combination.py
@@ -13,5 +13,13 @@ def test_one_coin():
     assert get_coin_combination(1) == [1, 0, 0, 0]
 
 
+def test_only_nickels():
+    assert get_coin_combination(5) == [0, 1, 0, 0]
 
 
+def test_only_dimes():
+    assert get_coin_combination(10) == [0, 0, 1, 0]
+
+
+def test_only_quarters():
+    assert get_coin_combination(50) == [0, 0, 0, 2]
