675f9a6c83daf8344e90e3977172e2e566db3f8e
polinatytarenko <p.tytarenko.20@gmail.com>
2022-02-08 08:43:04 +0200

    Add description to tests

--- tests/test_coin_combination.py
+++ tests/test_coin_combination.py
@@ -5,11 +5,31 @@ from app.coin_combination import get_coin_combination
 @pytest.mark.parametrize(
     "cents,expected_coin_combination",
     [
-        (0, [0, 0, 0, 0]),
-        (2, [2, 0, 0, 0]),
-        (6, [1, 1, 0, 0]),
-        (18, [3, 1, 1, 0]),
-        (72, [2, 0, 2, 2]),
+        pytest.param(
+            0,
+            [0, 0, 0, 0],
+            id="should return zeroes when `cents` equal to zero",
+        ),
+        pytest.param(
+            2,
+            [2, 0, 0, 0],
+            id="should return correct combination of pence",
+        ),
+        pytest.param(
+            6,
+            [1, 1, 0, 0],
+            id="should return correct combination of pence and nickels",
+        ),
+        pytest.param(
+            18,
+            [3, 1, 1, 0],
+            id="should return correct combination of pence, nickels and dimes"
+        ),
+        pytest.param(
+            72,
+            [2, 0, 2, 2],
+            id="should return correct combination of all coins together"
+        ),
     ]
 )
 def test_coin_combination_is_correct(cents, expected_coin_combination):
