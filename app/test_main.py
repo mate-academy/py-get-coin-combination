ce047ae6dec70baa17fa97423994b7077b89da26
andrii kravchenko <andriej.krawczenko@gmail.com>
2022-06-01 18:53:18 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,17 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_get_coin_combination_one_penny():
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+
+
+def test_get_coin_combination_one_nickel_plus():
+    assert get_coin_combination(6) == [1, 1, 0, 0]
+
+
+def test_get_coin_combination_one_dime_plus():
+    assert get_coin_combination(16) == [1, 1, 1, 0]
+
+
+def test_get_coin_combination_one_quarter_plus():
+    assert get_coin_combination(41) == [1, 1, 1, 1]
