6593c7713b0b31308953ec304c6a2449abce56db
Dima Svirsa <august4smile@gmail.com>
2022-06-01 15:16:18 +0300

    'Solution'

--- tests/test_coin_combination.py
+++ tests/test_coin_combination.py
@@ -1,3 +1,37 @@
+import pytest
+
 from app.coin_combination import get_coin_combination
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "cents, coins",
+    [
+        pytest.param(
+            0,
+            [0, 0, 0, 0],
+            id="no money"
+        ),
+        pytest.param(
+            2,
+            [2, 0, 0, 0],
+            id="just a penny"
+        ),
+        pytest.param(
+            7,
+            [2, 1, 0, 0],
+            id="penny and nickel"
+        ),
+        pytest.param(
+            17,
+            [2, 1, 1, 0],
+            id="penny, nickel and dime"
+        ),
+        pytest.param(
+            67,
+            [2, 1, 1, 2],
+            id="penny, nickel, dime and quarters"
+        ),
+    ]
+)
+def test_results(cents, coins):
+    assert get_coin_combination(cents) == coins
