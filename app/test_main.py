81c3094bb61d0cade79b9e66ce0075460a3b60fc
Roma Tsybulia <Romalonstejn@gmail.com>
2022-06-02 14:42:48 +0300

    Deleted unused tests

--- app/test_main.py
+++ app/test_main.py
@@ -2,38 +2,6 @@ from app.main import get_coin_combination
 
 
 def test_get_one_nickel():
-    coins = 0
-
-    value = get_coin_combination(coins)
-
-    assert value == [0, 0, 0, 0]
-
-
-def test_get_one_penny():
-    coins = 1
-
-    value = get_coin_combination(coins)
-
-    assert value == [1, 0, 0, 0]
-
-
-def test_get_one_nickel():
-    coins = 6
-
-    value = get_coin_combination(coins)
-
-    assert value == [1, 1, 0, 0]
-
-
-def test_get_one_nickel():
-    coins = 16
-
-    value = get_coin_combination(coins)
-
-    assert value == [1, 1, 1, 0]
-
-
-def test_get_one_nickel():
     coins = 41
 
     value = get_coin_combination(coins)
