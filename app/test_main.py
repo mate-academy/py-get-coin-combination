04978d087d4bdaa44e02e804ff185db9deb7202e
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-06-02 15:45:45 +0300

    'FixedFlake8'

--- app/test_main.py
+++ app/test_main.py
@@ -4,11 +4,14 @@ from app.main import get_coin_combination
 def test_1_coin():
     assert get_coin_combination(1) == [1, 0, 0, 0]
 
+
 def test_even_coins():
     assert get_coin_combination(6) == [1, 1, 0, 0]
 
+
 def test_odd_coins():
     assert get_coin_combination(17) == [2, 1, 1, 0]
+
 
 def test_big_amount():
     assert get_coin_combination(50) == [0, 0, 0, 2]
