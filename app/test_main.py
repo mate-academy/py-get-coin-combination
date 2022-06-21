8996a5f210d28ccbcf953668136621eff28cb3d6
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-06-21 20:52:46 +0300

    fixed test

--- app/test_main.py
+++ app/test_main.py
@@ -2,8 +2,4 @@ from app.main import get_coin_combination
 
 
 def test_should_return_correct_list_when_cents_equal_50():
-    assert get_coin_combination(50) == [0, 0, 0, 2]
-
-
-def test_should_return_correct_list_when_cents_equal_100():
-    assert get_coin_combination(50) == [0, 0, 0, 4]
+    assert get_coin_combination(68) == [3, 1, 1, 2]
