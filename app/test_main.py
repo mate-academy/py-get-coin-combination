67a3827d05ae24732281422a7075c29242780a29
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-06-21 18:29:50 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,9 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_should_return_correct_list_when_cents_equal_50():
+    assert get_coin_combination(50) == [0, 0, 0, 2]
+
+
+def test_should_return_correct_list_when_cents_equal_100():
+    assert get_coin_combination(50) == [0, 0, 0, 4]
