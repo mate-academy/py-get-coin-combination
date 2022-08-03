865db3ce44c3a66a798deb57d07151dfb1270622
Anastasiia Shevchenko <anastasiia.sshevchenko@gmail.com>
2022-08-03 18:06:28 +0300

    MY_Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,18 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+class TestCoin:
+    def test_if_coin_is_zero(self):
+        assert get_coin_combination(0) == [0, 0, 0]
+
+    def test_if_coin_is_one(self):
+        assert get_coin_combination(1) == [1, 0, 0, 0]
+
+    def test_return_nickels(self):
+        assert get_coin_combination(6) == [1, 1, 0, 0]
+
+    def test_return_dimes(self):
+        assert get_coin_combination(17) == [2, 1, 1, 0]
+
+    def test_return_quarters(self):
+        assert get_coin_combination(50) == [0, 0, 0, 2]
