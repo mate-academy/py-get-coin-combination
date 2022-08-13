0939f7aa96a06f5d0c2b756be5ccb4433996f84c
Dima Povkh <dimapovkh@gmail.com>
2022-08-14 01:19:34 +0200

    get_coin_combination_v1

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,17 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+class TestGetCoinsCombination:
+    @pytest.mark.parametrize("initial_number, expected_coins",
+        [
+            pytest.param(
+                41,
+                [1, 1, 1, 1],
+                id="return different coins, not only pennies"
+            )
+        ]
+    )
+    def test_count_coins_correctly(self, initial_number, expected_coins):
+        assert get_coin_combination(initial_number) == expected_coins
