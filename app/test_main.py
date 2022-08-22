9b8de00572fea83cae4334ada447fef1a77fa8c6
yaroslav-demchenko <dastex1411@gmail.com>
2022-08-22 18:36:37 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -12,6 +12,8 @@ class TestGetCoinCombination:
             (50, [0, 0, 0, 2])
         ]
     )
-    def test_should_return_the_least_number_of_coins(self, denomination, expected_value):
+    def test_should_return_the_least_number_of_coins(self,
+                                                     denomination,
+                                                     expected_value):
         result = get_coin_combination(denomination)
         assert result == expected_value
