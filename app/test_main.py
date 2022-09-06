a22718d6c239d73c0a93f21f8d489d504c0c22e3
Katya Vasylieva <katevvasylyeva@gmail.com>
2022-09-06 18:02:45 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,11 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_should_return_correct_value():
+    coins = get_coin_combination(17)
+    assert coins == [2, 1, 1, 0]
+
+
+def test_should_return_zeros_when_cents_equal_0():
+    coins = get_coin_combination(0)
+    assert coins == [0, 0, 0, 0]
