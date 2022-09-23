f607e1f341fb5b8eb5da35846529de43352fad8e
Davyd Kryvko <davidkrivko@gmail.com>
2022-09-23 12:41:36 +0200

    'Solution_5'

--- app/test_main.py
+++ app/test_main.py
@@ -11,21 +11,21 @@ class TestGetCoinCombination:
             [1, 0, 0, 0],
             id="Should return list"
         ),
-        pytest.param(
-            6,
-            [1, 1, 0, 0],
-            id="Should return list"
-        ),
-        pytest.param(
-            17,
-            [2, 1, 1, 0],
-            id="Should return list"
-        ),
-        pytest.param(
-            50,
-            [0, 0, 0, 2],
-            id="Should return list"
-        )]
+            pytest.param(
+                6,
+                [1, 1, 0, 0],
+                id="Should return list"
+            ),
+            pytest.param(
+                17,
+                [2, 1, 1, 0],
+                id="Should return list"
+            ),
+            pytest.param(
+                50,
+                [0, 0, 0, 2],
+                id="Should return list"
+            )]
     )
     def test_should_return_expected_list(
             self,
