b6ff785898b46a64b47901a6277e6814c84c9544
Davyd Kryvko <davidkrivko@gmail.com>
2022-09-23 12:40:10 +0200

    'Solution_4'

--- app/test_main.py
+++ app/test_main.py
@@ -3,27 +3,32 @@ import pytest
 from app.main import get_coin_combination
 
 
-pytest.mark.parametrize(
-    "cents,expected_list",
-    pytest.param(
-        1, [1, 0, 0, 0],
-        id="Should return list"
-    ),
-    pytest.param(
-        6, [1, 1, 0, 0],
-        id="Should return list"
-    ),
-    pytest.param(
-        17, [2, 1, 1, 0],
-        id="Should return list"
-    ),
-    pytest.param(
-        50, [0, 0, 0, 2],
-        id="Should return list"
+class TestGetCoinCombination:
+    @pytest.mark.parametrize(
+        "cents,expected_list",
+        [pytest.param(
+            1,
+            [1, 0, 0, 0],
+            id="Should return list"
+        ),
+        pytest.param(
+            6,
+            [1, 1, 0, 0],
+            id="Should return list"
+        ),
+        pytest.param(
+            17,
+            [2, 1, 1, 0],
+            id="Should return list"
+        ),
+        pytest.param(
+            50,
+            [0, 0, 0, 2],
+            id="Should return list"
+        )]
     )
-)
-
-
-def test_should_return_expected_list(
-        cents, expected_list):
-    assert get_coin_combination(cents) == expected_list
+    def test_should_return_expected_list(
+            self,
+            cents,
+            expected_list):
+        assert get_coin_combination(cents) == expected_list
