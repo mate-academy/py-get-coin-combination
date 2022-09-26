04a7e576d2d4e6fbafa1551b66c7d3cab652eef1
Stas Okhrym <s.okhrym@gmail.com>
2022-09-26 21:33:28 +0300

    Fixed formating

--- app/test_main.py
+++ app/test_main.py
@@ -4,23 +4,14 @@ from app.main import get_coin_combination
 
 
 class TestGetCoinCombination:
-    @pytest.mark.parametrize("cents,coins",
+    @pytest.mark.parametrize(
+        "cents,coins",
         [
-            pytest.param(
-                0, [0, 0, 0, 0]
-            ),
-            pytest.param(
-                1, [1, 0, 0, 0]
-            ),
-            pytest.param(
-                6, [1, 1, 0, 0]
-            ),
-            pytest.param(
-                17, [2, 1, 1, 0]
-            ),
-            pytest.param(
-                50, [0, 0, 0, 2]
-            )
+            (0, [0, 0, 0, 0]),
+            (1, [1, 0, 0, 0]),
+            (6, [1, 1, 0, 0]),
+            (7, [2, 1, 1, 0]),
+            (50, [0, 0, 0, 2])
         ]
     )
     def test_correct_coins(
