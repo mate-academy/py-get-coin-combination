abca00e23b42c818118d181a1c2f95ede92300d4
Stas Okhrym <s.okhrym@gmail.com>
2022-09-26 21:27:07 +0300

    Fixed solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,23 +4,23 @@ from app.main import get_coin_combination
 
 
 class TestGetCoinCombination:
-    @pytest.mark.parametrize("cents, coins",
+    @pytest.mark.parametrize("cents,coins",
         [
-            (
+            pytest.param(
                 0, [0, 0, 0, 0]
             ),
-            (
+            pytest.param(
                 1, [1, 0, 0, 0]
             ),
-            (
+            pytest.param(
                 6, [1, 1, 0, 0]
             ),
-            (
+            pytest.param(
                 17, [2, 1, 1, 0]
             ),
-            (
+            pytest.param(
                 50, [0, 0, 0, 2]
-            ),
+            )
         ]
     )
     def test_correct_coins(
