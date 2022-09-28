913077a4523a66cef0fe08806d10a9eaf9e761c5
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-09-28 23:14:54 +0300

    Solution for the task

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,13 @@
 from app.main import get_coin_combination
+import pytest
 
 
-# write your tests here
+class TestCoinCombination:
+    @pytest.mark.parametrize("initial_variable, expected_results", [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2])
+    ])
+    def test_values_have_the_correct_type(self, initial_variable, expected_results):
+        assert get_coin_combination(initial_variable) == expected_results
