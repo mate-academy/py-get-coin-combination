e2356a878835b017fca804fc5765e7d050190034
Taras Andruschenko <And999110@gmail.com>
2022-09-28 19:48:39 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,36 @@
 from app.main import get_coin_combination
+import pytest
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, par",
+    [
+        pytest.param(
+            1,
+            [1, 0, 0, 0],
+            id="should return only pennies"
+        ),
+        pytest.param(
+            6,
+            [1, 1, 0, 0],
+            id="should return pennies and nickels"
+        ),
+        pytest.param(
+            17,
+            [2, 1, 1, 0],
+            id="should return pennies, nickels and dimes"
+        ),
+        pytest.param(
+            50,
+            [0, 0, 0, 2],
+            id="should return only quarters"
+        ),
+        pytest.param(
+            947,
+            [2, 0, 2, 37],
+            id="shoul equal to result for a big number"
+        )
+    ]
+)
+def test_get_coin_combination(cents: int, par: list) -> None:
+    assert get_coin_combination(cents) == par
