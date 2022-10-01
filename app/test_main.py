b564ad9f9406abca56b782cddd2841acdd0bdc78
AndriyKy <akrechovetskiy@gmail.com>
2022-10-01 12:04:31 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,18 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "coin,expected_rezult",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 3])
+    ]
+)
+def test_should_return_number_of_coins(coin, expected_rezult):
+    assert get_coin_combination(coin) == expected_rezult, \
+        "The function should return the combination of the smallest \
+possible number of coins"
