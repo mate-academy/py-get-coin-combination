84a7389b88caac054e05c4ae9729d5b4e9abcaa3
Serhii Kovalenko <27kovalenko@gmail.com>
2022-10-02 18:48:45 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,19 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents,combination",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2])
+    ]
+)
+def test_three_params_are_passed(cents, combination):
+    assert get_coin_combination(cents) == combination, (
+        f"Function 'get_coin_combination' should return '{combination}' "
+        f"when 'cents' is equal to {cents}"
+    )
