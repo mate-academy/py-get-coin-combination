f8cddbfa1ba40230b03dcb9ef448a1f62e7ecaa0
Yulia Bozhieva <bozhieva93@gmail.com>
2022-10-03 14:16:35 -0400

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,21 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_should_return_1_penny_if_given_1_cent():
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+
+
+def test_should_return_1_nickel_if_given_5_cent():
+    assert get_coin_combination(5) == [0, 1, 0, 0]
+
+
+def test_should_return_1_dime_if_given_10_cent():
+    assert get_coin_combination(10) == [0, 0, 1, 0]
+
+
+def test_should_return_1_quarter_if_given_25_cent():
+    assert get_coin_combination(25) == [0, 0, 0, 1]
+
+
+def test_should_return_6_coins_if_given_67_cent():
+    assert get_coin_combination(67) == [2, 1, 1, 2]
