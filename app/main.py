bdb997dca27d5b15b0f62edba2fbcecfa0c4f913
Dima Ursatii <Dimon4ir@gmail.com>
2022-10-04 21:31:44 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -7,9 +7,3 @@ def get_coin_combination(cents: int) -> list:
         cents -= coins[i] * values[i]
 
     return coins
-
-
-print(get_coin_combination(1)) == [1, 0, 0, 0]  # 1 penny
-print(get_coin_combination(6)) == [1, 1, 0, 0]  # 1 penny + 1 nickel
-print(get_coin_combination(17)) == [2, 1, 1, 0]   # 2 pennies + 1 nickel + 1 dime
-print(get_coin_combination(50)) #== [0, 0, 0, 2]   # 2 quarters
