1ce2cf51b307e7a28ac5c50d6216c1ed83a448e7
Alexander <alexzubkoff123@gmail.com>
2022-10-04 23:28:27 +0300

    Created tests for coin combinations

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,25 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_get_coin_combination_could_return_zero_coins():
+    assert get_coin_combination(0) == [0, 0, 0, 0]
+
+
+def test_get_coin_combination_could_return_pennies():
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+
+
+def test_get_coin_combination_could_return_nickels():
+    assert get_coin_combination(5) == [0, 1, 0, 0]
+
+
+def test_get_coin_combination_could_return_dimes():
+    assert get_coin_combination(10) == [0, 0, 1, 0]
+
+
+def test_get_coin_combination_could_return_quarters():
+    assert get_coin_combination(25) == [0, 0, 0, 1]
+
+
+def test_get_coin_combination_could_return_coins_different_types():
+    assert get_coin_combination(394) == [4, 1, 1, 15]
