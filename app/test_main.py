e6abe76244d013f9e4d51d1d855b6a5077bbeb69
Vitalii Shevchuk <shevch.nl@gmail.com>
2022-10-05 22:57:19 +0300

    annotation fixed

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,21 @@
 from app.main import get_coin_combination
 
 
-def test_return_zero_when_input_zero():
+def test_return_zero_when_input_zero() -> None:
     assert get_coin_combination(0) == [0, 0, 0, 0]
 
 
-def test_return_pennies_when_input_less_then_6():
+def test_return_pennies_when_input_less_then_6() -> None:
     assert get_coin_combination(3) == [3, 0, 0, 0]
 
 
-def test_return_nickels_when_input_more_then_6_and_less_10():
+def test_return_nickels_when_input_more_then_6_and_less_10() -> None:
     assert get_coin_combination(8) == [3, 1, 0, 0]
 
 
-def test_return_dimes_when_input_more_then_ten_and_less_25():
+def test_return_dimes_when_input_more_then_ten_and_less_25() -> None:
     assert get_coin_combination(21) == [1, 0, 2, 0]
 
 
-def test_return_quarters_when_input_more_then_25():
+def test_return_quarters_when_input_more_then_25() -> None:
     assert get_coin_combination(56) == [1, 1, 0, 2]
