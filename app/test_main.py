9100857ab72e00548e76acb6c348082f5771c12b
Your Name <y>
2022-10-15 15:59:05 +0300

    Tests added

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,15 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+class TestCoin:
+    @pytest.mark.parametrize("cents, coins", [
+        pytest.param(1, [1, 0, 0, 0], id="1 cent test"),
+        pytest.param(5, [0, 1, 0, 0], id="5 cents test"),
+        pytest.param(10, [0, 0, 1, 0], id="10 cents test"),
+        pytest.param(25, [0, 0, 0, 1], id="25 cents test"),
+        pytest.param(66, [1, 1, 1, 2], id="66 cents test")
+    ])
+    def test_coin_combinations(self, cents: int, coins: list) -> None:
+        assert get_coin_combination(cents) == coins
