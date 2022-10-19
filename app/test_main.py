90c0eade34afe8446247be7dc4f26e25d6883a1d
Oleksandr Mamchur <irex1988@gmail.com>
2022-10-19 12:59:17 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,29 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_should_return_all_0_when_coins_is_0() -> None:
+    assert get_coin_combination(0) == [0, 0, 0, 0]
+
+
+def test_should_return_expected_result_when_coins_1() -> None:
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+
+
+def test_should_return_expected_result_when_coins_5() -> None:
+    assert get_coin_combination(5) == [0, 1, 0, 0]
+
+
+def test_should_return_expected_result_when_coins_10() -> None:
+    assert get_coin_combination(10) == [0, 0, 1, 0]
+
+
+def test_should_return_expected_result_when_coins_25() -> None:
+    assert get_coin_combination(25) == [0, 0, 0, 1]
+
+
+def test_should_return_expected_result_with_all_values() -> None:
+    assert get_coin_combination(41) == [1, 1, 1, 1]
+
+
+def test_should_return_expected_result_when_big_number_of_coins() -> None:
+    assert get_coin_combination(1111116) == [1, 1, 1, 44444]
