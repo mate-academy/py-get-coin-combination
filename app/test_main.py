8eb4c6e7f1ff2e76578b03107890372f4aaa3058
Andrew.H <john.riderr9@gmail.com>
2022-11-07 15:52:43 +0200

    fix flake8

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,13 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_coin_combinationn_1() -> None:
+    assert (
+        get_coin_combination(50) == [0, 0, 0, 2]
+    ), "could return different coins, not only pennies"
+
+
+def test_coin_combinationn_2() -> None:
+    assert (
+        get_coin_combination(17) == [2, 1, 1, 0]
+    ), "could return coins of the different types"
