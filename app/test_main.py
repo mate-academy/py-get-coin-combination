502dcfee736830b8c379cdf9d9ed76d6cc2f9007
vani@linux <winfkill@gmail.com>
2022-11-20 21:58:46 +0200

    DONe

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,26 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_check_type_of_function():
+    assert isinstance(get_coin_combination(50), list)
+
+
+def test_check_right_values_():
+    assert len(get_coin_combination(1)) == 4
+
+
+def test_check_right_values_type():
+    assert isinstance(get_coin_combination(1123)[1], int)
+
+
+def test_check_right_values_from_small_to_big():
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+    assert get_coin_combination(6) == [1, 1, 0, 0]
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+    assert get_coin_combination(50) == [0, 0, 0, 2]
+
+
+def test_check_values_in_non_normal_range():
+    assert get_coin_combination(500) == [0, 0, 0, 20]
+    assert get_coin_combination(0) == [0, 0, 0, 0]
+    assert get_coin_combination(1424142) == [2, 1, 1, 56965]
