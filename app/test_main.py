7b07e8fa3351870b4e11d86e3b78c25eb19973f7
Yevhen Martyniuk <evgenijmartynuk07@gmail.com>
2022-12-25 14:24:56 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,9 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_should_return_pennies_nickel_dime_if_cents_17() -> None:
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+
+
+def test_should_return_quarter_when_cents_is_50() -> None:
+    assert get_coin_combination(50) == [0, 0, 0, 2]
