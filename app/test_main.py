31b57d1264015c13a1499d71de13f20a54908216
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2022-12-26 21:54:38 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,21 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+class TestCombination:
+    @pytest.mark.parametrize(
+        "cents,result",
+        [
+            (1, [1, 0, 0, 0]),
+            (6, [1, 1, 0, 0]),
+            (17, [2, 1, 1, 0]),
+            (50, [0, 0, 0, 2]),
+        ]
+    )
+    def test_correct_combination_of_the_coins(self, cents: int, result: list) -> None:
+        assert get_coin_combination(cents) == result
+
+
+def test_not_integer_type_exception():
+    with pytest.raises(TypeError):
+        get_coin_combination("5")
