88b2c0b4cff3686ef35354fe7e7d863f1f003569
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2022-12-26 21:58:20 +0200

    New solution

--- app/test_main.py
+++ app/test_main.py
@@ -12,10 +12,14 @@ class TestCombination:
             (50, [0, 0, 0, 2]),
         ]
     )
-    def test_correct_combination_of_the_coins(self, cents: int, result: list) -> None:
+    def test_correct_combination_of_the_coins(
+            self,
+            cents: int,
+            result: list
+    ) -> None:
         assert get_coin_combination(cents) == result
 
 
-def test_not_integer_type_exception():
+def test_not_integer_type_exception() -> None:
     with pytest.raises(TypeError):
         get_coin_combination("5")
