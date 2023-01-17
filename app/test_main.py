54e896e9a20667e23df1695cec1fcd277735cfdf
MIlan <milan.manoylov@gmail.com>
2023-01-17 15:02:49 -0600

    get_coins_combination_Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,21 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_number_of_pennies() -> None:
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+
+
+def test_number_of_nickels() -> None:
+    assert get_coin_combination(6) == [1, 1, 0, 0]
+
+
+def test_number_of_dimes() -> None:
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+
+
+def test_number_of_quarters() -> None:
+    assert get_coin_combination(50) == [0, 0, 0, 2]
+
+
+def test_number_equals_0() -> None:
+    assert get_coin_combination(0) == [0, 0, 0, 0]
