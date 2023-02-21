f4e5868949fcb42b80f028a572e1ff57dc767542
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-02-21 18:02:15 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -8,4 +8,3 @@ def test_get_coin_combination() -> None:
     assert get_coin_combination(10) == [0, 0, 1, 0]
     assert get_coin_combination(25) == [0, 0, 0, 1]
     assert get_coin_combination(99) == [4, 0, 2, 3]
-
