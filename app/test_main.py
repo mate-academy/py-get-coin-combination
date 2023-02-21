e101fa96952b1014e46d7996c9de52e3d8813075
Bohdan Lazaryshyn <lazaryshyn1998@gmail.com>
2023-02-21 14:14:18 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -3,7 +3,7 @@ import pytest
 from app.main import get_coin_combination
 
 
-class TestGetHumanAgeClass:
+class TestGetCoinCombinationClass:
     @pytest.mark.parametrize(
         "get_coins, sorted_coins",
         [
