d45a7ece45a84b74a1b481a2c06ebdb9ef20be10
Ruslan <noezyboy1@gmail.com>
2023-02-22 13:15:00 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,36 +1,23 @@
+import pytest
 from app.main import get_coin_combination
 
 
-def test_returns_correct_change() -> None:
-    assert get_coin_combination(1) == [1, 0, 0, 0]
-    assert get_coin_combination(6) == [1, 1, 0, 0]
-    assert get_coin_combination(17) == [2, 1, 1, 0]
-    assert get_coin_combination(50) == [0, 0, 0, 2]
+def test_return_only_pennies() -> None :
+    assert get_coin_combination(6) != [6, 0, 0, 0]
 
 
-def test_returns_list_of_length_4() -> None:
-    assert len(get_coin_combination(0)) == 4
-    assert len(get_coin_combination(1)) == 4
-    assert len(get_coin_combination(100)) == 4
+def test_return_only_one_type() -> None :
+    assert get_coin_combination(25) != [0, 0, 3, 0]
 
 
-def test_returns_only_non_negative_integers() -> None:
-    for i in range(100):
-        coins = get_coin_combination(i)
-        assert all(isinstance(c, int) and c >= 0 for c in coins)
+def test_negative_input() -> None :
+    with pytest.raises(ValueError):
+        get_coin_combination(-1)
 
 
-def test_returns_smallest_combination() -> None:
-    assert get_coin_combination(16) == [1, 1, 1, 0]
-    assert get_coin_combination(12) == [2, 0, 1, 0]
-    assert get_coin_combination(19) == [4, 1, 1, 0]
-    assert get_coin_combination(26) == [1, 0, 0, 1]
+def test_zero_input() -> None :
+    assert get_coin_combination(0) == [0, 0, 0, 0]
 
 
-def test_returns_coins_of_different_denominations() -> None:
-    assert sum(get_coin_combination(99)) == 99
-
-
-def test_returns_coins_of_different_types() -> None:
-    coins = get_coin_combination(99)
-    assert coins[0] > 0 or coins[1] > 0 or coins[2] > 0 or coins[3] > 0
+def test_larger_input() -> None :
+    assert get_coin_combination(123) == [3, 0, 2, 4]
