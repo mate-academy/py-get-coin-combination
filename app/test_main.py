f1bd882147efea00cf4a2c77e94ef5320554e342
Mykola Savchyn <kola.savchyn@gmail.com>
2023-02-22 13:46:44 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,21 @@
 from app.main import get_coin_combination
 
 
-def test_zero_cents_returns_list_with_zeroes():
+def test_zero_cents_returns_list_with_zeroes() -> None:
     assert get_coin_combination(0) == [0, 0, 0, 0]
 
 
-def test_only_cents():
+def test_only_cents() -> None:
     assert get_coin_combination(2) == [2, 0, 0, 0]
 
 
-def test_if_5_or_more_count_nickels():
+def test_if_5_or_more_count_nickels() -> None:
     assert get_coin_combination(7) == [2, 1, 0, 0]
 
 
-def test_if_10_or_more_count_dimes():
+def test_if_10_or_more_count_dimes() -> None:
     assert get_coin_combination(17) == [2, 1, 1, 0]
 
 
-def test_if_25_or_more_count_quarters():
+def test_if_25_or_more_count_quarters() -> None:
     assert get_coin_combination(51) == [1, 0, 0, 2]
