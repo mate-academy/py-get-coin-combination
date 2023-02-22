fb2440be3bdab9ad038b0690c67257f6865f0352
Serhii Melnyk <serg.melnyk1998@gmail.com>
2023-02-22 21:11:53 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,41 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+class TestGetCoinCombination:
+    @pytest.mark.parametrize(
+        "cents, result",
+        [
+            pytest.param(
+                1,
+                [1, 0, 0, 0],
+                id="test for 1 cent should return [1, 0, 0, 0]"
+            ),
+            pytest.param(
+                6,
+                [1, 1, 0, 0],
+                id="test for 1 penny + 1 nickel should return [1, 1, 0, 0]"
+            ),
+            pytest.param(
+                17,
+                [2, 1, 1, 0],
+                id="test for 2 pennies + 1 nickel + 1 dime should return [2, 1, 1, 0]"
+            ),
+            pytest.param(
+                50,
+                [0, 0, 0, 2],
+                id="test for 2 quarters should return [0, 0, 0, 2]"
+            ),
+            pytest.param(
+                0,
+                [0, 0, 0, 0],
+                id="test for zero penny should return [0, 0, 0, 0]"
+            )
+        ]
+    )
+    def test_get_coin_combination(
+            self,
+            cents: int,
+            result: list
+    ) -> None:
+        assert get_coin_combination(cents) == result
