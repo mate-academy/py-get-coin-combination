7b5c5dbc8880327443ef449d82aa5118000e21d6
Ostap <anastasiia@Anastasiias-Air.Dlink>
2023-02-22 15:32:01 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,17 @@
 from app.main import get_coin_combination
 
 
-def test_should_return_one_penny():
+def test_should_return_one_penny() -> None:
     assert get_coin_combination(1) == [1, 0, 0, 0]
 
 
-def test_should_return_one_penny_and_one_nickel():
+def test_should_return_one_penny_and_one_nickel() -> None:
     assert get_coin_combination(6) == [1, 1, 0, 0]
 
 
-def test_should_return_two_pennies_one_nickel_and_one_dime():
+def test_should_return_two_pennies_one_nickel_and_one_dime() -> None:
     assert get_coin_combination(17) == [2, 1, 1, 0]
 
 
-def test_should_return_two_quarters():
+def test_should_return_two_quarters() -> None:
     assert get_coin_combination(50) == [0, 0, 0, 2]
