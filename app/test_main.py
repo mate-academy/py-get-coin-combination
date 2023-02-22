a4a139b3132140cccf3e259dbb077167fe68c365
Ruslan <noezyboy1@gmail.com>
2023-02-22 13:03:24 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,36 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_returns_correct_change():
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+    assert get_coin_combination(6) == [1, 1, 0, 0]
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+    assert get_coin_combination(50) == [0, 0, 0, 2]
+
+
+def test_returns_list_of_length_4():
+    assert len(get_coin_combination(0)) == 4
+    assert len(get_coin_combination(1)) == 4
+    assert len(get_coin_combination(100)) == 4
+
+
+def test_returns_only_non_negative_integers():
+    for i in range(100):
+        coins = get_coin_combination(i)
+        assert all(isinstance(c, int) and c >= 0 for c in coins)
+
+
+def test_returns_smallest_combination():
+    assert get_coin_combination(16) == [1, 1, 1, 0]
+    assert get_coin_combination(12) == [2, 0, 1, 0]
+    assert get_coin_combination(19) == [4, 1, 1, 0]
+    assert get_coin_combination(26) == [1, 0, 0, 1]
+
+def test_returns_coins_of_different_denominations():
+    assert sum(get_coin_combination(99)) == 99
+
+
+def test_returns_coins_of_different_types():
+    coins = get_coin_combination(99)
+    assert coins[0] > 0 or coins[1] > 0 or coins[2] > 0 or coins[3] > 0
