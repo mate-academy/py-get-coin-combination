4dbd1ebb61b601d9881919b58caf2c1db7790863
Yulia <Mala>
2023-02-22 18:34:09 +0200

    solution-5

--- app/test_main.py
+++ app/test_main.py
@@ -5,7 +5,7 @@ from app.main import get_coin_combination
 @pytest.mark.parametrize(
     "cents,result",
     [
-        pytest.param(0, [], id="zero cents"),
+        pytest.param(0, [0, 0, 0, 0], id="zero cents"),
         pytest.param(5, [0, 1, 0, 0], id="1 coin type"),
         pytest.param(41, [1, 1, 1, 1], id="all coin types"),
         pytest.param(55, [0, 1, 0, 2], id="two coin types")
