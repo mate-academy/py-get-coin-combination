0fa83225010bf0020acd6ffedc4206b2d16a8b7e
NVMBR13 <108105455+NVMBR13@users.noreply.github.com>
2023-02-28 18:21:59 +0200

    v1

--- app/test_main.py
+++ app/test_main.py
@@ -4,11 +4,12 @@ import pytest
 from app.main import get_coin_combination
 
 
-@pytest.mark.parametrize("coins, result", [
-        (1, [1, 0, 0, 0]),
-        (6, [1, 1, 0, 0]),
-        (17, [2, 1, 1, 0]),
-        (50, [0, 0, 0, 2])
-    ])
+@pytest.mark.parametrize(
+    "coins, result",
+    [(1, [1, 0, 0, 0]),
+     (6, [1, 1, 0, 0]),
+     (17, [2, 1, 1, 0]),
+     (50, [0, 0, 0, 2])],
+)
 def test_coin_combination(coins: int, result: list[int]) -> None:
     assert get_coin_combination(coins) == result
