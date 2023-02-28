9a3af28a1f017238be5e5e6f87dc6d60773bb0eb
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-02-28 19:39:14 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -10,7 +10,7 @@ from app.main import get_coin_combination
         (1, [1, 0, 0, 0]),
         (6, [1, 1, 0, 0]),
         (17, [2, 1, 1, 0]),
-        (50, [0, 0, 0, 2]),
+        (50, [0, 0, 0, 2])
     ]
 )
 def test_return_result(cents: int, expected_result: list[int]) -> None:
