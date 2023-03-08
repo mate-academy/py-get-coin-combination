df9aeeff501164698f95eaa554732ee0b764658d
Oleksandr Hryshchenko <oleksandr.grk@gmail.com>
2023-03-08 21:47:31 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,17 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_should_return_list_of_four_zeroes_when_zero_cent_given() -> None:
+    assert get_coin_combination(0) == [0, 0, 0, 0]
+
+
+def test_should_return_valid_amount_of_pennies() -> None:
+    assert get_coin_combination(4) == [4, 0, 0, 0]
+
+
+def test_should_return_biggest_coins_first() -> None:
+    assert get_coin_combination(42) == [2, 1, 1, 1]
+
+
+def test_should_return_only_quarters_when_cent_divided_by_25() -> None:
+    assert get_coin_combination(50) == [0, 0, 0, 2]
