38b5be0169e60b92cbe2e4c44a55bd00266103cf
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-04-06 18:57:39 +0300

    Slava Ukraini!

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,26 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cent,result",
+    [
+        pytest.param(
+            0,
+            [0, 0, 0, 0],
+            id="must work with zero"
+        ),
+        pytest.param(
+            50,
+            [0, 0, 0, 2],
+            id="must work when 2 quarters"
+        ),
+        pytest.param(
+            17,
+            [2, 1, 1, 0],
+            id="must work when 2 pennies + 1 nickel + 1 dime"
+        )
+    ]
+)
+def test_checking_money(cent: int, result: list[int]) -> None:
+    assert get_coin_combination(cent) == result
