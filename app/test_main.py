47920b7b734cb0926d102d79327baaf525dab311
Munkoor <eee1lvlboss@gmail.com>
2023-05-07 23:14:04 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,17 @@
 from app.main import get_coin_combination
 
 
-def should_return_only_penny() -> None:
+def test_should_return_only_penny() -> None:
     assert get_coin_combination(1) == [1, 0, 0, 0]
 
 
-def should_return_penny_and_nickel() -> None:
+def test_should_return_penny_and_nickel() -> None:
     assert get_coin_combination(6) == [1, 1, 0, 0]
 
 
-def should_return_penny_and_nickel_and_dime() -> None:
+def test_should_return_penny_and_nickel_and_dime() -> None:
     assert get_coin_combination(17) == [2, 1, 1, 0]
 
 
-def should_return_all_values() -> None:
+def test_should_return_all_values() -> None:
     assert get_coin_combination(50) == [0, 0, 0, 2]
