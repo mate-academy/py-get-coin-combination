0b72a852c4e0b1ff2d21679e6b8e3461123ce37c
Anton Piddubnyi <antonpiddubnyii@gmail.com>
2023-05-13 16:38:49 -0600

    added type test

--- app/test_main.py
+++ app/test_main.py
@@ -2,9 +2,18 @@ import pytest
 from app.main import get_coin_combination
 
 
+def test_return_type() -> None:
+    assert isinstance(get_coin_combination(1), list)
+
+
 @pytest.mark.parametrize(
     "amount, expected_result",
     [
+        pytest.param(
+            0,
+            [0, 0, 0, 0],
+            id="should return zero"
+        ),
         pytest.param(
             1,
             [1, 0, 0, 0],
@@ -15,19 +24,11 @@ from app.main import get_coin_combination
             [1, 1, 0, 0],
             id="Should be 1 penny + 1 nickel"
         ),
-
         pytest.param(
             17,
             [2, 1, 1, 0],
             id="Should be 2 pennies + 1 nickel + 1 dime"
         ),
-
-        pytest.param(
-            -5,
-            [0, 0, 0, 0],
-            id="Test should work correct with negative arguments"
-        ),
-
         pytest.param(
             50,
             [0, 0, 0, 2],
@@ -40,5 +41,3 @@ def test_the_number_of_coins(
         expected_result: list
 ) -> None:
     assert get_coin_combination(amount) == expected_result
-
-# write your tests here
