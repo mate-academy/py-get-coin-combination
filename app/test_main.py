59049b76dccf609449c1e5af624c4e73d3131447
Oleksandr Gubarev <alexgubarev885@gmail.com>
2023-05-20 15:25:58 +0300

    Fix Py get coin combination

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,17 @@
 from app.main import get_coin_combination
 
 
-def test_return_only_penny():
+def test_return_only_penny() -> None:
     assert get_coin_combination(1) == [1, 0, 0, 0]
 
 
-def test_return_penny_and_nickel():
+def test_return_penny_and_nickel() -> None:
     assert get_coin_combination(6) == [1, 1, 0, 0]
 
 
-def test_return_pennies_nickel_dime():
+def test_return_pennies_nickel_dime() -> None:
     assert get_coin_combination(17) == [2, 1, 1, 0]
 
 
-def test_return_only_quarters():
+def test_return_only_quarters() -> None:
     assert get_coin_combination(50) == [0, 0, 0, 2]
