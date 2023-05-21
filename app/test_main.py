a4814b96aa7273daa690218ba6b203e1d9e78c08
MaksymUK <clear>
2023-05-21 08:52:40 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -9,9 +9,8 @@ from app.main import get_coin_combination
         (6, [1, 1, 0, 0]),
         (17, [2, 1, 1, 0]),
         (50, [0, 0, 0, 2]),
-        (50, [0, 0, 5, 2]),
         (99, [4, 0, 2, 3]),
-        (123, [3, 4, 2, 4])
+        (123, [3, 0, 2, 4])
     ]
 )
 def test_right_return_for_given_number_of_coins(
