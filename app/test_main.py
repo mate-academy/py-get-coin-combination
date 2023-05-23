0db46e495e47658a0509115e24a8acb503ba03de
Volodymyr Bohomol <8773927@gmail.com>
2023-05-23 22:35:48 +0300

    Has writen tests for `get_coin_combination` function

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,22 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents,combination",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2])
+    ],
+    ids=[
+        "Start count from penny",
+        "Checking if 1 nickel = 5 pennies",
+        "Checking if 1 dime = 10 pennies",
+        "Checking counting quarters"
+    ]
+)
+def test_get_coin_combination(cents: int, combination: list) -> None:
+    assert get_coin_combination(cents) == combination
