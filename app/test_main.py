647f5383c1f8e6df0ad519a1ca23c83a88fa20a6
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-05-24 22:37:10 +0300

    ......

--- app/test_main.py
+++ app/test_main.py
@@ -3,32 +3,14 @@ import pytest
 from app.main import get_coin_combination
 
 
-def test_func_return_list():
-    assert isinstance(get_coin_combination(123), list)
-
-
-def test_func_return_right_len():
-    assert len(get_coin_combination(123)) == 4
-
-
-def test_fun_can_convert_all_values():
-    assert 0 not in get_coin_combination(41)
-
-
 @pytest.mark.parametrize(
     "cents, result",
     [
-        (1, [1, 0, 0, 0]),
-        (6, [1, 1, 0, 0]),
-        (17, [2, 1, 1, 0]),
-        (50, [0, 0, 0, 2])
-    ],
-    ids=["penni",
-         "nickel convert",
-         "dime convert",
-         "quarter convert"]
+        pytest.param(0, [0, 0, 0, 0]),
+        pytest.param(4, [4, 0, 0, 0]),
+        pytest.param(5, [0, 1, 0, 0]),
+        pytest.param(9, [4, 1, 0, 0])
+    ]
 )
-def test_big_parametrize_daddy(cents, result):
-    assert (
-            get_coin_combination(cents) == result
-    )
+def test_returns_smallest_combination(cents: int, result: list) -> None:
+    assert get_coin_combination(cents) == result
