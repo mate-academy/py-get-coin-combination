82e8819ac7a0d1077eb4db62217e8ab25e1147fd
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-05-24 22:13:38 +0300

    .

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,30 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_func_return_list():
+    assert isinstance(get_coin_combination(123), list)
+
+
+def test_func_return_right_len():
+    assert len(get_coin_combination(123)) == 4
+
+
+def test_fun_can_convert_all_values():
+    assert 0 not in get_coin_combination(41)
+
+
+@pytest.mark.parametrize(
+    "cents, result",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2])
+    ]
+)
+def test_big_parametrize_daddy(cents, result):
+    assert (
+            get_coin_combination(cents) == result
+    )
