391264255b057c5a61f7fd2c680e1536aee3e37a
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-05-24 22:47:25 +0300

    .

--- app/test_main.py
+++ app/test_main.py
@@ -3,14 +3,38 @@ import pytest
 from app.main import get_coin_combination
 
 
+def test_func_return_list():
+    assert isinstance(get_coin_combination(123), list)
+
+
+def test_func_return_right_len():
+    assert len(get_coin_combination(123)) == 4
+
+
+def test_fun_can_convert_all_values():
+    assert 0 not in get_coin_combination(41)
+
+
+def test_fun_can_convert_all_values():
+    assert get_coin_combination(40)[1] == 1
+    assert get_coin_combination(40)[2] == 1
+    assert get_coin_combination(40)[3] == 1
+
+
 @pytest.mark.parametrize(
     "cents, result",
     [
-        pytest.param(0, [0, 0, 0, 0]),
-        pytest.param(4, [4, 0, 0, 0]),
-        pytest.param(5, [0, 1, 0, 0]),
-        pytest.param(9, [4, 1, 0, 0])
-    ]
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2])
+    ],
+    ids=["coins -> penny",
+         "coins -> penny + nickel",
+         "coins -> 2 pennies + nickel + dime",
+         "coins -> quarters"]
 )
-def test_returns_smallest_combination(cents: int, result: list) -> None:
-    assert get_coin_combination(cents) == result
+def test_func_should_return_expected_results(cents, result):
+    assert (
+            get_coin_combination(cents) == result
+    )
