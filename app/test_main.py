6945f2ab0b668e70a524c24be65131cb20d69c57
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-05-24 22:55:08 +0300

    flake

--- app/test_main.py
+++ app/test_main.py
@@ -3,19 +3,19 @@ import pytest
 from app.main import get_coin_combination
 
 
-def test_func_return_list():
+def test_func_return_list() -> None:
     assert isinstance(get_coin_combination(0), list)
 
 
-def test_func_return_right_len():
+def test_func_return_right_len() -> None:
     assert len(get_coin_combination(0)) == 4
 
 
-def test_fun_can_convert_all_values():
+def test_all_coins() -> None:
     assert 0 not in get_coin_combination(41)
 
 
-def test_fun_can_convert_all_values():
+def test_fun_can_convert_all_values() -> None:
     assert get_coin_combination(40)[1] == 1
     assert get_coin_combination(40)[2] == 1
     assert get_coin_combination(40)[3] == 1
@@ -34,7 +34,7 @@ def test_fun_can_convert_all_values():
          "coins -> 2 pennies + nickel + dime",
          "coins -> quarters"]
 )
-def test_func_should_return_expected_results(cents, result):
-    assert (
-            get_coin_combination(cents) == result
-    )
+def test_func_should_return_expected_results(cents: int,
+                                             result: list[int]
+                                             ) -> None:
+    assert get_coin_combination(cents) == result
