7e62b03496193ae8a29b18d9f65c256537218432
Tarasidze <dtar84@ukr.net>
2023-06-14 15:54:16 +0300

    Solution 1.01

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,24 @@
+import pytest
+
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+class TestGetCoinCombination:
+    @pytest.mark.parametrize(
+        "value, exp_result",
+        [
+            pytest.param(0, [0, 0, 0, 0],
+                         id="Should return empty list when value is zero"),
+            pytest.param(1, [1, 0, 0, 0],
+                         id="Should return only one number in list"),
+            pytest.param(6, [1, 1, 0, 0],
+                         id="test six cents"),
+            pytest.param(17, [2, 1, 1, 0],
+                         id="test seventeen cents"),
+            pytest.param(50, [0, 0, 0, 2],
+                         id="test seventeen cents"),
+        ]
+    )
+    def test_different_values(self, value: int, exp_result: list) -> None:
+        assert get_coin_combination(value) == exp_result
