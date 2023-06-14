9eb422ebf99a4e2a41ad343f749e08c7b9d8c566
Alex Yatsun <starcrafter4444@gmail.com>
2023-06-14 16:21:13 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,33 @@
+import pytest
+
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents,expected_coins",
+    [
+        pytest.param(
+            0, [0, 0, 0, 0],
+            id="0 cents should be equal to list with all 0"
+        ),
+        pytest.param(
+            1, [1, 0, 0, 0],
+            id="0 cents should be equal to list with all 0"
+        ),
+        pytest.param(
+            6, [1, 1, 0, 0],
+            id="0 cents should be equal to list with all 0"
+        ),
+        pytest.param(
+            17, [2, 1, 1, 0],
+            id="0 cents should be equal to list with all 0"
+        ),
+        pytest.param(
+            50, [0, 0, 0, 2],
+            id="0 cents should be equal to list with all 0"
+        ),
+    ]
+)
+def test_coin_combination(cents: int, expected_coins: list[int]) -> None:
+    assert get_coin_combination(cents) == expected_coins
