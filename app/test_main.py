7a3609bcfab68ffee6922e827cc51ca01cb614e7
Oleksii Kolii <igravkompik@gmail.com>
2023-06-14 12:28:09 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,21 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_should_return_zero_when_no_coins() -> None:
+    assert get_coin_combination(0) == [0, 0, 0, 0]
+
+
+def test_should_count_cents() -> None:
+    assert get_coin_combination(4) == [4, 0, 0, 0]
+
+
+def test_should_count_nickels() -> None:
+    assert get_coin_combination(6) == [1, 1, 0, 0]
+
+
+def test_should_count_dimes() -> None:
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+
+
+def test_should_count_quarters() -> None:
+    assert get_coin_combination(26) == [1, 0, 0, 1]
