2eb2eeea359c3c161d273f9be86eed88a194b481
Vlad Larin <lordgadyka71@gmail.com>
2023-06-20 23:45:11 +0300

    develop

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,23 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+class TestGetCoinCombination:
+    @pytest.mark.parametrize(
+        "input_cents,output_coins",
+        [
+            (25, [0, 0, 0, 1]),
+            (10, [0, 0, 1, 0]),
+            (5, [0, 1, 0, 0]),
+            (1, [1, 0, 0, 0]),
+            (41, [1, 1, 1, 1]),
+            (82, [2, 1, 0, 3])
+        ]
+    )
+    def test_should_calculate_smallest_possible_number_of_coins(
+            self,
+            input_cents,
+            output_coins
+    ) -> None:
+        assert get_coin_combination(input_cents) == output_coins
