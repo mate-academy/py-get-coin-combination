02229f11b44b2759c894546ef1b18503e233be83
stepband <vladshvik@gmail.com>
2023-06-28 22:26:20 +0300

    Minor changes with flake8

--- app/test_main.py
+++ app/test_main.py
@@ -1,23 +1,23 @@
 import pytest
 from app.main import get_coin_combination
+from typing import List
 
 
 class TestCoinCombination:
     @pytest.mark.parametrize(
         "cents, expected_coins",
         [
-            pytest.param(1, [1, 0, 0, 0],
-            id= "Test pennies"),
+            pytest.param(1, [1, 0, 0, 0], id="Test pennies"),
             pytest.param
-            (6, [1, 1, 0, 0],
-             id="Test penny and nickel"),
+            (6, [1, 1, 0, 0], id="Test penny and nickel"),
             pytest.param
-            (17, [2, 1, 1, 0],
-             id="Test pennies, nickel and dime"),
+            (17, [2, 1, 1, 0], id="Test pennies, nickel and dime"),
             pytest.param
-            (50, [0, 0, 0, 2],
-             id="Test quarters"),
+            (50, [0, 0, 0, 2], id="Test quarters"),
         ]
     )
-    def test_get_coin_combination(self, cents, expected_coins):
+    def test_get_coin_combination(
+            self,
+            cents: int,
+            expected_coins: List[int]) -> None:
         assert get_coin_combination(cents) == expected_coins
