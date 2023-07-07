1bd757c028d098731409319188a8a6a95357ca48
Bohdan Liutko <luitko007@gmail.com>
2023-07-07 04:50:32 +0000

    Solutio

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,25 @@
+from __future__ import annotations
 from app.main import get_coin_combination
+import pytest
+
+def test_should_return_type_list() -> None:
+    assert (
+        isinstance(get_coin_combination(23), list) is True
+    ), "Function 'get_coin_combination' should return list"
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "coins_num,result",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
+        (66, [1, 1, 1, 2])
+    ]
+)
+def test_get_coin_combination(
+        coins_num: int,
+        result: list[int]
+) -> None:
+    assert get_coin_combination(coins_num) == result
