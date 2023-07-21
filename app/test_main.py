9a5d44fe690f31f5224e5e4ed0bbbd152c44362f
Volodymyr Yerokhov <kavbian@gmail.com>
2023-07-21 18:02:37 +0300

    'flake8'

--- app/test_main.py
+++ app/test_main.py
@@ -13,5 +13,5 @@ from app.main import get_coin_combination
         (50, [0, 0, 0, 2])
     ]
 )
-def test_get_coin_combination(cents: int, result: list[int]):
+def test_get_coin_combination(cents: int, result: list[int]) -> None:
     assert get_coin_combination(cents) == result
