952a398dca9cd968a8913fabf8b8721c18c0bd01
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-07-23 23:11:45 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,17 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cent, expected",
+    [
+        (len(get_coin_combination(1)), 4),
+        (get_coin_combination(1), [1, 0, 0, 0]),
+        (get_coin_combination(6), [1, 1, 0, 0]),
+        (get_coin_combination(17), [2, 1, 1, 0]),
+        (get_coin_combination(50), [0, 0, 0, 2]),
+    ]
+)
+def test_coin_combination(cent, expected) -> None:
+    assert cent == expected
