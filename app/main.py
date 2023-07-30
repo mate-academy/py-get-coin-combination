c2b26af111882c2aaf712308efc64b574af8fc57
Marina <marishka28023@gmail.com>
2023-07-30 12:26:04 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 def get_coin_combination(cents: int) -> list:
-    values = [1, 5, 10, 25]
+    values = [25, 10, 5, 1]
     coins = [0, 0, 0, 0]
 
-    for i in range(3, -1, -1):
+    for i in range(4):
         coins[i] = cents // values[i]
         cents -= coins[i] * values[i]
 
     return coins
+
+
+print(get_coin_combination(130))
--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,27 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_get_coin_combination() -> None:
+    try:
+        assert get_coin_combination(1) == [1, 0, 0, 0]  # 1 пенні
+        print("Ok!")
+    except:
+        print("Not ok!")
+    try:
+        assert get_coin_combination(6) == [1, 1, 0, 0]  # 1 пенні + 1 нікель
+        print("Ok!")
+    except:
+        print("Not ok!")
+    try:
+        assert get_coin_combination(17) == [2, 1, 1, 0]  # 2 пенні + 1 нікель + 1 дайм
+        print("Ok!")
+    except:
+        print("Not ok!")
+    try:
+        assert get_coin_combination(50) == [0, 0, 0, 2]  # 2 кварталі
+        print("Ok!")
+    except:
+        print("Not ok!")
+
+
+test_get_coin_combination()
