05de747171142bbd43b691ed2a6f737848826591
niksgawrilenko <90038040+niksgawrilenko@users.noreply.github.com>
2023-08-07 14:29:41 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,40 @@
 from app.main import get_coin_combination
+import pytest
 
 
-# write your tests here
+def test_return_type() -> None:
+    assert isinstance(get_coin_combination(1), list)
+
+
+@pytest.mark.parametrize(
+    "cents, expected_result",
+    [
+        pytest.param(
+            0,
+            [0, 0, 0, 0],
+            id="should return zero"
+        ),
+        pytest.param(
+            1,
+            [1, 0, 0, 0],
+            id="should return 1 penny"
+        ),
+        pytest.param(
+            6,
+            [1, 1, 0, 0],
+            id="should convert to nickel correctly"
+        ),
+        pytest.param(
+            17,
+            [2, 1, 1, 0],
+            id="should convert to dime correctly"
+        ),
+        pytest.param(
+            50,
+            [0, 0, 0, 2],
+            id="should convert to quarter correctly"
+        ),
+    ]
+)
+def test_get_coin_combination(cents: int, expected_result: list) -> None:
+    assert get_coin_combination(cents) == expected_result
