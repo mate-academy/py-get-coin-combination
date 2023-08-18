7301693e26b673371d42d7c102a583c387e7d367
Vladyslav Shepilov <shepsterya@gmail.com>
2023-08-18 17:13:05 +0300

    resloved

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,22 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "amount, expected",
+    (
+        pytest.param(0, [0, 0, 0, 0], id="Zero"),
+        pytest.param(4, [4, 0, 0, 0], id="Pennie higher bound"),
+        pytest.param(5, [0, 1, 0, 0], id="Nickel lower bound"),
+        pytest.param(9, [4, 1, 0, 0], id="Nickel higher bound"),
+        pytest.param(10, [0, 0, 1, 0], id="Dime lower bound"),
+        pytest.param(19, [4, 1, 1, 0], id="Dime higher bound"),
+        pytest.param(25, [0, 0, 0, 1], id="Quarter lower bound"),
+        pytest.param(44, [4, 1, 1, 1], id="All fields count"),
+        pytest.param(1019, [4, 1, 1, 40], id="Huge value")
+    )
+)
+def test_get_coin_combination(amount: int, expected: list) -> None:
+
+    assert get_coin_combination(amount) == expected
