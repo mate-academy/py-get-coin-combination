5169917539c2d7b00e00f95635627ef974fcf1e3
Olha Davydova <o.chookity@gmail.com>
2023-08-22 16:20:06 +0300

    Implement tests for get_coin_combination function

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,24 @@
+import pytest as pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents,expected_result",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (52, [2, 0, 0, 2]),
+    ],
+    ids=[
+        "0 cents should return 0 coins",
+        "1 cent should return 1 penny",
+        "6 cents should return 1 penny and 1 nickel",
+        "17 cents should return 2 penny, 1 nickel and 1 dime",
+        "52 cents should return 2 penny and 2 quarters",
+    ]
+)
+def test_coin_combination(cents: int, expected_result: list[int]) -> None:
+    assert (get_coin_combination(cents) == expected_result)
