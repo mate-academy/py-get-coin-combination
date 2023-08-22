8dfbe339ab18589ae48d70fc00b3062f83041341
Yeva <yevabrookman@gmail.com>
2023-08-22 15:01:51 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,22 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+class TestCoinCombination:
+    @pytest.mark.parametrize(
+        "cents, expected_result",
+        [
+            pytest.param(1, [1, 0, 0, 0], id="Test for one cents"),
+            pytest.param(6, [1, 1, 0, 0], id="Test for 1 nickel and 1 cent"),
+            pytest.param(
+                17, [2, 1, 1, 0], id="Test for 2 pennies, nickel and dime"
+            ),
+            pytest.param(50, [0, 0, 0, 2], id="Test 2 quarters"),
+        ],
+    )
+    def test_check_correct_convert(
+        self, cents: int, expected_result: list
+    ) -> None:
+        result = get_coin_combination(cents)
+        assert result == expected_result
