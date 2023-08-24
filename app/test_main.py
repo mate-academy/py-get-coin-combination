65244c4ba330f196e88eea3ebb5b2dc8fa66dc7d
Olexander Bryl <edlrian814@gmail.com>
2023-08-24 19:21:34 +0300

    develop

--- app/test_main.py
+++ app/test_main.py
@@ -7,6 +7,11 @@ import pytest
     "cents, expected_result",
     [
         pytest.param(
+            -5,
+            [0, 0, 0, 0],
+            id="should treat negative numbers as zero"
+        ),
+        pytest.param(
             0,
             [0, 0, 0, 0],
             id="should return 4 zeros when 0 cents given"
@@ -20,20 +25,3 @@ import pytest
 )
 def test_should_convert_correctly(cents: int, expected_result: list) -> None:
     assert get_coin_combination(cents) == expected_result
-
-
-@pytest.mark.parametrize(
-    "cents, expected_error",
-    [
-        pytest.param(
-            -20,
-            ValueError,
-            id="should not accept negative values"
-        )
-    ]
-)
-def test_should_raise_type_error_when_string_given(
-        cents: str, expected_error: Exception
-) -> None:
-    with pytest.raises(expected_error):
-        get_coin_combination(cents)
