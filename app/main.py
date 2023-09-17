b3b33df9f8a66b06d8b464727db252ba840722a8
Vitalii Zhabskii <vitalik15031996@gmail.com>
2023-09-17 14:14:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 def get_coin_combination(cents: int) -> list:
+    if cents < 0:
+        raise ValueError("Entered negative function")
+
     values = [1, 5, 10, 25]
     coins = [0, 0, 0, 0]
 
     for i in range(3, -1, -1):
         coins[i] = cents // values[i]
-        cents -= coins[i] * values[i]
+        cents %= values[i]
 
     return coins
--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,28 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize("cents, expected_coins", [
+    (0, [0, 0, 0, 0]),
+    (1, [1, 0, 0, 0]),
+    (5, [0, 1, 0, 0]),
+    (10, [0, 0, 1, 0]),
+    (25, [0, 0, 0, 1]),
+    (30, [0, 1, 0, 1]),
+    (41, [1, 1, 1, 1]),
+    (99, [4, 0, 2, 3]),
+    (123, [3, 0, 2, 4]),
+    (50, [0, 0, 5, 0]),
+])
+def test_get_coin_combination(cents, expected_coins):
+    assert get_coin_combination(cents) == expected_coins
+
+
+def test_negative_cents():
+    with pytest.raises(ValueError):
+        get_coin_combination(-1)
+
+
+if __name__ == "__main__":
+    pytest.main()
