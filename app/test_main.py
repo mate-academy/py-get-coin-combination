09473e4b4188fc9b0ee4295fcfa55cb9471a65b7
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-17 14:29:50 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -13,7 +13,7 @@ from app.main import get_coin_combination
     (41, [1, 1, 1, 1]),
     (99, [4, 0, 2, 3]),
     (123, [3, 0, 2, 4]),
-    (50, [0, 0, 5, 0]),
+    (50, [0, 0, 0, 2]),
 ])
 def test_get_coin_combination(cents: int, expected_coins: int) -> None:
     assert get_coin_combination(cents) == expected_coins
