5b2cf1daefa59ee54bb6b80caf45baeaaf0f5aef
Vadym Tiron <tironvadim1@gmail.com>
2023-10-02 18:05:04 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -4,17 +4,21 @@ import pytest
 from app.main import get_coin_combination
 
 
-def test_get_coin_combination_1_cent() -> None:
+def test_get_coin_combination_with_1_cent() -> None:
     assert get_coin_combination(1) == [1, 0, 0, 0]
 
 
-def test_get_coin_combination_6_cents() -> None:
+def test_get_coin_combination_with_6_cents() -> None:
     assert get_coin_combination(6) == [1, 1, 0, 0]
 
 
-def test_get_coin_combination_11_cents() -> None:
-    assert get_coin_combination(11) == [1, 0, 1, 0]
+def test_get_coin_combination_with_17_cents() -> None:
+    assert get_coin_combination(17) == [2, 1, 1, 0]
 
 
-def test_get_coin_combination_26_cents() -> None:
-    assert get_coin_combination(26) == [1, 0, 0, 1]
+def test_get_coin_combination_with_50_cents() -> None:
+    assert get_coin_combination(50) == [0, 0, 0, 2]
+
+
+def test_get_coin_combination_with_zero_cents() -> None:
+    assert get_coin_combination(0) == [0, 0, 0, 0]
