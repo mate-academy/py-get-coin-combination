9874dece211c3b618b730cc9709c36baf1e8fea8
Vadym Tiron <tironvadim1@gmail.com>
2023-10-02 19:24:46 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -9,11 +9,6 @@ from app.main import get_coin_combination
     (6, [1, 1, 0, 0]),  # 1 penny + 1 nickel
     (17, [2, 1, 1, 0]),  # 2 pennies + 1 nickel + 1 dime
     (50, [0, 0, 0, 2]),  # 2 quarters
-    (0, [0, 0, 0, 0]),  # No coins
-    (25, [0, 0, 0, 1]),  # 2 dimes
-    (100, [0, 0, 0, 4]),  # 4 quarters
-    (7, [2, 1, 0, 0]),  # 2 pennies + 1 nickel
-    (99, [4, 0, 2, 3]),  # 4 pennies + 2 dimes + 3 quarters
 ])
 def test_get_coin_combination(cents, expected_result) -> None:
     assert get_coin_combination(cents) == expected_result
