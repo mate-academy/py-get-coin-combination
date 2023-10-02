33811c9c4ad03994e2f1990d4db2fb798debc59e
Vadym Tiron <tironvadim1@gmail.com>
2023-10-02 18:16:13 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -4,21 +4,21 @@ import pytest
 from app.main import get_coin_combination
 
 
-def test_get_coin_combination_with_1_cent() -> None:
-    assert get_coin_combination(1) == [1, 0, 0, 0]
+@pytest.mark.parametrize("cents, expected_result", [
+    (1, [1, 0, 0, 0]),  # 1 penny
+    (6, [1, 1, 0, 0]),  # 1 penny + 1 nickel
+    (17, [2, 1, 1, 0]),  # 2 pennies + 1 nickel + 1 dime
+    (50, [0, 0, 0, 2]),  # 2 quarters
+    (0, [0, 0, 0, 0]),  # No coins
+    (25, [0, 0, 0, 1]),  # 2 dimes
+    (100, [0, 0, 0, 4]),  # 4 quarters
+    (7, [2, 1, 0, 0]),  # 2 pennies + 1 nickel
+    (99, [4, 0, 2, 3]),  # 4 pennies + 2 dimes + 3 quarters
+])
+def test_get_coin_combination(cents, expected_result) -> None:
+    assert get_coin_combination(cents) == expected_result
 
 
-def test_get_coin_combination_with_6_cents() -> None:
-    assert get_coin_combination(6) == [1, 1, 0, 0]
-
-
-def test_get_coin_combination_with_17_cents() -> None:
-    assert get_coin_combination(17) == [2, 1, 1, 0]
-
-
-def test_get_coin_combination_with_50_cents() -> None:
-    assert get_coin_combination(50) == [0, 0, 0, 2]
-
-
-def test_get_coin_combination_with_zero_cents() -> None:
-    assert get_coin_combination(0) == [0, 0, 0, 0]
+def test_large_cents() -> None:
+    result = get_coin_combination(10 ** 9)
+    assert result[3] == 40000000
