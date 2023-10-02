ca1cf8ef82effb4ab287e1c31cc43e20b9b7d071
Vadym Tiron <tironvadim1@gmail.com>
2023-10-02 17:34:08 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -12,18 +12,9 @@ def test_get_coin_combination_6_cents() -> None:
     assert get_coin_combination(6) == [1, 1, 0, 0]
 
 
-def test_get_coin_combination_17_cents() -> None:
-    assert get_coin_combination(17) == [2, 1, 1, 0]
+def test_get_coin_combination_11_cents() -> None:
+    assert get_coin_combination(11) == [1, 0, 1, 0]
 
 
-def test_get_coin_combination_50_cents() -> None:
-    assert get_coin_combination(50) == [0, 0, 0, 2]
-
-
-def test_get_coin_combination_large_amount() -> None:
-    assert get_coin_combination(9999) == [4, 0, 2, 399]
-
-
-def test_get_coin_combination_invalid_type() -> None:
-    with pytest.raises(TypeError):
-        get_coin_combination("gogo")
+def test_get_coin_combination_26_cents() -> None:
+    assert get_coin_combination(26) == [1, 0, 0, 1]
