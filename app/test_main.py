3d69f638ad6186b8d72ec09d5b38f576cf234c95
Vladyslav Shatrovskyi <shatrovskyi.vlad@gmail.com>
2023-10-19 16:49:57 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,19 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, result",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
+    ]
+)
+def test_conversion_of_cents(cents: int, result: list) -> None:
+    assert(
+        get_coin_combination(cents) == result
+    ), ("Conversion of cents should equal to {result}."
+        "Where [penny, nickel, dime, quarters]")
