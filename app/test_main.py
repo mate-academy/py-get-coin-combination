7c1b2b6da9c0ac0ecfcfae3295a768fb05dbe120
Vyacheslav Vasylenko <vvg04021968@gmail.com>
2023-10-19 16:57:06 +0300

    'Solution_get-coin-combination'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,35 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents,expected_coin_combination",
+    [
+        pytest.param(
+            1, [1, 0, 0, 0],
+            id="get only pennies"
+        ),
+        pytest.param(
+            5, [0, 1, 0, 0],
+            id="get only nickels"
+        ),
+        pytest.param(
+            10, [0, 0, 1, 0],
+            id="get only dimes"
+        ),
+        pytest.param(
+            25, [0, 0, 0, 1],
+            id="get only quarters"
+        ),
+        pytest.param(
+            41, [1, 1, 1, 1],
+            id="get every coin"
+        ),
+    ]
+)
+def test_get_human_age(
+        cents: int,
+        expected_coin_combination: list
+) -> None:
+    assert get_coin_combination(cents) == expected_coin_combination
