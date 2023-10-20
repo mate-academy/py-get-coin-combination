11dd80ac41f6a3acba635ec8138522b14708126e
Andrii Silenko <andrewformicr@gmail.com>
2023-10-20 09:21:56 +0300

    flake8 fixed

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,21 @@
 from app.main import get_coin_combination
 
 
-def test_with_no_coins():
+def test_with_no_coins() -> None:
     assert get_coin_combination(0) == [0, 0, 0, 0]
 
 
-def test_with_pennies():
+def test_with_pennies() -> None:
     assert get_coin_combination(3) == [3, 0, 0, 0]
 
 
-def test_with_pennies_and_nickles():
+def test_with_pennies_and_nickles() -> None:
     assert get_coin_combination(7) == [2, 1, 0, 0]
 
 
-def test_with_pennies_nickles_and_dimes():
+def test_with_pennies_nickles_and_dimes() -> None:
     assert get_coin_combination(17) == [2, 1, 1, 0]
 
 
-def test_with_pennies_nickles_dimes_and_quarters():
+def test_with_pennies_nickles_dimes_and_quarters() -> None:
     assert get_coin_combination(43) == [3, 1, 1, 1]
