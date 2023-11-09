2954064176f72f6c2e8fff108f6497d3245887bd
Illya Maznitskiy <comercaleuros@gmail.com>
2023-11-09 19:21:48 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,23 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, excepted",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
+        (0, [0, 0, 0, 0]),
+        (25, [0, 0, 0, 1]),
+        (11, [1, 0, 1, 0]),
+        (30, [0, 1, 0, 1])
+    ]
+)
+def test_coin_combination_with_different_numbers(
+        cents: int,
+        excepted: list
+) -> None:
+    assert get_coin_combination(cents) == excepted
