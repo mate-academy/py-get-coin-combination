539b846fb4a46e0132f532e02bf3f59c168c736a
Alex <oleksa.shcherbakov@gmail.com>
2023-11-16 18:02:35 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,18 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "amount, result",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1 ,1, 0]),
+        (50, [0, 0,0, 2])
+    ]
+)
+def test_coin_combination(
+        amount: int,
+                result: list
+) -> None:
+    assert get_human_age(amount) == result
