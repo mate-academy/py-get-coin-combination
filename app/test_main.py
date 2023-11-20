696c2465c7c44bd4d552c5037df8802b9d7453e5
Yuliia Dudych <juliadudych@gmail.com>
2023-11-20 12:55:53 +0200

    Add tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,15 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "coins,result",
+    [
+        pytest.param(1, [1, 0, 0, 0]),
+        pytest.param(6, [1, 1, 0, 0]),
+        pytest.param(17, [2, 1, 1, 0]),
+        pytest.param(50, [0, 0, 0, 2]),
+    ]
+)
+def test_returns_correct_coin_combination(coins: int, result: list) -> None:
+    assert get_coin_combination(coins) == result
