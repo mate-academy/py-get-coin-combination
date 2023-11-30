637b7b232314128bfa2c5176acda125d803a52e0
Vlad <mr.darmstadtium@gmail.com>
2023-11-30 19:36:36 +0100

    pass all tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,25 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "coins,result",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (10, [0, 0, 1, 0]),
+        (17, [2, 1, 1, 0]),
+        (100, [0, 0, 0, 4]),
+        (25, [0, 0, 0, 1]),
+        (5, [0, 1, 0, 0]),
+        (84, [4, 1, 0, 3]),
+        (3278, [3, 0, 0, 131])
+
+    ]
+)
+def test_can_get_coin_combination(coins: int, result: list) -> None:
+    assert(
+        get_coin_combination(coins) == result
+    ), f"Function get_coin_combination should return" \
+       f" {result} for {coins}"
