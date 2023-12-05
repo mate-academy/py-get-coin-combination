3f29675c4636e8003b5493d83d963a5e22f3d0ab
Anton Konyk <antonkonyk.com>
2023-12-05 19:37:44 +0100

    'Solution_05122023'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,63 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, result",
+    [
+        pytest.param(
+            0,
+            [0, 0, 0, 0],
+            id="should return the wright result: [0, 0, 0, 0]"
+        ),
+        pytest.param(
+            1,
+            [1, 0, 0, 0],
+            id="should return the wright result: [1, 0, 0, 0]"
+        ),
+        pytest.param(
+            6,
+            [1, 1, 0, 0],
+            id="should return the wright result: [1, 1, 0, 0]"
+        ),
+        pytest.param(
+            16,
+            [1, 1, 1, 0],
+            id="should return the wright result: [1, 1, 1, 0]"
+        ),
+        pytest.param(
+            41,
+            [1, 1, 1, 1],
+            id="should return the wright result: [1, 1, 1, 1]"
+        ),
+        pytest.param(
+            100041,
+            [1, 1, 1, 4001],
+            id="should return the wright result: [1, 1, 1, 4001]"
+        ),
+    ]
+)
+def test_calculation_correctly(
+        cents: int,
+        result: list[int]
+) -> None:
+    assert get_coin_combination(cents) == result
+
+
+@pytest.mark.parametrize(
+    "cents, expected_error",
+    [
+        pytest.param(
+            "2",
+            TypeError,
+            id="should raise error if cents is string type"
+        ),
+
+    ]
+)
+def test_raising_error_correctly(
+        cents: int | str,
+        expected_error: Exception,
+) -> None:
+    with pytest.raises(expected_error):
+        get_coin_combination(cents)
