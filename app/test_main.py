006652990a15f055b30adfc2144145a03027d550
Vishnevskyi Dmytro <vishneman@gmail.com>
2023-12-06 18:21:37 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,18 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "coins, expected_result",
+    [
+        pytest.param(1, [1, 0, 0, 0]),
+        pytest.param(6, [1, 1, 0, 0]),
+        pytest.param(17, [2, 1, 1, 0]),
+        pytest.param(50, [0, 0, 0, 2]),
+    ]
+)
+def test_returns_correct_coin_combination(
+        coins: int,
+        expected_result: list
+) -> None:
+    assert get_coin_combination(coins) == expected_result
