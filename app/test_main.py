e1b5ffe4240675afa8e833fde250408e6cc2a81d
Mykhailo Slavsky <slavskij2017@gmail.com>
2023-12-14 12:09:20 +0200

    Fix Solution

--- app/test_main.py
+++ app/test_main.py
@@ -11,10 +11,10 @@ class TestMain:
             pytest.param(6, [1, 1, 0, 0]),
             pytest.param(17, [2, 1, 1, 0]),
             pytest.param(50, [0, 0, 0, 2])
-         ]
+        ]
     )
     def test_get_coin_combination(
             self,
-            coin,
-            list_of_cents) -> None:
+            coin: int,
+            list_of_cents: list) -> None:
         assert get_coin_combination(coin) == list_of_cents
