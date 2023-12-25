b431df1296b3fa4b1f91b4318553778ec0426622
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2023-12-25 09:53:36 +0200

    ready solution!

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,25 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_one_cent() -> None:
+    assert (
+        get_coin_combination(1) == [1, 0, 0, 0]
+    ), "Should return 1 penny for 1 cent"
+
+
+def test_more_than_five_cents() -> None:
+    assert (
+        get_coin_combination(6) == [1, 1, 0, 0]
+    ), "Should return 1 penny and 1 nickel for 6 cent's"
+
+
+def test_more_than_15_cents() -> None:
+    assert (
+        get_coin_combination(17) == [2, 1, 1, 0]
+    ), "Should return 2 penny 1 nickel and 1 dime for 17 cent"
+
+
+def test_50_cents() -> None:
+    assert (
+        get_coin_combination(50) == [0, 0, 0, 2]
+    ), "Should return 2 quarters for 50 cent"
