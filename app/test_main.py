8ff1003e9351d7f063e479b2d7c68de263940ce3
Oleksii Isaev <lesisv@gmail.com>
2023-12-27 19:39:30 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,25 @@
+import pytest
+
 from app.main import get_coin_combination
+from typing import List
 
 
-# write your tests here
+class TestAddCssClass:
+    @pytest.mark.parametrize(
+        "coin, combination", [
+            (0, [0, 0, 0, 0]),
+            (1, [1, 0, 0, 0]),
+            (4, [4, 0, 0, 0]),
+            (5, [0, 1, 0, 0]),
+            (9, [4, 1, 0, 0]),
+            (10, [0, 0, 1, 0]),
+            (24, [4, 0, 2, 0]),
+            (25, [0, 0, 0, 1])
+        ]
+    )
+    def test_get_human_age(
+            self,
+            coin: int,
+            combination: List[int]
+    ) -> None:
+        assert get_coin_combination(coin) == combination
