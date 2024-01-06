067001d0255e89458b4fc639aad8892fdea4ef4a
Volodymyr Yablonskyi <wowa2807@gmail.com>
2024-01-06 20:14:37 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,21 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_get_coin_combination_single_penny() -> None:
+    result = get_coin_combination(1)
+    assert result[0] == 1
+
+
+def test_get_coin_combination_penny_and_nickel() -> None:
+    result = get_coin_combination(6)
+    assert result[0] == 1 and result[1] == 1
+
+
+def test_get_coin_combination_multiple_coins() -> None:
+    result = get_coin_combination(17)
+    assert result[0] == 2 and result[1] == 1 and result[2] == 1
+
+
+def test_get_coin_combination_quarters_only() -> None:
+    result = get_coin_combination(50)
+    assert result[3] == 2
