7ed48059589542b502bc67ffc7cb926ccbd34593
AlexHCom <alexhcomgrit@gmail.com>
2024-01-17 01:15:44 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -12,11 +12,11 @@ class TestGetCoinCombination:
             (17, [2, 1, 1, 0]),      # 2 pennies + 1 nickel + 1 dime
             (50, [0, 0, 0, 2]),      # 2 quarters
             (99, [4, 0, 2, 3]),      # 4 pennies + 2 dimes + 3 quarters
-            (100, [0, 0, 0, 4]),     # Corrected: 4 quarters (1 dime was missing)
+            (100, [0, 0, 0, 4]),
             (125, [0, 0, 0, 5]),     # 5 quarters
         ],
     )
-    def test_get_coin_combination(self, cents: int, expected_result: list) -> None:
+    def test_get_coin_combination(self, cents: int, expected_result: list) -> None: # Noqa E501
         assert get_coin_combination(cents) == expected_result
 
     def test_get_coin_combination_negative(self) -> None:
