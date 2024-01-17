47808a85db0a84d172a30ea7a0639df47187a79a
AlexHCom <alexhcomgrit@gmail.com>
2024-01-18 01:08:18 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,28 @@
-import pytest
 from app.main import get_coin_combination
 
 
-class TestGetCoinCombination:
-    @pytest.mark.parametrize(
-        "cents, expected_result",
-        [
-            (0, [0, 0, 0, 0]),       # 0 cents
-            (1, [1, 0, 0, 0]),       # 1 penny
-            (6, [1, 1, 0, 0]),       # 1 penny + 1 nickel
-            (17, [2, 1, 1, 0]),      # 2 pennies + 1 nickel + 1 dime
-            (50, [0, 0, 0, 2]),      # 2 quarters
-            (99, [4, 0, 2, 3]),      # 4 pennies + 2 dimes + 3 quarters
-            (100, [0, 0, 0, 4]),
-            (125, [0, 0, 0, 5]),     # 5 quarters
-        ],
-    )
-    def test_get_coin_combination(self, cents: int, expected_result: list) -> None: # Noqa E501
-        assert get_coin_combination(cents) == expected_result
+def test_get_coin_combination_pennies() -> None:
+    assert get_coin_combination(1) == [1, 0, 0, 0]  # 1 penny
+    assert get_coin_combination(3) == [3, 0, 0, 0]  # 3 pennies
+    assert get_coin_combination(10) == [10, 0, 0, 0]  # 10 pennies
 
-    def test_get_coin_combination_negative(self) -> None:
-        # Testing the negative case without monkeypatch
-        with pytest.raises(ValueError, match="Input value must be non-negative"): # Noqa E501
-            get_coin_combination(-1)
+
+def test_get_coin_combination_nickels() -> None:
+    assert get_coin_combination(5) == [0, 1, 0, 0]  # 1 nickel
+    assert get_coin_combination(15) == [0, 3, 0, 0]  # 3 nickels
+
+
+def test_get_coin_combination_dimes() -> None:
+    assert get_coin_combination(10) == [0, 0, 1, 0]  # 1 dime
+    assert get_coin_combination(25) == [0, 0, 2, 0]  # 2 dimes
+
+
+def test_get_coin_combination_quarters() -> None:
+    assert get_coin_combination(25) == [0, 0, 0, 1]  # 1 quarter
+    assert get_coin_combination(100) == [0, 0, 0, 4]  # 4 quarters
+
+
+def test_get_coin_combination_mixed() -> None:
+    assert get_coin_combination(6) == [1, 1, 0, 0]  # 1 penny + 1 nickel
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+    assert get_coin_combination(50) == [0, 0, 0, 2]
