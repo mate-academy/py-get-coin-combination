610df258a9dd45d923f7c89f731421762c892248
Dmytry-S <sviridoffd@gmail.com>
2024-01-22 22:08:46 +0200

    Implement coin-combination task.

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,30 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_should_return_type_list() -> None:
+    assert (
+        isinstance(get_coin_combination(23), list) is True
+    ), "Function should return list"
+
+
+@pytest.mark.parametrize(
+    "cents_int, cents_list",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (5, [0, 1, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (10, [0, 0, 1, 0]),
+        (100, [0, 0, 0, 4]),
+        (964, [4, 0, 1, 38]),
+        (4653, [3, 0, 0, 186]),
+        (78655, [0, 1, 0, 3146]),
+        (345875, [0, 0, 0, 13835]),
+    ],
+)
+def test_coin_combination(cents_int: int, cents_list: list) -> None:
+    assert get_coin_combination(cents_int) == cents_list, (
+        f"Function should return {cents_list} "
+        f"when value is {cents_int}"
+    )
