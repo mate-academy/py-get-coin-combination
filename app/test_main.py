5fb72faa62ee0e91ca99f026cb94b49b2a29ee1a
Maksym Nimych <maksimnimitch@gmail.com>
2024-01-23 16:12:42 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,48 @@
+import pytest
+
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, expected_result",
+    [
+        pytest.param(
+            0, [0, 0, 0, 0],
+            id="should return zeros when there is no any cent"
+        ),
+        pytest.param(
+            1, [1, 0, 0, 0],
+            id="should return [1, 0, 0, 0] when there is 1 cent"
+        ),
+        pytest.param(
+            6, [1, 1, 0, 0],
+            id="should return [1, 1, 0, 0] when there are 6 cents"
+        ),
+        pytest.param(
+            18, [3, 1, 1, 0],
+            id="should return [3, 1, 1, 0] when there are 18 cents"
+        ),
+        pytest.param(
+            44, [4, 1, 1, 1],
+            id="should return [4, 1, 1, 1] when there are 44 cents"
+        ),
+        pytest.param(
+            100, [0, 0, 0, 4],
+            id="should return [0, 0, 0, 4] when there are 100 cents"
+        ),
+        pytest.param(
+            124, [4, 0, 2, 4],
+            id="should return [4, 0, 2, 4] when there are 124 cents"
+        ),
+        pytest.param(
+            1248, [3, 0, 2, 49],
+            id="should return [3, 0, 2, 49] when there are 1248 cents"
+        )
+    ]
+)
+def test_return_combination_correctly(
+    cents: int,
+    expected_result: list
+) -> None:
+    assert get_coin_combination(cents) == expected_result
