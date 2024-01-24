d3ef0aa28e3253056889b5bf318be4678175e3eb
AlexHCom <alexhcomgrit@gmail.com>
2024-01-24 22:54:44 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,28 +1,44 @@
+import pytest
 from app.main import get_coin_combination
 
 
-def test_get_coin_combination_pennies() -> None:
-    assert get_coin_combination(1) == [1, 0, 0, 0]  # 1 penny
-    assert get_coin_combination(3) == [3, 0, 0, 0]  # 3 pennies
-    assert get_coin_combination(10) == [10, 0, 0, 0]  # 10 pennies
+@pytest.mark.parametrize("amount, expected_result", [
+    (1, [1, 0, 0, 0]),
+    (3, [3, 0, 0, 0]),
+    (10, [10, 0, 0, 0]),
+])
+def test_get_coin_combination_pennies(amount, expected_result) -> None:
+    assert get_coin_combination(amount) == expected_result
 
 
-def test_get_coin_combination_nickels() -> None:
-    assert get_coin_combination(5) == [0, 1, 0, 0]  # 1 nickel
-    assert get_coin_combination(15) == [0, 3, 0, 0]  # 3 nickels
+@pytest.mark.parametrize("amount, expected_result", [
+    (5, [0, 1, 0, 0]),
+    (15, [0, 3, 0, 0]),
+])
+def test_get_coin_combination_nickels(amount, expected_result) -> None:
+    assert get_coin_combination(amount) == expected_result
 
 
-def test_get_coin_combination_dimes() -> None:
-    assert get_coin_combination(10) == [0, 0, 1, 0]  # 1 dime
-    assert get_coin_combination(25) == [0, 0, 2, 0]  # 2 dimes
+@pytest.mark.parametrize("amount, expected_result", [
+    (10, [0, 0, 1, 0]),
+    (25, [0, 0, 2, 0]),
+])
+def test_get_coin_combination_dimes(amount, expected_result) -> None:
+    assert get_coin_combination(amount) == expected_result
 
 
-def test_get_coin_combination_quarters() -> None:
-    assert get_coin_combination(25) == [0, 0, 0, 1]  # 1 quarter
-    assert get_coin_combination(100) == [0, 0, 0, 4]  # 4 quarters
+@pytest.mark.parametrize("amount, expected_result", [
+    (25, [0, 0, 0, 1]),
+    (100, [0, 0, 0, 4]),
+])
+def test_get_coin_combination_quarters(amount, expected_result) -> None:
+    assert get_coin_combination(amount) == expected_result
 
 
-def test_get_coin_combination_mixed() -> None:
-    assert get_coin_combination(6) == [1, 1, 0, 0]  # 1 penny + 1 nickel
-    assert get_coin_combination(17) == [2, 1, 1, 0]
-    assert get_coin_combination(50) == [0, 0, 0, 2]
+@pytest.mark.parametrize("amount, expected_result", [
+    (6, [1, 1, 0, 0]),
+    (17, [2, 1, 1, 0]),
+    (50, [0, 0, 0, 2]),
+])
+def test_get_coin_combination_mixed(amount, expected_result) -> None:
+    assert get_coin_combination(amount) == expected_result
