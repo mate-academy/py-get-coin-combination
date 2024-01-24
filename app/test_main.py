9f1d3ac461d5157328d089a8e11b07b3710f74b6
barabarinov <barabarinov@gmail.com>
2024-01-24 23:26:38 +0200

    fixed annotation and long lines

--- app/test_main.py
+++ app/test_main.py
@@ -14,9 +14,13 @@ from app.main import get_coin_combination
     ids=[
         "1 cent should return 1 penny",
         "6 cents should return a combination of 1 penny + 2 nickel",
-        "17 cents should return a combination of 2 pennies + 1 nickel + 1 dime",
+        "17 cents should return a combination "
+        "of 2 pennies + 1 nickel + 1 dime",
         "50 cents should return a combination of 2 quarters",
     ]
 )
-def test_get_coin_combination(cents: int, expected_coin_combination) -> None:
+def test_get_coin_combination(
+    cents: int,
+    expected_coin_combination: list[int],
+) -> None:
     assert get_coin_combination(cents) == expected_coin_combination
