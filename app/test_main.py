b750a79f333cd7e3d40ac0e4e1e70361424332ea
AlexHCom <alexhcomgrit@gmail.com>
2024-01-24 23:03:50 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,43 +2,16 @@ import pytest
 from app.main import get_coin_combination
 
 
-@pytest.mark.parametrize("amount, expected_result", [
-    (1, [1, 0, 0, 0]),
-    (3, [3, 0, 0, 0]),
-    (10, [10, 0, 0, 0]),
-])
-def test_get_coin_combination_pennies(amount: int, expected_result: list[int]) -> None: # Noqa E501
-    assert get_coin_combination(amount) == expected_result
-
-
-@pytest.mark.parametrize("amount, expected_result", [
-    (5, [0, 1, 0, 0]),
-    (15, [0, 3, 0, 0]),
-])
-def test_get_coin_combination_nickels(amount: int, expected_result: list[int]) -> None: # Noqa E501
-    assert get_coin_combination(amount) == expected_result
-
-
-@pytest.mark.parametrize("amount, expected_result", [
-    (10, [0, 0, 1, 0]),
-    (25, [0, 0, 2, 0]),
-])
-def test_get_coin_combination_dimes(amount: int, expected_result: list[int]) -> None: # Noqa E501
-    assert get_coin_combination(amount) == expected_result
-
-
-@pytest.mark.parametrize("amount, expected_result", [
-    (25, [0, 0, 0, 1]),
-    (100, [0, 0, 0, 4]),
-])
-def test_get_coin_combination_quarters(amount: int, expected_result: list[int]) -> None: # Noqa E501
-    assert get_coin_combination(amount) == expected_result
-
-
-@pytest.mark.parametrize("amount, expected_result", [
-    (6, [1, 1, 0, 0]),
-    (17, [2, 1, 1, 0]),
-    (50, [0, 0, 0, 2]),
-])
-def test_get_coin_combination_mixed(amount: int, expected_result: list[int]) -> None: # Noqa E501
-    assert get_coin_combination(amount) == expected_result
+@pytest.mark.parametrize(
+    "cents, result",
+    [
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
+        (100, [0, 0, 0, 4])
+    ]
+)
+def test_check_input_value(cents: int, result: list) -> None:
+    assert (
+        get_coin_combination(cents) == result
+    ), f"{cents} cents is {result} coin."
