9ff1088640291d18dd0c73b29189d5b476f1fcad
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-01-27 11:30:04 +0200

    fix flake8

--- app/test_main.py
+++ app/test_main.py
@@ -17,9 +17,12 @@ from app.main import get_coin_combination
         "0 common sum should be convert to [0, 0, 0, 0]",
         "1 common sum should be convert to 1 penny [1, 0, 0, 0]",
         "6 common sum should be convert to 1 penny and 1 nickel [1, 1, 0, 0]",
-        "17 common sum should be convert to 2 penny and 1 nickel and 1 dimes [2, 1, 1, 0]",
+        "17 common sum should be convert "
+        "to 2 penny and 1 nickel and 1 dimes [2, 1, 1, 0]",
         "50 common sum should be convert to 2 quarters [0, 0, 0, 2]",
-        "67 common sum should be convert to 2 penny and 1 nickel and 1 dimes and 2 quarters [2, 1, 1, 2]",
+        "67 common sum should be convert "
+        "to 2 penny and 1 nickel and 1 dimes "
+        "and 2 quarters [2, 1, 1, 2]",
     ],
 )
 def test_get_coin_combination(
