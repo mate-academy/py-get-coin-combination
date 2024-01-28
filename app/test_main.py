f3951dfe6be4568a7e009bfe91ca11aa81314c7e
Sikach Illia <55293120+isikach@users.noreply.github.com>
2024-01-28 13:03:14 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -9,5 +9,5 @@ from app.main import get_coin_combination
     (17, [2, 1, 1, 0]),
     (50, [0, 0, 0, 2])
 ])
-def test_get_coin_combination(test_input, expected):
+def test_get_coin_combination(test_input: int, expected: list) -> None:
     assert get_coin_combination(test_input) == expected
