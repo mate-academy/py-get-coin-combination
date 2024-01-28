cea831f3f77661bb01826fecbb77ca58ff908522
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-01-28 23:37:56 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,14 +1,16 @@
-import pytest
-
 from app.main import get_coin_combination
 
 
-def test_get_coin_combination():
-    assert get_coin_combination(1) == [1, 0, 0, 0], "Test failed for input: 1"
-    assert get_coin_combination(6) == [1, 1, 0, 0], "Test failed for input: 6"
-    assert get_coin_combination(17) == [2, 1, 1, 0], "Test failed for input: 17"
-    assert get_coin_combination(50) == [0, 0, 0, 2], "Test failed for input: 50"
-
-    assert get_coin_combination(41) == [1, 1, 1, 1], "Test failed for input: 41"
-
-    assert get_coin_combination(100) == [0, 0, 0, 4], "Test failed for input: 100"
+def test_get_coin_combination() -> None:
+    assert get_coin_combination(1) == [1, 0, 0, 0], \
+        "Test failed for input: 1"
+    assert get_coin_combination(6) == [1, 1, 0, 0], \
+        "Test failed for input: 6"
+    assert get_coin_combination(17) == [2, 1, 1, 0], \
+        "Test failed for input: 17"
+    assert get_coin_combination(50) == [0, 0, 0, 2], \
+        "Test failed for input: 50"
+    assert get_coin_combination(41) == [1, 1, 1, 1], \
+        "Test failed for input: 41"
+    assert get_coin_combination(100) == [0, 0, 0, 4], \
+        "Test failed for input: 100"
