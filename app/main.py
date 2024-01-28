2a0075699f715474f3d23167fae354aac74d21be
Maksym Operchuk <operlast529@gmail.com>
2024-01-28 17:00:40 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ def get_coin_combination(cents: int) -> list:
     coins = [0, 0, 0, 0]
 
     for i in range(3, -1, -1):
-        coins[i] = cents // values[i]
-        cents -= coins[i] * values[i]
+        coins[i] = cents // values[i]  # 0 = 15 // 10
+        cents -= coins[i] * values[i]  #15 -= 0 * 25
 
     return coins
+
