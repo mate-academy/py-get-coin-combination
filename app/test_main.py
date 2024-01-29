3570ed5cd6f2bb892ba9e82b3ba3c5dd4aec1b9e
Tymofii Skopenko <youmustdeth@gmail.com>
2024-01-29 17:22:34 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,16 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_get_coin_combination() -> None:
+    assert get_coin_combination(1) == [1, 0, 0, 0], \
+        "Test failed for input: 1"
+    assert get_coin_combination(6) == [1, 1, 0, 0], \
+        "Test failed for input: 6"
+    assert get_coin_combination(17) == [2, 1, 1, 0], \
+        "Test failed for input: 17"
+    assert get_coin_combination(50) == [0, 0, 0, 2], \
+        "Test failed for input: 50"
+    assert get_coin_combination(41) == [1, 1, 1, 1], \
+        "Test failed for input: 41"
+    assert get_coin_combination(100) == [0, 0, 0, 4], \
+        "Test failed for input: 100"
