79190ff3599f565439b9755d5f753b3f892d10af
voronvasia <voronvasia@gmail.com>
2024-02-05 23:14:38 +0200

    task completed

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,42 @@
+from typing import Type
+
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+class TestCoinCombinationValues:
+    @pytest.mark.parametrize(
+        "cents, result",
+        [
+            (1, [1, 0, 0, 0]),
+            (6, [1, 1, 0, 0]),
+            (17, [2, 1, 1, 0]),
+            (50, [0, 0, 0, 2])
+        ]
+    )
+    def test_get_coin_combination(
+            self,
+            cents: int,
+            result: list
+    ) -> None:
+        assert (get_coin_combination(cents) == result)
+
+
+class TestCoinCombinationTypes:
+    @pytest.mark.parametrize(
+        "cents, exception",
+        [
+            ("1", TypeError),
+            ({}, TypeError),
+            ([], TypeError),
+            (None, TypeError),
+        ]
+    )
+    def test_get_coin_combination(
+            self,
+            cents: int,
+            exception: Type[Exception]
+    ) -> None:
+        with pytest.raises(exception):
+            get_coin_combination(cents)
