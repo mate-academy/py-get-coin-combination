16a020915fbc454fdd2d94ba4dfb7c831441ffe9
IvanGuculyak <ivan.guculyak@gmail.com>
2024-02-06 03:29:28 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -7,7 +7,7 @@ from app.main import get_coin_combination
     ("cents", "coin_combination"),
     [
         (0, [0, 0, 0, 0]),
-        (1, [1, 1, 0, 0]),
+        (6, [1, 1, 0, 0]),
         (17, [2, 1, 1, 0]),
         (50, [0, 0, 0, 2])
     ]
