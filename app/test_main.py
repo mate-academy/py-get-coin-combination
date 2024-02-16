27d982f4f57bc846d37c2c457191b082ab77bdd6
Maxim Moshenskiy <osidiv@ukr.net>
2024-02-16 18:07:48 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -10,11 +10,11 @@ from app.main import get_coin_combination
         (2, [2, 0, 0, 0]),
         (3, [3, 0, 0, 0]),
         (4, [4, 0, 0, 0]),
-        (41,[1, 1, 1, 1]),
+        (41, [1, 1, 1, 1]),
         (0, [0, 0, 0, 0])
     ],
 )
-def test_only_for_nickels(cents, expected):
+def test_only_for_nickels(cents: int, expected: list) -> None:
     assert get_coin_combination(cents) == expected
 
 
@@ -26,7 +26,7 @@ def test_only_for_nickels(cents, expected):
         (999999, [4, 0, 2, 39999]),
     ],
 )
-def test_for_large_cents(cents, expected):
+def test_for_large_cents(cents: int, expected: list) -> None:
     assert get_coin_combination(cents) == expected
 
 
@@ -38,5 +38,5 @@ def test_for_large_cents(cents, expected):
         (950, [0, 0, 0, 38]),
     ],
 )
-def test_only_for_quarters(cents, expected):
+def test_only_for_quarters(cents: int, expected: list) -> None:
     assert get_coin_combination(cents) == expected
