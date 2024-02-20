c78f59de3e19067642fa05d1a8e8266b95108789
Anna Zadorozhna <annalepiska@gmail.com>
2024-02-20 18:26:30 +0200

    Implement py-get-coin-combination

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,32 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents_int,coins_list",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (5, [0, 1, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (8, [3, 1, 0, 0]),
+        (10, [0, 0, 1, 0]),
+        (11, [1, 0, 1, 0]),
+        (17, [2, 1, 1, 0]),
+        (20, [0, 0, 2, 0]),
+        (35, [0, 0, 1, 1]),
+        (50, [0, 0, 0, 2]),
+        (56, [1, 1, 0, 2]),
+        (163, [3, 0, 1, 6]),
+        (548, [3, 0, 2, 21]),
+        (1892, [2, 1, 1, 75]),
+        (747394, [4, 1, 1, 29895])
+
+    ]
+)
+def test_get_coin_combination(
+        cents_int: int,
+        coins_list: list
+) -> None:
+    assert get_coin_combination(cents_int) == coins_list
