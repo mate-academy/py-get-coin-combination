6874b289c5a78fe289b1f0ba3aa050f3dcaed0f4
TarasFirst <firsttaras@mail.com>
2024-02-21 07:32:54 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,11 @@
 from app.main import get_coin_combination
+import pytest
 
 
-# write your tests here
+def test_should_return_list_with_correct_values() -> None:
+    assert get_coin_combination(18) == [3, 1, 1, 0]
+
+
+def test_coins_is_positive() -> None:
+    with pytest.raises(ValueError):
+        get_coin_combination(-1)
