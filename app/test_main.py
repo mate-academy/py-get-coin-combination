79fd1597bfea46e1e39ea4aea750945303323780
donbas777 <renary34@gmail.com>
2024-03-06 19:22:09 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,31 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_normal_values() -> None:
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+
+
+def test_zero() -> None:
+    assert get_coin_combination(0) == [0, 0, 0, 0]
+
+
+def test_penny() -> None:
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+
+
+def test_nickel() -> None:
+    assert get_coin_combination(5) == [0, 1, 0, 0]
+
+
+def test_dime() -> None:
+    assert get_coin_combination(10) == [0, 0, 1, 0]
+
+
+def test_quarter() -> None:
+    assert get_coin_combination(25) == [0, 0, 0, 1]
+
+
+def test_positive_number() -> None:
+    with pytest.raises(ValueError):
+        get_coin_combination(-1)
