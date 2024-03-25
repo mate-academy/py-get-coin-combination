5357b17464a46ce42bdc5a1bf09fe6accbc4d475
Shershen Alexandra <hornet240204@gmail.com>
2024-03-25 18:48:20 +0200

    Add type annotation

--- app/test_main.py
+++ app/test_main.py
@@ -1,9 +1,8 @@
 from app.main import get_coin_combination
 
 
-def test_get_coin_combination():
-    assert get_coin_combination(1) == [1, 0, 0, 0]  # 1 penny
-    assert get_coin_combination(6) == [1, 1, 0, 0]  # 1 penny + 1 nickel
-    assert get_coin_combination(17) == [2, 1, 1, 0]  # 2 pennies + 1 nickel + 1 dime
-    assert get_coin_combination(50) == [0, 0, 0, 2]  # 2 quarters
-
+def test_get_coin_combination() -> None:
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+    assert get_coin_combination(6) == [1, 1, 0, 0]
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+    assert get_coin_combination(50) == [0, 0, 0, 2]
