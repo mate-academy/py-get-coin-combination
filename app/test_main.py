58a819cb39651f715729d1a0017df38c419a5cf5
Shershen Alexandra <hornet240204@gmail.com>
2024-03-25 18:44:44 +0200

    Solution for task get coin combination

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,9 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_get_coin_combination():
+    assert get_coin_combination(1) == [1, 0, 0, 0]  # 1 penny
+    assert get_coin_combination(6) == [1, 1, 0, 0]  # 1 penny + 1 nickel
+    assert get_coin_combination(17) == [2, 1, 1, 0]  # 2 pennies + 1 nickel + 1 dime
+    assert get_coin_combination(50) == [0, 0, 0, 2]  # 2 quarters
+
