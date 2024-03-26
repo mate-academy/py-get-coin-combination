2f243d22fce1d86c193dc1fadcfacb47d35025d7
Vyacheslav Malchyk <k2.mvo98@gmail.com>
2024-03-26 19:48:09 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,26 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_should_return_list():
+    goals = get_coin_combination(5)
+    assert isinstance(goals, list)
+
+def test_should_return_list_length_must_be_four():
+    goals = get_coin_combination(30)
+    assert len(goals) == 4
+
+def test_should_return_expected_goals_when_value_is_one():
+    goals = get_coin_combination(1)
+    assert goals == [1, 0, 0, 0]
+
+def test_should_return_expected_goals_when_value_is_six():
+    goals = get_coin_combination(6)
+    assert goals == [1, 1, 0, 0]
+
+def test_should_return_expected_goals_when_value_is_seventeen():
+    goals = get_coin_combination(17)
+    assert goals == [2, 1, 1, 0]
+
+def test_should_return_expected_goals_when_value_is_fifty():
+    goals = get_coin_combination(50)
+    assert goals == [0, 0, 0, 2]
