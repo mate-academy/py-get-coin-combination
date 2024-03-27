02b9c82a2f2fcd73cbffa4abbe714aa024abfc2b
Khrystenko_Denys <deonis062002@gmail.com>
2024-03-27 12:12:53 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,21 @@
 from app.main import get_coin_combination
 
 
-def test_when_only_one_cent():
+def test_when_only_one_cent() -> None:
     assert (get_coin_combination(1) == [1, 0, 0, 0]
             ), "cent should be in zero indexes"
 
 
-def test_when_cents_more_than_5():
+def test_when_cents_more_than_5() -> None:
     assert (get_coin_combination(6) == [1, 1, 0, 0]
             ), "1 penny + 1 nickel"
 
 
-def test_entered_dimes():
+def test_entered_dimes() -> None:
     assert (get_coin_combination(17) == [2, 1, 1, 0]
             ), "2 pennies + 1 nickel + 1 dime"
 
 
-def test_entered_two_quarters():
+def test_entered_two_quarters() -> None:
     assert (get_coin_combination(50) == [0, 0, 0, 2]
             ), "2 quarters"
