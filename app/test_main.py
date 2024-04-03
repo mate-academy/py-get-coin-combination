e4aec2bc8ac38678d425a331fc7eee5919ec9d4b
semermaks <semermaks@mail.com>
2024-04-03 17:44:20 +0300

    Solution 1

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,24 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "amount, expected",
+    [
+            (1, [1, 0, 0, 0]),
+            (0, [0, 0, 0, 0]),
+            (6, [1, 1, 0, 0]),
+            (17, [2, 1, 1, 0]),
+            (25, [0, 0, 0, 1]),
+            (-1, [4, 0, 2, -1]),
+    ],
+    ids=["One",
+         "Zero",
+         "Six",
+         "17"
+         "25",
+         "Negative number",
+         "Number Example"]
+)
+def test_get_human_age(amount: int, expected: list) -> None:
+    assert get_coin_combination(amount) == expected
