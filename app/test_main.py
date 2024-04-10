b47e16e7188b67c7f1acb34a43cff299b6c6d5ed
Maksym Hekalo <maximgekalo@gmail.com>
2024-04-10 09:02:23 +0300

    test for get_coin_combination

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,32 @@
 from app.main import get_coin_combination
+import pytest
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, expected",
+    [
+        pytest.param(
+            1, [1, 0, 0, 0],
+            id="Return only pennie"
+        ),
+        pytest.param(
+            5, [0, 1, 0, 0],
+            id="Return only nickel"
+        ),
+        pytest.param(
+            10, [0, 0, 1, 0],
+            id="Return only dime"
+        ),
+        pytest.param(
+            25, [0, 0, 0, 1],
+            id="Return only quarter"
+        ),
+        pytest.param(
+            41, [1, 1, 1, 1],
+            id="Return coins combinations"
+        )
+
+    ]
+)
+def test_logic_of_get_coin_combination(cents: int, expected: list) -> None:
+    assert get_coin_combination(cents) == expected
