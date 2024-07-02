89ae9255747de64d0542f5a070274343bd74b6a7
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2024-07-02 20:10:05 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,17 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, coin_count_list",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
+    ]
+)
+def test_correctly_conversion_cents_into_coins(
+        cents: int, coin_count_list: list[int]
+) -> None:
+    assert get_coin_combination(cents) == coin_count_list
