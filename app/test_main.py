d913e22d5e2b788676e313129dfba281a9a4c47f
Liza Shulika <liza.shulika2009@gmail.com>
2025-01-04 15:21:38 +0200

    money

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,30 @@
+import pytest
+
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+class Money:
+    @pytest.mark.parametrize(
+        "cents, result",
+        [
+            pytest.param(
+                1,
+                [1, 1, 0, 0],
+                id="one penny"
+            ),
+            pytest.param(
+                6,
+                [1, 1, 0, 0],
+                id="six cents"
+            ),
+            pytest.param(
+                17,
+                [2, 1, 1, 0],
+                id="seventeen cents"
+            ),
+        ]
+    )
+    def test_modify_class(self, cents: int,
+                          result: list[int]) -> None:
+        assert get_coin_combination(cents) == result
