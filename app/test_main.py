3366ebfacc37b3f6f3de8a266481a7daacc0c825
Ivan Khomych <ivan.khomychh@gmail.com>
2025-01-04 23:40:33 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,18 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "cents,expected_result",
+    [
+        pytest.param(1, [1, 0, 0, 0], id="Test should return one penny"),
+        pytest.param(6, [1, 1, 0, 0], id="Test should return one penny and one nickel"),
+        pytest.param(17, [2, 1, 1, 0], id="Test should return three different coins"),
+        pytest.param(50, [0, 0, 0, 2], id="Test should return only biggest coins")
+    ]
+)
+def test_should_return_the_least_number_of_coins(cents: int, expected_result) -> None:
+    result = get_coin_combination(cents)
+    assert result == expected_result
