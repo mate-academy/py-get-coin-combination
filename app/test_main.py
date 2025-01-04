ea5fcc35ef2dddb3d43f145f6b565ba722532465
Bohdan <bogdannew@ukr.net>
2025-01-04 17:10:42 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -3,26 +3,79 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "cents, result",
+    "cents,coins",
     [
         pytest.param(
-            1, [1, 0, 0, 0],
-            id="Test 1: 1 cent = [1, 0, 0, 0]"),
+            0,
+            [0, 0, 0, 0],
+            id="should return 0"
+        ),
         pytest.param(
-            7, [2, 1, 0, 0],
-            id="Test 2: 7 cent = [2, 1, 0, 0]"),
+            1,
+            [1, 0, 0, 0],
+            id="should return 1 penny"
+        ),
         pytest.param(
-            18, [3, 1, 1, 0],
-            id="Test 3: 18 cent = [3, 1, 1, 0]"),
+            5,
+            [0, 1, 0, 0],
+            id="should return 1 nickel"
+        ),
         pytest.param(
-            41, [1, 1, 1, 1],
-            id="Test 4: 41 cent = [1, 1, 1, 1]"),
+            10,
+            [0, 0, 1, 0],
+            id="should return 1 dime"
+        ),
+        pytest.param(
+            25,
+            [0, 0, 0, 1],
+            id="should return 1 quarter"
+        ),
+        pytest.param(
+            6,
+            [1, 1, 0, 0],
+            id="should return 1 penny + 1 nickel"
+        ),
+        pytest.param(
+            15,
+            [0, 1, 1, 0],
+            id="should return 1 nickel + 1 dime"
+        ),
+        pytest.param(
+            35,
+            [0, 0, 1, 1],
+            id="should return 1 dime + 1 quarter"
+        ),
+        pytest.param(
+            26,
+            [1, 0, 0, 1],
+            id="should return 1 penny + 1 quarter"
+        ),
+        pytest.param(
+            17,
+            [2, 1, 1, 0],
+            id="should return 2 pennies + 1 nickel + 1 dime"
+        ),
+        pytest.param(
+            99,
+            [4, 0, 2, 3],
+            id="should return 4 pennies + 2 dimes + 3 quarters"
+        ),
+        pytest.param(
+            40,
+            [0, 1, 1, 1],
+            id="should return 1 nickel + 1 dime + 1 quarter"
+        ),
+        pytest.param(
+            31,
+            [1, 1, 0, 1],
+            id="should return 1 penny + 1 nickel + 1 quarter"
+        ),
+        pytest.param(
+            41,
+            [1, 1, 1, 1],
+            id="should return 1 penny + 1 nickel + 1 dime + 1 quarter"
+        )
     ]
 )
-
-
-def test_should_return_list_with_1_position(
-        cents: int,
-        result: list[int]
-) -> None:
-    assert get_coin_combination(cents) == result
+def test_get_coins_combination_correctly(cents: int, coins: list) -> None:
+    assert get_coin_combination(cents) == coins
