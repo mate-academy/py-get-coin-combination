f85781249fd4334210211f5d39b59bfe7c14db65
Liza Shulika <liza.shulika2009@gmail.com>
2025-01-05 21:30:06 +0200

    coin2

--- app/test_main.py
+++ app/test_main.py
@@ -5,24 +5,24 @@ from app.main import get_coin_combination
 
 
 @pytest.mark.parametrize(
-        "cents, result",
-        [
-            pytest.param(
-                1,
-                [1, 0, 0, 0],
-                id="one penny"
-            ),
-            pytest.param(
-                6,
-                [1, 1, 0, 0],
-                id="six cents"
-            ),
-            pytest.param(
-                17,
-                [2, 1, 1, 0],
-                id="seventeen cents"
-            ),
-        ]
-    )
+    "cents, result",
+    [
+        pytest.param(
+            1,
+            [1, 0, 0, 0],
+            id="one penny"
+        ),
+        pytest.param(
+            6,
+            [1, 1, 0, 0],
+            id="six cents"
+        ),
+        pytest.param(
+            17,
+            [2, 1, 1, 0],
+            id="seventeen cents"
+        ),
+    ]
+)
 def test_modify_class(cents: int, result: list[int]) -> None:
-        assert get_coin_combination(cents) == result
+    assert get_coin_combination(cents) == result
