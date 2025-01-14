3e2a9251785cdbfeda2cd72b8fccef8f584b0479
Miwacer <il>
2025-01-14 20:34:01 +0200

    update

--- app/test_main.py
+++ app/test_main.py
@@ -1,14 +1,8 @@
+import pytest
 from app.main import get_coin_combination
 
 
-def test_get_coin_combination_1_penny() -> None:
+def test_get_coin_combination() -> None:
     assert get_coin_combination(1) == [1, 0, 0, 0]
-
-def test_get_coin_combination_1_penny_and_1_nickel() -> None:
     assert get_coin_combination(6) == [1, 1, 0, 0]
-
-def test_get_coin_combination_2_penny_and_1_nickel_and_1_dime() -> None:
     assert get_coin_combination(17) == [2, 1, 1, 0]
-
-def test_get_coin_combination_2_quarters() -> None:
-    assert get_coin_combination(50) == [0, 0, 0, 2]
