222b311f566e389650f6a6f18744144a93c6b600
Olha Yukhnenko <pyolh36@gmail.com>
2025-01-15 11:53:26 +0200

    fix_flake8_type_annotation

--- app/test_main.py
+++ app/test_main.py
@@ -2,19 +2,19 @@ import pytest
 from app.main import get_coin_combination
 
 
-def test_return_only_penny():
+def test_return_only_penny() -> None:
     assert get_coin_combination(1) == [1, 0, 0, 0]
 
 
-def test_return_penny_and_nickel():
+def test_return_penny_and_nickel() -> None:
     assert get_coin_combination(6) == [1, 1, 0, 0]
 
 
-def test_return_pennies_nickel_dime():
+def test_return_pennies_nickel_dime() -> None:
     assert get_coin_combination(17) == [2, 1, 1, 0]
 
 
-def test_return_quarters():
+def test_return_quarters() -> None:
     assert get_coin_combination(50) == [0, 0, 0, 2]
 
 
@@ -26,5 +26,5 @@ def test_return_quarters():
         (50, [0, 0, 0, 2]),
     ]
 )
-def test_various_cases(cents, expected) -> None:
+def test_various_cases(cents: int, expected: int) -> None:
     assert get_coin_combination(cents) == expected
