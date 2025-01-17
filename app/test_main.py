833efe03b452e3b70a1002a50df8f374a1f8b829
Anastasiia Mylnikova <87014295+amylnikova@users.noreply.github.com>
2025-01-17 17:06:21 +0200

    test get coin combination solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,41 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+class TestCoinCombination:
+    @pytest.mark.parametrize(
+        "cents, expected_coin_combination",
+        [
+            pytest.param(
+                0,
+                [0, 0, 0, 0],
+                id="zero value"
+            ),
+            pytest.param(
+                1,
+                [1, 0, 0, 0],
+                id="1 cent is 1 penny"
+            ),
+            pytest.param(
+                6,
+                [1, 1, 0, 0],
+                id="6 cents are 1 penny and 1 nickel"
+            ),
+            pytest.param(
+                17,
+                [2, 1, 1, 0],
+                id="17 cents are 2 pennies, 1 nickel, and 1 dime "
+            ),
+            pytest.param(
+                50,
+                [0, 0, 0, 2],
+                id="50 cents are 2 quarters"
+            )
+        ]
+    )
+    def test_correct_coin_combination(
+            self,
+            cents: int,
+            expected_coin_combination: list
+    ):
+        assert get_coin_combination(cents) == expected_coin_combination
