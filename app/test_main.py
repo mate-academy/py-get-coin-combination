9d9f4a1e6c39f422674b266812a87a3893b4298e
IrynaBrenko <brenko.iryna1997@gmail.com>
2025-01-17 10:08:19 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,31 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_get_number_of_pennies() -> None:
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+
+
+def test_get_numbers_of_nickels() -> None:
+    assert get_coin_combination(6) == [1, 1, 0, 0]
+
+
+def test_get_number_of_dimes() -> None:
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+
+
+def test_get_number_of_quarters() -> None:
+    assert get_coin_combination(50) == [0, 0, 0, 2]
+
+
+@pytest.mark.parametrize(
+    "cents, expected",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
+    ]
+)
+def test_coins_combination(cents: int, expected: [int, int, int, int]) -> None:
+    assert get_coin_combination(cents) == expected
