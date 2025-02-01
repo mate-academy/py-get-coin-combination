96f2b9e6dd3573b7bffbc6266c576f3286fa478f
VolodymyrKaraniuk <volodimirkaranuk7@gmail.com>
2025-02-01 13:43:06 +0100

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -5,7 +5,7 @@ from app.main import get_coin_combination
 
 
 @pytest.mark.parametrize(
-    "amount,result",
+    "cents,result",
     [
         pytest.param(1, [1, 0, 0, 0], id="should convert 5 to 1 penny"),
         pytest.param(6, [1, 1, 0, 0], id="should convert 6 to 1 nickel and 1 penny"),
@@ -13,5 +13,5 @@ from app.main import get_coin_combination
         pytest.param(50, [0, 0, 0, 2], id="should convert 50 to 2 quarters"),
     ]
 )
-def test_get_coin_combination(amount: int,  result: list) -> None:
-   assert (get_coin_combination(amount) == result)
+def test_get_coin_combination(cents: int,  result: list) -> None:
+   assert (get_coin_combination(cents) == result)
