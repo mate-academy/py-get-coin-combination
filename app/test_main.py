676de742b3182c351dfa4c4bb3ba7e0c312bb3e3
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-02-02 17:31:28 +0100

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,22 @@
+import pytest
 from app.main import get_coin_combination
 
+def test_one_penny():
+    assert get_coin_combination(1) == [1, 0, 0, 0]
 
-# write your tests here
+def test_one_nickel():
+    assert get_coin_combination(5) == [0, 1, 0, 0]
+
+def test_one_dime():
+    assert get_coin_combination(10) == [0, 0, 1, 0]
+
+def test_one_quarter():
+    assert get_coin_combination(25) == [0, 0, 0, 1]
+
+def test_mixed_coins():
+    assert get_coin_combination(6) == [1, 1, 0, 0]
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+    assert get_coin_combination(50) == [0, 0, 0, 2]
+
+def test_large_amount():
+    assert get_coin_combination(99) == [4, 0, 2, 3]
