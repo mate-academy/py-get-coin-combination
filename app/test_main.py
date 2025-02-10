1ed24a7211a2ee2874d87e60217c414715e5ea1e
Bartolomeu <bartoszokroj@wp.pl>
2025-02-10 18:33:20 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,21 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "amount,coins_list",
+    [
+        (4, [4,0,0,0]),
+        (52, [2,0,0,2]),
+        (41, [1,1,1,1]),
+        (0, [0,0,0,0])
+    ]
+)
+def test_should_calculate_num_of_coins_correctly(amount, coins_list) -> None:
+    assert get_coin_combination(amount) == coins_list
+
+
+def test_should_return_value_error_if_amount_is_negative_number() -> None:
+    with pytest.raises(ValueError):
+        get_coin_combination(-1)
