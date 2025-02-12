2bc95571df1e30b940b98a5a2758bc4df7aff337
Yaroslav <yaroslav.liubarets@icloud.com>
2025-02-12 19:08:45 +0000

    Solution 1.1

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,19 @@
 import pytest
+from typing import List
+
 from app.main import get_coin_combination
 
 
 @pytest.mark.parametrize(
-    "value,result",
+    "amount,coins_list",
     [
-        (1, [1, 0, 0, 0]),
-        (6, [1, 1, 0, 0]),
-        (17, [2, 1, 1, 0]),
-        (17, [2, 1, 1, 0])
-    ],
-    ids=[
-        "1 penny",
-        "1 penny + 1 nickel",
-        "2 pennies + 1 nickel + 1 dime",
-        "2 quarters"
-    ])
-def test_if_calculates_proper_amount_of_coins(
-        value: int, result: list[int]) -> None:
-    assert get_coin_combination(value) == result
+        (4, [4, 0, 0, 0]),
+        (52, [2, 0, 0, 2]),
+        (41, [1, 1, 1, 1]),
+        (0, [0, 0, 0, 0])
+    ]
+)
+def test_should_calculate_num_of_coins_correctly(
+        amount: int,
+        coins_list: List) -> None:
+    assert get_coin_combination(amount) == coins_list
