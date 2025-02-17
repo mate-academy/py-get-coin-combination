8090b868fa022f0bb0e7b4a460220130c2a06e84
Vik <viktoria.om@gmail.com>
2025-02-17 15:36:33 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,33 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+class TestDiffNumbers:
+    @pytest.mark.parametrize(
+        "coins_to_count,number_of_coins",
+        [
+            pytest.param(
+                1,
+                [1, 0, 0, 0]
+            ),
+            pytest.param(
+                6,
+                [1, 1, 0, 0]
+            ),
+            pytest.param(
+                17,
+                [2, 1, 1, 0]
+            ),
+            pytest.param(
+                50,
+                [0, 0, 0, 2]
+            )
+        ]
+    )
+    def test_should_return_correct_number_of_coins(
+            self,
+            coins_to_count: int,
+            number_of_coins: list
+    ) -> None:
+        assert get_coin_combination(coins_to_count) == number_of_coins
