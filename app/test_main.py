e1c6b278e5fae241846904060f534efe0733d760
Nikita Korenevskyi <korenevsky37@gmail.com>
2025-02-28 15:49:14 +0200

    Done

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,14 @@
 from app.main import get_coin_combination
+import pytest
 
 
-# write your tests here
+class TestGetCoinCombination:
+    def test_value_cents_equals_0(self):
+        cents = 0
+        result = get_coin_combination(cents)
+        assert result == [0, 0, 0, 0]
+
+    def test_value_cents_equals_41(self):
+        cents = 41
+        result = get_coin_combination(cents)
+        assert result == [1, 1, 1, 1]
