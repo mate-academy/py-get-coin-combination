6cc1a24ed509882bb95f80426f546c5696d12ada
Dolhyi Valerii <v.dolhyi@gmail.com>
2025-03-05 20:57:34 +0100

    get_coin_v1

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,25 @@
 from app.main import get_coin_combination
+import pytest
 
 
-# write your tests here
+@pytest.mark.parametrize("cents,result",
+                         [
+                             (1, [1, 0, 0, 0]),
+                             (6, [1, 1, 0, 0]),
+                             (17, [2, 1, 1, 0]),
+                             (50, [0, 0, 0, 2]),
+                         ]
+                         )
+def test_should_return_expected_result(cents: int,
+                                       result: list) -> None:
+    assert get_coin_combination(cents) == result
+
+
+@pytest.mark.parametrize("values,result",
+                         [
+                             ([1, 5, 10, 25], 5)
+                         ]
+                         )
+def test_value_of_coins(values: list,
+                        result: int) -> None:
+    assert values[3] / values[1] == result
