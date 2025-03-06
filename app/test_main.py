7df341be2c839488776c318c74a15f20784a5836
Maksym Omelkov <omelkov85@gmail.com>
2025-03-06 23:23:47 +0200

    Coin combination (1.0)

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,18 @@
 from app.main import get_coin_combination
+import pytest
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, expected_list",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (16, [1, 1, 1, 0]),
+        (41, [1, 1, 1, 1]),
+        (1_000_049, [4, 0, 2, 40_001])
+    ]
+)
+def test_get_human_age(cents: int,
+                        expected_list: list[int]) -> None:
+    assert get_coin_combination(cents) == expected_list
