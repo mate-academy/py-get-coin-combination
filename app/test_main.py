d3e4ed5e9dba71e303a923319c3b1c53ff831c15
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-03-11 22:11:07 +0200

    Fixed flake8

--- app/test_main.py
+++ app/test_main.py
@@ -3,15 +3,21 @@ from app.main import get_coin_combination
 
 
 @pytest.mark.parametrize(
-    "cents, expected_result, id",
+    "cents, expected_result",
     [
-        (1, [1, 0, 0, 0], "get_coin_combination_one_cent"),
-        (6, [1, 1, 0, 0], "get_coin_combination_six_cents"),
-        (17, [2, 1, 1, 0], "get_coin_combination_seventeen_cents"),
-        (50, [0, 0, 0, 2], "get_coin_combination_fifty_cents"),
+        (1, [1, 0, 0, 0],),
+        (6, [1, 1, 0, 0],),
+        (17, [2, 1, 1, 0],),
+        (50, [0, 0, 0, 2],),
+    ],
+    ids=[
+        "Test for 1 cent",
+        "Test for 6 cents",
+        "Test for 17 cents",
+        "Test for 50 cents"
     ]
 )
-def test_get_coin_combination(cents: int, expected_result: list, id) -> None:
+def test_get_coin_combination(cents: int, expected_result: list) -> None:
     result = get_coin_combination(cents)
     assert result == expected_result, (
         f"Result should be {expected_result}"
@@ -19,29 +25,42 @@ def test_get_coin_combination(cents: int, expected_result: list, id) -> None:
 
 
 @pytest.mark.parametrize(
-    "cents, id",
+    "cents",
     [
-        (1, "data_type_check_for_1_cent"),
-        (6, "data_type_check_for_6_cent"),
-        (17, "data_type_check_for_17_cent"),
-        (50, "data_type_check_for_50_cent"),
+        1,
+        6,
+        17,
+        50,
+    ],
+    ids=[
+        "Test for data type of 1 cent",
+        "Test for data type of 6 cents",
+        "Test for data type of 17 cents",
+        "Test for data type of 50 cents"
     ]
 )
-def test_data_type_of_cents(cents: int, id) -> None:
+def test_data_type_of_cents(cents: int) -> None:
     assert isinstance(cents, int), (
-        f"Cents should be int"
+        "Cents should be int"
     )
 
+
 @pytest.mark.parametrize(
-    "cents, expected_result, id",
+    "cents",
     [
-        (1, [1, 0, 0, 0], "data_type_check_for_result_(cents=1)"),
-        (6, [1, 1, 0, 0], "data_type_check_for_result_(cents=6)"),
-        (17, [2, 1, 1, 0], "data_type_check_for_result_(cents=17)"),
-        (50, [0, 0, 0, 2], "data_type_check_for_result_(cents=50)"),
+        1,
+        6,
+        17,
+        50,
+    ],
+    ids=[
+        "Test for result type when cents = 1",
+        "Test for result type when cents = 6",
+        "Test for result type when cents = 17",
+        "Test for result type when cents = 50"
     ]
 )
-def test_data_type_of_result(cents: int, expected_result: list, id) -> None:
+def test_data_type_of_result(cents: int) -> None:
     result = get_coin_combination(cents)
     assert isinstance(result, list), (
         "Result should be list"
