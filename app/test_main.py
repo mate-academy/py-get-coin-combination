8d445dc8fe49eee5464bafadf188f7acfa1e219d
Polevoy Yevhen <polevoye90@gmail.ua>
2025-03-15 16:40:51 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -13,6 +13,19 @@ class TestCoin:
         (99, [4, 0, 2, 3]),
         (100, [0, 0, 0, 4]),
     ])
-    def test_get_coin_combination(self, cents, expected):
+    def test_get_coin_combination(self, cents: int, expected: list) -> None:
         assert get_coin_combination(cents) == expected
 
+    @pytest.mark.parametrize(
+        "invalid_input",
+        [
+            ("10",
+             None,
+             [10],
+             {"cents": 5},
+             )
+        ]
+    )
+    def test_get_coin_combination_invalid_type(self, invalid_input) -> None:
+        with pytest.raises(TypeError):
+            get_coin_combination(invalid_input)
