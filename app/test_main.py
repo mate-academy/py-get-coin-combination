7c9281bf60fd01e8b92331aafeb5514c0a28912c
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2025-03-16 11:52:48 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,20 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents,list_of_coins",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2])
+    ]
+)
+def test_does_return_the_least_amount_of_coins(
+    cents: int,
+    list_of_coins: list[int]
+) -> None:
+    assert get_coin_combination(cents) == list_of_coins
