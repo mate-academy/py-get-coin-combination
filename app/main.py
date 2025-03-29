a29d013553a5afda776e9d5e43bf4ae82e0d7a8d
Yaroslav Didkivskiy <yaroslavdidkivskiy@gmail.com>
2025-03-30 00:34:26 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ def get_coin_combination(cents: int) -> list:
     values = [1, 5, 10, 25]
     coins = [0, 0, 0, 0]
 
-    for i in range(3, -1, -1):
+    for i in range(4):
         coins[i] = cents // values[i]
         cents -= coins[i] * values[i]
 
-    return coins
+    return coins[::-1]
