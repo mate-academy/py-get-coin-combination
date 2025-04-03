5bd2b028522f8a3beaf604cf5b8d56ed54551618
Farzam <farzamgt@gmail.com>
2025-04-03 15:05:42 +0300

    Fixed flake8

--- app/test_main.py
+++ app/test_main.py
@@ -9,7 +9,7 @@ def test_get_coin_combination_6() -> None:
     assert get_coin_combination(6) == [1, 1, 0, 0]
 
 
-def test_get_coin_combination_17():
+def test_get_coin_combination_17() -> None:
     assert get_coin_combination(17) == [2, 1, 1, 0]
 
 
