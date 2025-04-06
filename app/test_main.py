a2dbc061854846a5c74d4dd58a53c9b703a569ec
Timur Khvat <timurkhvat@gmail.com>
2025-04-06 11:03:41 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,21 @@
 from app.main import get_coin_combination
 
 
-def test_returns_one_penny_for_1_cent():
+def test_get_coin_combination_returns_one_penny_for_1_cent():
     assert get_coin_combination(1) == [1, 0, 0, 0]
 
 
-def test_returns_one_nickel_for_5_cents():
+def test_get_coin_combination_returns_penny_and_nickel_for_6_cents():
     assert get_coin_combination(6) == [1, 1, 0, 0]
 
 
-def test_returns_one_dime_for_10_cents():
+def test_get_coin_combination_returns_penny_nickel_dime_for_17_cents():
     assert get_coin_combination(17) == [2, 1, 1, 0]
 
 
-def test_returns_one_quarter_for_25_cents():
+def test_get_coin_combination_returns_two_quarters_for_50_cents():
     assert get_coin_combination(50) == [0, 0, 0, 2]
+
+
+def test_get_coin_combination_returns_all_coin_types_for_41_cents():
+    assert get_coin_combination(41) == [1, 1, 1, 1]
