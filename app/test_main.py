650cdd5e42f81c626089f322876cdb723d25c996
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-04-19 21:23:32 +0100

    v1

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,21 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_return_zeros_for_zero_input() -> None:
+    assert get_coin_combination(0) == [0, 0, 0, 0]
+
+
+def test_return_1_for_1_input() -> None:
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+
+
+def test_return_correct_for_6_input() -> None:
+    assert get_coin_combination(6) == [1, 1, 0, 0]
+
+
+def test_return_correct_for_17_input() -> None:
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+
+
+def test_return_correct_for_50_input() -> None:
+    assert get_coin_combination(50) == [0, 0, 0, 2]
