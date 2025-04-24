260f4be9e5e9b0a5eb363bc57b05ddc755e0aa73
Rostyslav Kopotiienko <rost.kop.200.5@gmail.com>
2025-04-24 16:18:02 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,25 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cent,result",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2])
+    ]
+)
+def test_all_combinations(cent: int, result: list):
+    assert get_coin_combination(cent) == result
+
+
+def test_age_is_less_then_zero() -> None:
+    with pytest.raises(ValueError):
+        get_coin_combination(-1)
+
+
+def test_age_is_not_integer() -> None:
+    with pytest.raises(TypeError):
+        get_coin_combination("10")
