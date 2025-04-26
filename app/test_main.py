a01b724d240ff01d5e8cfa0b3289cd54e21b3143
Oleksii Drenov <leshadrenev@gmail.com>
2025-04-26 16:51:49 +0300

    fix flake solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,21 @@
 from app.main import get_coin_combination
 
 
-def test_should_return_correct_combination_for_1():
+def test_should_return_correct_combination_for_1() -> None:
     assert get_coin_combination(1) == [1, 0, 0, 0]
 
-def test_should_return_correct_combination_for_6():
+
+def test_should_return_correct_combination_for_6() -> None:
     assert get_coin_combination(6) == [1, 1, 0, 0]
 
-def test_should_return_correct_combination_for_17():
+
+def test_should_return_correct_combination_for_17() -> None:
     assert get_coin_combination(17) == [2, 1, 1, 0]
 
-def test_should_return_correct_combination_for_50():
+
+def test_should_return_correct_combination_for_50() -> None:
     assert get_coin_combination(50) == [0, 0, 0, 2]
 
-def test_should_return_correct_combination_for_41():
+
+def test_should_return_correct_combination_for_41() -> None:
     assert get_coin_combination(41) == [1, 1, 1, 1]
