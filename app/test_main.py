c83dffc6747df24932b505d0ca8036f35c7cb166
Oleksii Drenov <leshadrenev@gmail.com>
2025-04-26 16:50:26 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,17 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_should_return_correct_combination_for_1():
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+
+def test_should_return_correct_combination_for_6():
+    assert get_coin_combination(6) == [1, 1, 0, 0]
+
+def test_should_return_correct_combination_for_17():
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+
+def test_should_return_correct_combination_for_50():
+    assert get_coin_combination(50) == [0, 0, 0, 2]
+
+def test_should_return_correct_combination_for_41():
+    assert get_coin_combination(41) == [1, 1, 1, 1]
