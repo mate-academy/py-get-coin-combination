848c090ba2d9d0ec8ecc046ac6d2ba10e06d8a70
Bogdan0101 <kononovb777@gmail.com>
2025-05-01 13:27:50 +0300

    Solution#1

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,35 @@
+from pytest import mark, param
 from app.main import get_coin_combination
 
 
-# write your tests here
+class TestModify:
+    @mark.parametrize(
+        "cents, result",
+        [
+            param(
+                1,
+                [1, 0, 0, 0],
+                id="Test result != [1, 0, 0, 0]"
+            ),
+            param(
+                6,
+                [1, 1, 0, 0],
+                id="Test result != [1, 1, 0, 0]"
+            ),
+            param(
+                17,
+                [2, 1, 1, 0],
+                id="Test result != [2, 1, 1, 0]"
+            ),
+            param(
+                50,
+                [0, 0, 0, 2],
+                id="Test result != [0, 0, 0, 2]"
+            ),
+
+        ]
+    )
+    def test_modify(self, cents, result) -> None:
+        assert isinstance(cents, int), "Please enter an integer"
+        assert cents > 0, "Please enter a positive number"
+        assert get_coin_combination(cents) == result
