8fb26ec8f664548dec4c36707bb54e9aa0b4c9e5
Anastasiia Matsebon <anastasiiamatsebon@gmail.com>
2025-05-16 20:29:07 +0200

    завдання

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,29 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_one_penny():
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+
+def test_one_nickel():
+    assert get_coin_combination(5) == [0, 1, 0, 0]
+
+def test_one_dime():
+    assert get_coin_combination(10) == [0, 0, 1, 0]
+
+def test_one_quarter():
+    assert get_coin_combination(25) == [0, 0, 0, 1]
+
+def test_six_cents():
+    assert get_coin_combination(6) == [1, 1, 0, 0]
+
+def test_seventeen_cents():
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+
+def test_fifty_cents():
+    assert get_coin_combination(50) == [0, 0, 0, 2]
+
+def test_zero_cents():
+    assert get_coin_combination(0) == [0, 0, 0, 0]
+
+def test_large_amount():
+    assert get_coin_combination(99) == [4, 0, 2, 3]
