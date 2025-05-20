5c2a037c8869afdb8e71060bb3630858ff1ca7b0
Andrii Omelchenko <andreyomem@gmail.com>
2025-05-20 18:57:47 +0300

    implement tests for get_coin_combination

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,8 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_get_coin_combination_return_0_when_given_0():
+    assert get_coin_combination(0) == [0, 0, 0, 0]
+
+def test_get_coin_combination_return_1_of_each_currency_given_41():
+    assert get_coin_combination(41) == [1, 1, 1, 1]
