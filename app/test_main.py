a8408344fdaedaab96d92b75c82ffbfdbc9f247f
Andrii Omelchenko <andreyomem@gmail.com>
2025-05-20 19:33:12 +0300

    implement tests for get_coin_combination

--- app/test_main.py
+++ app/test_main.py
@@ -3,14 +3,12 @@ import pytest
 
 
 class TestGetCoinCombination:
-
-    test_cases = [
+    @pytest.mark.parametrize("cents, expected", [
         (0, [0, 0, 0, 0]),
-        (6, [1, 1, 0, 0 ]),
+        (6, [1, 1, 0, 0]),
         (16, [1, 1, 1, 0]),
         (50, [0, 0, 0, 2]),
-        (10327,[2, 0, 0, 413])
-    ]
-    @pytest.mark.parametrize("cents, expected", test_cases)
-    def test_get_coin_combination(self, cents, expected):
+        (10327, [2, 0, 0, 413])
+    ])
+    def test_get_coin_combination(self, cents: int, expected: list) -> None:
         assert get_coin_combination(cents) == expected
