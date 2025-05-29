125ffa34384b7dae7ca733dc91fa369b89aab98a
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-05-29 17:00:46 +0200

    Test conversion currency function

--- app/test_main.py
+++ app/test_main.py
@@ -2,7 +2,8 @@ import pytest
 from app.main import get_coin_combination
 
 
-@pytest.mark.parametrize("cents, expected", [
+@pytest.mark.parametrize("cents, expected",
+[
     (1, [1, 0, 0, 0]),
     (6, [1, 1, 0, 0]),
     (17, [2, 1, 1, 0]),
@@ -13,7 +14,10 @@ from app.main import get_coin_combination
     (24, [4, 0, 2, 0]),
     (49, [4, 0, 2, 1]),
     (99, [4, 0, 2, 3]),
-], ids=[
+],
+
+ids=
+[
     "1 cent = 1 penny",
     "6 cents = 1 penny + 1 nickel",
     "17 cents = 2 pennies + 1 nickel + 1 dime",
@@ -25,20 +29,32 @@ from app.main import get_coin_combination
     "49 cents = 4 pennies + 2 dimes + 1 quarter",
      "99 cents = 3 quarters + 2 dimes + 4 pennies"
 ])
-def test_get_coin_combination(cents: int, expected: list[int]) -> None:
-    assert get_coin_combination(cents) == expected, \
-        f"Expected {expected} for {cents} cents, but got {get_coin_combination(cents)}"
 
-@pytest.mark.parametrize("cents, expected", [
+
+def test_get_coin_combination(cents: int,
+                              expected: list[int]) -> None:
+    assert get_coin_combination(cents) == expected, \
+        (f"Expected {expected} for {cents} cents, "
+         f"but got {get_coin_combination(cents)}")
+
+
+@pytest.mark.parametrize("cents, expected",
+[
     (0, [0, 0, 0, 0]),
     (-1, [0, 0, 0, 0]),
-], ids=[
+],
+ids=
+[
     "0 cents = no coins",
     "-1 cents = no coins",
 ])
-def test_get_coin_combination_boundaries(cents: int, expected: list[int]) -> None:
+
+
+def test_get_coin_combination_boundaries(cents: int,
+                                         expected: list[int]) -> None:
     assert get_coin_combination(cents) == expected, \
-        f"Expected {expected} for {cents} cents, but got {get_coin_combination(cents)}"
+        (f"Expected {expected} for {cents} cents,"
+         f" but got {get_coin_combination(cents)}")
 
 if __name__ == "__main__":
     pass
