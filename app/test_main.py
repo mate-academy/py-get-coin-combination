2a7cd5331695eae10d6bc6c3ffd02eebcab5838e
Denis <godlike.kipish@gmail.com>
2025-06-10 20:58:09 +0300

    Result 2

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,17 @@
 from app.main import get_coin_combination
 
 
-def test_should_return_1_penny():
+def test_should_return_1_penny() -> None:
     assert get_coin_combination(1) == [1, 0, 0, 0]
 
 
-def test_should_return_1_penny_and_1_nickel():
+def test_should_return_1_penny_and_1_nickel() -> None:
     assert get_coin_combination(6) == [1, 1, 0, 0]
 
 
-def test_should_return_1_penny_1_nickel_1_dime():
+def test_should_return_1_penny_1_nickel_1_dime() -> None:
     assert get_coin_combination(16) == [1, 1, 1, 0]
 
 
-def test_should_return_2_quarters():
+def test_should_return_2_quarters() -> None:
     assert get_coin_combination(50) == [0, 0, 0, 2]
