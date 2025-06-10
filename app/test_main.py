3813509f07362eb8307680245255f7df44c0389f
Denis <godlike.kipish@gmail.com>
2025-06-10 20:53:41 +0300

    Result

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,17 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_should_return_1_penny():
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+
+
+def test_should_return_1_penny_and_1_nickel():
+    assert get_coin_combination(6) == [1, 1, 0, 0]
+
+
+def test_should_return_1_penny_1_nickel_1_dime():
+    assert get_coin_combination(16) == [1, 1, 1, 0]
+
+
+def test_should_return_2_quarters():
+    assert get_coin_combination(50) == [0, 0, 0, 2]
