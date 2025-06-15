5804170a993e3e40e0a98889f390223246f3bc5d
Maksym Subchak <donjkee2@gmail.com>
2025-06-15 20:21:04 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -2,14 +2,15 @@ import pytest
 from app.main import get_coin_combination
 
 
-@pytest.mark.parametrize("cents, expected",
+@pytest.mark.parametrize(
+    "cents, expected",
     [
-    (0, [0, 0, 0, 0]),
-    (1, [1, 0, 0, 0]),
-    (6, [1, 1, 0, 0]),
-    (17, [2, 1, 1, 0]),
-    (50, [0, 0, 0, 2]),
-    (75, [0, 0, 0, 3])
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
+        (75, [0, 0, 0, 3])
     ]
 )
 def test_cents(cents: int, expected: list[int]) -> None:
