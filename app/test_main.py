99b33c55d0981112c674055af30d1ee228e94b13
Valerii <valerapiliaev@gmail.com>
2025-06-23 15:41:10 +0300

    add tests for get_coin_combination function

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,23 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cent, result",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2])
+    ],
+    ids=[
+        "1 penny",
+        "1 penny + 1 nickel",
+        "2 pennies + 1 nickel + 1 dime",
+        "2 quarters"
+    ]
+)
+def test_coin_combination(cent: int, result: list[int]) -> None:
+    assert (
+        get_coin_combination(cent) == result
+    ), f"Cent: {cent} should be converted to {result}"
