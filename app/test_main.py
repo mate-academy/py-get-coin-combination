ecba2b632b40c5d30bed104e3c3a0a9e39517375
turulko-oleksandr <SashaTurulko@i.ua>
2025-06-24 13:17:39 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -14,6 +14,9 @@ from app.main import get_coin_combination
                  id="Given 1 number of dimes returns [0, 0, 1, 0]"),
     pytest.param(25, [0, 0, 0, 1],
                  id="Given 1 number of quarters returns [0, 0, 0, 1]"),
+    pytest.param(41, [1, 1, 1, 1],
+                 id="Given 41 returns [1, 1, 1, 1]"),
+
 ])
 def test_get_coin_combination(number: int, expected: List[int]) -> None:
     assert get_coin_combination(number) == expected
