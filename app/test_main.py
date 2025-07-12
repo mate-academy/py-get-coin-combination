a4042d2971e2e83f605903c02699941b6283963b
Vadym Vizirov <vadimplus57@gmail.com>
2025-07-12 17:03:15 +0300

    get coin combination

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,27 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, result",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2])
+    ],
+    ids=[
+        "returns zero",
+        "returns one",
+        "returns one nickels",
+        "returns one dimes",
+        "returns two quarters"
+    ],
+)
+def test_coins_yard(cents: int, result: list) -> None:
+    assert get_coin_combination(cents) == result
+
+
+def test_on_len_result_list() -> None:
+    assert len(get_coin_combination(6)) == 4
