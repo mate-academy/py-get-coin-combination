2588898b7ef8f3ce443c9726723f2296d24dad7a
mr_eugene <roberttimethone@gmail.com>
2025-07-25 17:27:12 +0200

    sol

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,19 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+from app.main import get_coin_combination
+import pytest
+
+
+@pytest.mark.parametrize(
+    "coins,result",
+    [
+        (1, [1, 0, 0, 0]),
+        (0, [0, 0, 0, 0]),
+        (74, [4, 0, 2, 2]),
+        (-10, [0, 0, 0, 0]),
+        (900980, [0, 1, 0, 36039])
+    ]
+)
+def test_get_coin_combination(coins: int, result: list) -> None:
+    assert get_coin_combination(coins) == result
