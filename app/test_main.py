b1aea28e51f41b8b28019bdc04a99b298ee38e09
Maksym Pakula <gargantadoto@gmail.com>
2025-08-08 18:55:51 +0300

    solution for coin combination tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,31 @@
 from app.main import get_coin_combination
+import pytest
+
+@pytest.mark.parametrize(
+    "cents,expected",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (51, [1, 0, 0, 2]),
+        (58, [3, 1, 0, 2]),
+        (71, [1, 0, 2, 2])
+    ]
+)
+def test_coin_combination(cents, expected):
+    assert get_coin_combination(cents) == expected
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents,exception",
+    [
+        ({0: None}, TypeError),
+        ("1", TypeError),
+        ([1], TypeError),
+        ({1, 3}, TypeError),
+    ]
+)
+def test_exceptions_if_cents_not_integers(cents, exception):
+    with pytest.raises(exception):
+        get_coin_combination(cents)
