477d76b21edd9e74e555f4ca972e4736cd70b01a
Alex_Lushenko <mralealex9@gmail.com>
2025-08-23 17:09:46 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,15 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_get_coin_combination():
+    assert get_coin_combination(0) == [0, 0, 0, 0]
+    assert get_coin_combination(4) == [4, 0, 0, 0]
+    assert get_coin_combination(5) == [0, 1, 0, 0]
+    assert get_coin_combination(10) == [0, 0, 1, 0]
+    assert get_coin_combination(25) == [0, 0, 0, 1]
+    assert get_coin_combination(100) == [0, 0, 0, 4]
+    assert get_coin_combination(23) == [3, 0, 2, 0]
+    assert get_coin_combination(15) == [0, 1, 1, 0]
+    assert get_coin_combination(26) == [1, 0, 0, 1]
+
+            
