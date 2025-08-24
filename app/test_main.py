5f1615dcae1aec19ae9bfaea79351b3106878ddb
Zabodashka <igorsulga6@gmail.com>
2025-08-24 17:10:26 +0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,9 @@
-# Было:
-assert get_coin_combination(99) == [4, 0, 2, 3]
+from app.main import get_coin_combination
 
-# Должно быть (в порядке quarters, dimes, nickels, pennies):
-assert get_coin_combination(99) == [3, 2, 0, 4]
+
+def test_coin_combination() -> None:
+    """
+    Test the get_coin_combination function.
+    """
+    # 3 quarters, 2 dimes, 0 nickels, 4 pennies = 99 cents
+    assert get_coin_combination(99) == [4, 0, 2, 3]
