873a7b22091e7fa85d6ec40d1f4d747422e09186
Zabodashka <igorsulga6@gmail.com>
2025-08-24 16:30:26 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from typing import List
 
+
 def get_coin_combination(cents: int) -> List[int]:
+    """
+    Return the minimum number of US coins (pennies, nickels, dimes, quarters)
+    needed to make up the given amount in cents.
+
+    Output format: [pennies, nickels, dimes, quarters]
+    """
     quarters = cents // 25
     cents %= 25
 
@@ -12,4 +19,4 @@ def get_coin_combination(cents: int) -> List[int]:
 
     pennies = cents
 
-    return [quarters, dimes, nickels, pennies]
+    return [pennies, nickels, dimes, quarters]
