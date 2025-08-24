5f0149a054af0343da5a4a50f2ffe9607becc4a4
Zabodashka <igorsulga6@gmail.com>
2025-08-24 15:57:40 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,14 +4,9 @@ from typing import List
 def get_coin_combination(cents: int) -> List[int]:
     """
     Calculate minimum coins for the given cents.
+
     Returns list with pennies, nickels, dimes, quarters.
     """
-    pennies = cents % 5
-    nickels = (cents // 5) % 2  # максимум 1 или 0 (т.к. по условию могут быть только минимальные монеты)
-    dimes = (cents // 10) % 3   # максимум 2 или 0 или 1
-    quarters = cents // 25
-
-    # Но лучше считать правильно:
     quarters = cents // 25
     cents %= 25
 
