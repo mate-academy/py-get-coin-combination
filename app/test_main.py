9f282798ff7432b9c3140db6e02ee311a5b281a7
Zabodashka <igorsulga6@gmail.com>
2025-08-24 15:53:50 +0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -5,6 +5,10 @@ def test_coin_combination() -> None:
     """
     Test the get_coin_combination function.
     """
-    assert get_coin_combination(99) == [3, 2, 0, 4]  # 3 quarters, 2 dimes, 0 nickels, 4 pennies
+    # 3 quarters, 2 dimes, 0 nickels, 4 pennies
+    assert get_coin_combination(99) == [3, 2, 0, 4]
+
     assert get_coin_combination(0) == [0, 0, 0, 0]
+
+    # 1 quarter, 1 dime, 1 nickel, 1 penny
     assert get_coin_combination(41) == [1, 1, 1, 1]
