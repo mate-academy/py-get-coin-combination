2931fd462b6b009291b821abcfeb72bdcc4bca5d
Oleksandr Shkliar <shklyara29@gmail.com>
2025-08-25 19:49:06 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,30 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "amount,expected",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2])
+    ]
+)
+def test_should_combination_possible_number(amount: int,
+                                            expected: list) -> None:
+    assert get_coin_combination(amount) == expected
+
+
+@pytest.mark.parametrize(
+    "amount",
+    [
+        ("6", ),
+        (None, ),
+        (4.5, ),
+        (-3, )
+    ]
+)
+def test_should_input_be_integer(amount: int) -> None:
+    with pytest.raises(TypeError):
+        get_coin_combination(amount)
