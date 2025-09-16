4ae5d7d039b5cce60988de09cb4d452794cdf7d6
Gena <gena.anzin@ukr.net>
2025-09-16 16:12:03 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,25 @@
 from app.main import get_coin_combination
+import pytest
+
+
+@pytest.mark.parametrize(
+    "coins",
+    "result",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
+    ],
+    ids=[
+        "test for  cent",
+        "test for nickels "
+        "test for dime"
+        "test for quarters",
+    ]
+)
+def test_get_coin_combination(coins: int, result: int) -> None:
+    assert get_coin_combination(coins) == result
 
 
 # write your tests here
