95d4497ef03aedfdcf2295119d8e83e06792d227
0lex1y <kotov.oleksiy@icloud.com>
2025-09-19 00:21:26 +0300

    Test

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,18 @@
 from app.main import get_coin_combination
+import pytest
 
 
-# write your tests here
+@pytest.fixture
+def coins():
+    return [0, 0, 0, 0] # [penny, nickel, dime, quarter]
+
+
+@pytest.mark.parametrize("cents, expected", [
+    (1, [1, 0, 0, 0]),
+    (6, [1, 1, 0, 0]),
+    (17, [2, 1, 1, 0]),
+    (50, [0, 0, 0, 2]),
+]
+)
+def test_get_coin_combination(cents, expected, coins):
+    assert get_coin_combination(cents) == expected
