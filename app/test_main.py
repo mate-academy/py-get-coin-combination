cc81a56a9229380d0ff3168d81a0bed8f7e7e94d
0lex1y <kotov.oleksiy@icloud.com>
2025-09-19 00:33:20 +0300

    Ff

--- app/test_main.py
+++ app/test_main.py
@@ -3,8 +3,8 @@ import pytest
 
 
 @pytest.fixture
-def coins():
-    return [0, 0, 0, 0] # [penny, nickel, dime, quarter]
+def coins() -> list:
+    return [0, 0, 0, 0]  # [penny, nickel, dime, quarter]
 
 
 @pytest.mark.parametrize("cents, expected", [
@@ -13,6 +13,22 @@ def coins():
     (17, [2, 1, 1, 0]),
     (50, [0, 0, 0, 2]),
 ]
-)
-def test_get_coin_combination(cents, expected, coins):
+                         )
+def test_get_coin_combination(cents: int, expected: list, coins: list):
     assert get_coin_combination(cents) == expected
+
+
+def test_cents_only_integer() -> None:
+    with pytest.raises(TypeError):
+        get_coin_combination("0")
+    with pytest.raises(TypeError):
+        get_coin_combination(None)
+    with pytest.raises(TypeError):
+        get_coin_combination(2.5)
+
+
+def test_return_type_and_structure() -> None:
+    result = get_coin_combination(28)
+    assert isinstance(result, list)
+    assert len(result) == 4
+    assert all(isinstance(x, int) for x in result)
