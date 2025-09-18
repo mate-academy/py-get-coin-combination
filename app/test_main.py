34e6ed446623223aea6cf6250d77d2d95d79b571
0lex1y <kotov.oleksiy@icloud.com>
2025-09-19 00:35:01 +0300

    fFF

--- app/test_main.py
+++ app/test_main.py
@@ -12,9 +12,11 @@ def coins() -> list:
     (6, [1, 1, 0, 0]),
     (17, [2, 1, 1, 0]),
     (50, [0, 0, 0, 2]),
-]
-                         )
-def test_get_coin_combination(cents: int, expected: list, coins: list):
+])
+
+def test_get_coin_combination(cents: int,
+                              expected: list,
+                              coins: list) -> None:
     assert get_coin_combination(cents) == expected
 
 
