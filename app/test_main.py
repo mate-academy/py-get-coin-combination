29bde5119136876f1ab68a07e673e346ce35dc17
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-09-22 15:26:14 -0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -5,11 +5,11 @@ import pytest
 @pytest.mark.parametrize(
     "cents,result",
     [
-        (1, [1, 0, 0, 0] ),
-        (6, [1, 1, 0, 0] ),
-        (17, [2, 1, 1, 0] ),
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
         (50, [0, 0, 0, 2])
     ]
 )
-def test_get_coin_combination(cents: int, result: list[int]):
-    assert get_coin_combination(cents) == result, f"Expected {result} but got {get_coin_combination(cents)}"
+def test_get_coin_combination(cents: int, result: list[int]) -> None:
+    assert (get_coin_combination(cents) == result), f"Expected {result} but got {get_coin_combination(cents)}"
