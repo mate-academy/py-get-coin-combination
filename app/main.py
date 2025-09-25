cf8983530b926f662f9646db91fb26940be0ae67
Ілля Витовтов <izbastenovarsen@gmail.com>
2025-09-25 19:36:50 +0300

    fix a problem5

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-def get_coin_combination(cents: int) -> list[int]:
+from typing import List
+
+
+def get_coin_combination(cents: int) -> List[int]:
     if not isinstance(cents, int):
         raise TypeError("Input must be an integer")
     if cents < 0:
@@ -6,10 +9,13 @@ def get_coin_combination(cents: int) -> list[int]:
 
     quarters = cents // 25
     cents %= 25
+
     dimes = cents // 10
     cents %= 10
+
     nickels = cents // 5
     cents %= 5
+
     pennies = cents
 
     return [pennies, nickels, dimes, quarters]
