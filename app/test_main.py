7a39fcaf5dd6ed00ceb586408537a39cfe0304de
Vasyl Cherkes <cherkesva@gmail.com>
2025-10-03 22:08:59 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -10,7 +10,9 @@ from app.main import get_coin_combination
         (1, [1, 0, 0, 0]),
         (6, [1, 1, 0, 0]),
         (17, [2, 1, 1, 0]),
-        (50, [0, 0, 0, 2])
+        (50, [0, 0, 0, 2]),
+        (0, [0, 0, 0, 0]),
+        (99, [4, 0, 2, 3])
     ]
 )
 def test_get_coin_combination(cents: int, values: int) -> None:
