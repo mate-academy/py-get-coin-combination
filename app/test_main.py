310a21d49b30713cfa9f8b0f7379c487263125b6
Maksym Dazidov <maksymdazidov@gmail.com>
2025-10-17 17:33:22 +0200

    tests 1

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,24 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "value,result",
+    [
+        (41, [1, 1, 1, 1]),
+        (4, [4, 0, 0, 0]),
+        (5, [0, 1, 0, 0]),
+        (9, [4, 1, 0, 0]),
+        (10, [0, 0, 1, 0]),
+        (24, [4, 0, 2, 0]),
+        (25, [0, 0, 0, 1]),
+        (100, [0, 0, 0, 4]),
+        (0, [0, 0, 0, 0])
+
+    ]
+)
+def test_should_check_that_func_return_diff_coins(
+        value,
+        result
+) -> None:
+    assert get_coin_combination(value) == result
