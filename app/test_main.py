605164ad45b5bb54fc86eeae0fffe6cddb517a47
Yevhen Dmytrenko <zdmitrenko26@gmail.com>
2025-10-18 00:01:02 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,14 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
 # write your tests here
+def test_cannot_use_str_as_cents() -> None:
+    with pytest.raises(TypeError):
+        get_coin_combination(cents="50")
+
+
+def test_cannot_use_float_as_cents() -> None:
+    with pytest.raises(TypeError):
+        get_coin_combination(cents=25.3)
