d15adf4e01c2948e39917de59e047172995df389
Luan <luan.valentino78@gmail.com>
2025-10-22 12:58:38 -0300

    Commit

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,19 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents_input, expected_output",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
+        (41, [1, 1, 1, 1]),
+        (0, [0, 0, 0, 0]),
+        (25, [0, 0, 0, 1]),
+        (99, [4, 0, 2, 3]),
+    ],
+)
+def test_get_coin_combination(cents_input, expected_output):
+    assert get_coin_combination(cents_input) == expected_output
