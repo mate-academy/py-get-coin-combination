5b4bc1e487dfe4e6eac29ddc056d9ff4f0d5dc06
Anastasiia Pohonets <29karasik@gmail.com>
2025-10-24 16:39:23 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -8,10 +8,10 @@ class TestCoinCombination:
         "cents,expected",
         [
             (1, [1, 0, 0, 0]),
-            (6, [1, 1, 0, 0] ),
+            (6, [1, 1, 0, 0]),
             (17, [2, 1, 1, 0]),
             (50, [0, 0, 0, 2])
         ]
     )
-    def test_get_coin_combination(self, cents, expected):
+    def test_get_coin_combination(self, cents: int, expected: list) -> None:
         assert get_coin_combination(cents) == expected
