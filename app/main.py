703e0610fd6e8fbdf705d2a5d46ed8501c8e8f44
venlivan <venlivandovskyi@gmail.com>
2025-10-28 16:09:30 +0100

    added check of the type of data passed to the function

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def get_coin_combination(cents: int) -> list:
     values = [1, 5, 10, 25]
     coins = [0, 0, 0, 0]
-
+    if not isinstance(cents, int):
+        raise TypeError("cents should be an int")
     for i in range(3, -1, -1):
         coins[i] = cents // values[i]
         cents -= coins[i] * values[i]
