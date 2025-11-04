cfa248feddc7f70041e3e3cdf7de542c81b1a498
Alex <Rieznik.O.A@nmu.one>
2025-11-04 21:52:57 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,26 @@
 from app.main import get_coin_combination
+import pytest
+
+parametrize_dict = {
+    1: [1, 0, 0, 0], 6: [1, 1, 0, 0],
+    17: [2, 1, 1, 0], 50: [0, 0, 0, 2]
+}
 
 
-# write your tests here
+def test_raise_error_when_cents_is_negative() -> None:
+    with pytest.raises(ValueError):
+        get_coin_combination(cents=-1)
+
+
+def test_raise_error_when_cents_is_zero() -> None:
+    with pytest.raises(ValueError):
+        get_coin_combination(cents=0)
+
+
+@pytest.mark.parametrize(
+    "cents,expected",
+    [(1, parametrize_dict[1]), (6, parametrize_dict[6]),
+     (17, parametrize_dict[17]), (50, parametrize_dict[50])]
+)
+def can_get_absolute_combination(cents: int, expected: list) -> None:
+    assert get_coin_combination(cents) == expected
