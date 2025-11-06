76724dacf8eeca99dee8f8488da2c5c76a269391
AndrzejPeszel <andrzej.peszel@icloud.com>
2025-11-06 11:24:00 +0100

    fixed solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,22 +2,42 @@ import pytest
 from app.main import get_coin_combination
 
 
-@pytest.mark.parametrize("cents: int, expected: list", [
-    (0, [0, 0, 0, 0]),               # Brak monet
-    (1, [1, 0, 0, 0]),               # 1 penny
-    (4, [4, 0, 0, 0]),               # tylko pennies
-    (5, [0, 1, 0, 0]),               # 1 nickel
-    (6, [1, 1, 0, 0]),               # 1 penny + 1 nickel
-    (10, [0, 0, 1, 0]),              # 1 dime
-    (17, [2, 1, 1, 0]),              # 2 pennies + 1 nickel + 1 dime
-    (25, [0, 0, 0, 1]),              # 1 quarter
-    (30, [0, 1, 0, 1]),              # 1 quarter + 1 nickel
-    (41, [1, 1, 1, 1]),              # 1 z każdej monety
-    (49, [4, 0, 2, 1]),              # 1 quarter + 2 dimes + 4 pennies
-    (50, [0, 0, 0, 2]),              # 2 quarters
-    (99, [4, 0, 2, 3]),              # 3 quarters + 2 dimes + 4 pennies
-    (100, [0, 0, 0, 4]),             # 4 quarters
-    (123, [3, 0, 2, 4]),             # 4 quarters + 2 dimes + 3 pennies
+@pytest.mark.parametrize("cents, expected", [
+    (0, [0, 0, 0, 0]),
+    (1, [1, 0, 0, 0]),
+    (4, [4, 0, 0, 0]),
+    (5, [0, 1, 0, 0]),
+    (6, [1, 1, 0, 0]),
+    (10, [0, 0, 1, 0]),
+    (17, [2, 1, 1, 0]),
+    (25, [0, 0, 0, 1]),
+    (30, [0, 1, 0, 1]),
+    (41, [1, 1, 1, 1]),
+    (49, [4, 0, 2, 1]),
+    (50, [0, 0, 0, 2]),
+    (99, [4, 0, 2, 3]),
+    (100, [0, 0, 0, 4]),
+    (123, [3, 0, 2, 4]),
 ])
 def test_get_coin_combination(cents: int, expected: list) -> None:
     assert get_coin_combination(cents) == expected
+
+
+def test_only_pennies() -> None:
+    assert get_coin_combination(3) == [3, 0, 0, 0]
+
+
+def test_only_nickels() -> None:
+    assert get_coin_combination(5) == [0, 1, 0, 0]
+
+
+def test_only_dimes() -> None:
+    assert get_coin_combination(20) == [0, 0, 2, 0]
+
+
+def test_only_quarters() -> None:
+    assert get_coin_combination(75) == [0, 0, 0, 3]
+
+
+def test_combination_of_all_coin_types() -> None:
+    assert get_coin_combination(41) == [1, 1, 1, 1]
