8d1de04bfb388a3a6eb8277cb3aed03922a9d03d
Taras Ivashchuk <tvishy@disroot.org>
2025-11-11 22:36:23 +0200

    Initial commit

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,21 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_number_of_pennies() -> None:
+    assert get_coin_combination(1)[0] == 1
+
+
+def test_number_of_nickels() -> None:
+    assert get_coin_combination(5)[1] == 1
+
+
+def test_number_of_dimes() -> None:
+    assert get_coin_combination(10)[2] == 1
+
+
+def test_number_of_quarters() -> None:
+    assert get_coin_combination(25)[3] == 1
+
+
+def test_coin_combination() -> None:
+    assert get_coin_combination(17) == [2, 1, 1, 0]
