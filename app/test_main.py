02274f1253006b9b3363d959dd86480f74f103b2
Oleh-Fr <frasyniuk.oleh.dev@gmail.com>
2025-11-11 16:55:42 +0000

    first test

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,23 @@
+import pytest
 from app.main import get_coin_combination
 
+@pytest.mark.parametrize(
+    "coins_number, expected",
+    [
+        [1, [1, 0, 0, 0]],
+        [6, [1, 1, 0, 0]],
+        [17, [2, 1, 1, 0]],
+        [50, [0, 0, 0, 2]]
+    ]
+)
+def test_get_coin_correct(coins_number: int, expected: list[int]) -> None:
+    result = get_coin_combination(coins_number)
+    assert result == expected
 
-# write your tests here
+# @pytest.mark.parametrize(
+#     "coins_number, expected",
+#     ["five", ValueError]
+# )
+# def test_get_coin_incorrect(coins_number: str, expected: Exception) -> None:
+#     with pytest.raises(ValueError):
+#         get_coin_combination(coins_number)
