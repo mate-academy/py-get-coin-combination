b7ab356c18e9a4cf2a9cb6d394c93bb995a50626
Vadim <vadimchubok@gmail.com>
2025-11-12 17:54:40 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -3,16 +3,16 @@ from app.main import get_coin_combination
 
 
 @pytest.mark.parametrize("input_cents,expected_output,exception", [
-    pytest.param(1, [1, 0, 0, 0], None, id="Should return [1, 0, 0, 0] if "
-                                           "input_cents is 1"),
-    pytest.param(6, [1, 1, 0, 0], None, id="Should return [1, 1, 0, 0] if "
-                                           "input_cents is 6"),
-    pytest.param(17, [2, 1, 1, 0], None, id="Should return [2, 1, 1, 0] if "
-                                           "input_cents is 17"),
-    pytest.param(50, [0, 0, 0, 2], None, id="Should return [0, 0, 0, 2] if "
-                                           "input_cents is 50"),
-    pytest.param("a", None, TypeError, id="Should return None if "
-                                          "input_cents are not integer"),
+    pytest.param(1, [1, 0, 0, 0], None,
+                 id="Should return [1, 0, 0, 0] if input_cents is 1"),
+    pytest.param(6, [1, 1, 0, 0], None,
+                 id="Should return [1, 1, 0, 0] if input_cents is 6"),
+    pytest.param(17, [2, 1, 1, 0], None,
+                 id="Should return [2, 1, 1, 0] if input_cents is 17"),
+    pytest.param(50, [0, 0, 0, 2], None,
+                 id="Should return [0, 0, 0, 2] if input_cents is 50"),
+    pytest.param("a", None, TypeError,
+                 id="Should return None if input_cents are not integer"),
 ])
 def test_get_coin_combination(input_cents: int,
                               expected_output: list,
