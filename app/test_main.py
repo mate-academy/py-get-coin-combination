c785c735c72ef7aefb6cd08f843da31e70642250
Andrii Chychkan <mywork1728@gmail>
2025-11-12 16:09:44 +0200

    added tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,24 @@
 from app.main import get_coin_combination
+import pytest
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "value, result",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2])
+    ],
+    ids=[
+        "only one cents",
+        "6 cents",
+        "17 cents",
+        "50 cents"
+    ]
+)
+def test_can_calculating_smallest_possible_coins(value: int,
+                                                 result: list) -> None:
+    assert (
+        get_coin_combination(value) == result
+    ), f"Coin combination from '{value}' should be equal '{result}'"
