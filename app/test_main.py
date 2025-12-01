8b9ad21617786435442ee46b6b4e4cf38cb35f85
Paweł Dudek <dudek136@gmail.com>
2025-12-01 19:48:45 +0100

    Solution v2

--- app/test_main.py
+++ app/test_main.py
@@ -6,10 +6,10 @@ from app.main import get_coin_combination
 @pytest.mark.parametrize(
     "cents,coins",
     [
-        (1, [1,0,0,0]),
-        (6, [1,1,0,0]),
-        (16, [1,1,1,0]),
-        (41, [1,1,1,1])
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (16, [1, 1, 1, 0]),
+        (41, [1, 1, 1, 1])
     ],
     ids=[
         "penny",
@@ -18,5 +18,5 @@ from app.main import get_coin_combination
         "penny + nickel+dime+quarter"
     ]
 )
-def test_get_coin_combination(cents:int, coins: list) -> None:
+def test_get_coin_combination(cents: int, coins: list) -> None:
     assert get_coin_combination(cents) == coins
