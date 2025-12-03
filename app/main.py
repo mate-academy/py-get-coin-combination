bf0fea945ff7c081ffada5e7847e1e2c5a4149e2
Saulo Rodrigo <saulorodrigocfs@gmail.com>
2025-12-03 14:38:24 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 def get_coin_combination(cents: int) -> list:
+    if not isinstance(cents, int):
+        raise TypeError()
+    if cents < 0:
+        raise ValueError()
+
     values = [1, 5, 10, 25]
     coins = [0, 0, 0, 0]
-
+    
     for i in range(3, -1, -1):
         coins[i] = cents // values[i]
         cents -= coins[i] * values[i]
