2c2ebeebc46bcb76fd5bd630f292d1780bc64489
Darmi555 <mateusz.rudnicki05@wp.pl>
2025-12-19 19:33:56 +0100

    xd

--- app/test_main.py
+++ app/test_main.py
@@ -5,25 +5,25 @@ import pytest
 @pytest.mark.parametrize(
     "cents, expected",
     [
-        (0, [0,0,0,0]),
-        (1, [1,0,0,0]),
-        (5, [0,1,0,0]),
-        (10, [0,0,1,0]),
-        (25, [0,0,0,1]),
-        (6, [1,1,0,0]),
-        (17, [2,1,1,0]),
-        (99, [4,0,2,3]),
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (5, [0, 1, 0, 0]),
+        (10, [0, 0, 1, 0]),
+        (25, [0, 0, 0, 1]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (99, [4, 0, 2, 3]),
     ]
 )
-def test_various_combinations(cents, expected):
+def test_various_combinations(cents: int, expected: list) -> None:
     assert get_coin_combination(cents) == expected
 
 
-def test_for_negative_value():
+def test_for_negative_value() -> None:
     with pytest.raises(ValueError):
         get_coin_combination(-1)
 
 
-def test_for_non_integer_value():
+def test_for_non_integer_value() -> None:
     with pytest.raises(TypeError):
         get_coin_combination("1")
