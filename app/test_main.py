83b809a4659cd3a35521a4570e9111652bd3b391
Denys Herasymchuk <herasymchukdenyss@gmail.com>
2025-12-21 01:20:12 +0100

    Test fixing

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,10 @@
 import pytest
+from typing import Type, Any
 
 from app.main import get_coin_combination
 
 
-cases = [
-    (-10, [0, 0, 0, 0]),
-    (-1, [0, 0, 0, 0]),
+valid_cases = [
     (0, [0, 0, 0, 0]),
     (1, [1, 0, 0, 0]),
     (5, [0, 1, 0, 0]),
@@ -26,8 +25,8 @@ cases = [
 
 @pytest.mark.parametrize(
     "cents,coins",
-    cases,
-    ids=[f"{c} cents -> {coins}" for c, coins in cases]
+    valid_cases,
+    ids=[f"{c} cents -> {coins}" for c, coins in valid_cases]
 )
 def test_get_coin_combination_returns_correct_result(
         cents: int,
@@ -36,6 +35,24 @@ def test_get_coin_combination_returns_correct_result(
     assert get_coin_combination(cents) == coins
 
 
-def test_get_coin_combination_raises_error_when_not_valid_cents() -> None:
-    with pytest.raises(TypeError):
-        get_coin_combination("12")
+invalid_cases = [
+    (-5, ValueError),
+    (-1, ValueError),
+    ("5", TypeError),
+    (1.5, TypeError),
+    (True, TypeError),
+    ((1, 2, 3, 4), TypeError)
+]
+
+
+@pytest.mark.parametrize(
+    "cents,expected_error",
+    invalid_cases,
+    ids=[f"{c} cents -> {error.__name__}" for c, error in invalid_cases]
+)
+def test_get_coin_combination_raises_error_when_not_valid_cents(
+        cents: Any,
+        expected_error: Type[BaseException]
+) -> None:
+    with pytest.raises(expected_error):
+        get_coin_combination(cents)
