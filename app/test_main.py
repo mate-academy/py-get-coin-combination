8da8b26508c13ad880fdf4481ddc52b9add7a2a6
Никита <Goldbergnik1993@gmail.com>
2025-12-22 15:59:25 +0100

    tests3

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,3 @@
-from typing import List
-
 import pytest
 from app.main import get_coin_combination
 
@@ -7,6 +5,7 @@ from app.main import get_coin_combination
 @pytest.mark.parametrize(
     "amount, expected",
     [
+        (0, [0, 0, 0, 0]),
         (1, [1, 0, 0, 0]),
         (5, [0, 1, 0, 0]),
         (10, [0, 0, 1, 0]),
@@ -15,5 +14,5 @@ from app.main import get_coin_combination
         (99, [4, 0, 2, 3]),
     ],
 )
-def test_get_coin_combination(amount: int, expected: List[int]) -> None:
+def test_get_coin_combination(amount: int, expected: list) -> None:
     assert get_coin_combination(amount) == expected
