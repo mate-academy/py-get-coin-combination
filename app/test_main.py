a025df7c54fb0466ab57448777a076873925b33e
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-12-23 17:30:42 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,15 +4,11 @@ from app.main import get_coin_combination
 
 
 @pytest.mark.parametrize("cents, expected", [
-    (0, [0, 0, 0 ,0]),
-    (1, [1, 0, 0 ,0]),
-    (6, [1, 1, 0 ,0]),
-    (17, [2, 1, 1 ,0]),
+    (0, [0, 0, 0, 0]),
+    (1, [1, 0, 0, 0]),
+    (6, [1, 1, 0, 0]),
+    (17, [2, 1, 1, 0]),
     (50, [0, 0, 0, 2])
 ])
 def test_get_coin_combination(cents: int, expected: list) -> None:
     assert get_coin_combination(cents) == expected
-
-def test_should_return_different_coins():
-    result = get_coin_combination(6)
-    if result[0] > 5 or result[1] > 5:
