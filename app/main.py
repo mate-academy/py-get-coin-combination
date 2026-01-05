b1ce810e1289169f2a58be3aa6ad743102a0f7d2
Alexandre Schmid <alex@alxcloudit.com>
2026-01-05 17:41:52 -0500

    Solution

--- app/main.py
+++ app/main.py
@@ -2,6 +2,12 @@ def get_coin_combination(cents: int) -> list:
     values = [1, 5, 10, 25]
     coins = [0, 0, 0, 0]
 
+    if not isinstance(cents, int):
+        raise TypeError("Amount must be an integer.")
+
+    if cents < 0:
+        raise ValueError("Amount cannot be negative.")
+
     for i in range(3, -1, -1):
         coins[i] = cents // values[i]
         cents -= coins[i] * values[i]
--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,45 @@
 from app.main import get_coin_combination
 
+import pytest
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "amount, expected",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2])
+    ]
+)
+def test_get_coin_combination(amount, expected) -> None:
+    assert get_coin_combination(amount) == expected
+
+
+@pytest.mark.parametrize(
+    "amount",
+    [
+        -1,
+        -10,
+        -50
+    ]
+)
+def test_get_coin_combination_negative_amount(amount: int) -> None:
+    with pytest.raises(ValueError):
+        get_coin_combination(amount)
+
+
+@pytest.mark.parametrize(
+    "amount",
+    [
+        1.5,
+        7.8,
+        "10",
+        None,
+        [25, 10]
+    ]
+)
+def test_get_coin_combination_invalid_type(amount) -> None:
+    with pytest.raises(TypeError):
+        get_coin_combination(amount)
