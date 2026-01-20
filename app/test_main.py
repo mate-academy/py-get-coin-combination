e5ee43dea783edb5c709050928a0700f86440ae7
Daniil Nosov <nosov281@gmail.com>
2026-01-20 18:35:59 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,14 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_return_only_one_coin():
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+
+def test_return_two_type_of_coins():
+    assert get_coin_combination(6) == [1, 1, 0, 0]
+
+def test_return_three_type_of_coins():
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+
+def test_return_four_type_of_coins():
+    assert get_coin_combination(67) == [2, 1, 1, 2]
