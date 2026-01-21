0e6c83f73454f209b199d58b45199c4ceb81ae30
Felipe Pessin <fhpessin@gmail.com>
2026-01-21 11:23:06 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
-def get_coin_combination(cents: int) -> list:
-    values = [1, 5, 10, 25]
-    coins = [0, 0, 0, 0]
+def get_coin_combination(cents: int) -> list[int]:
+    if cents < 0:
+        return [0, 0, 0, 0]
 
-    for i in range(3, -1, -1):
-        coins[i] = cents // values[i]
-        cents -= coins[i] * values[i]
+    quarters = cents // 25
+    cents %= 25
+    dimes = cents // 10
+    cents %= 10
+    nickels = cents // 5
+    pennies = cents % 5
 
-    return coins
+    return [pennies, nickels, dimes, quarters]
