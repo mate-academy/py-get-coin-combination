52907b1d8dc8a62b4dc5fc613914c6f288a8d874
Andriy <andrijbuluk@gmail.com>
2026-01-23 18:54:52 +0100

    Get coin combination

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,39 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, coins",
+    [
+        pytest.param(
+            0, [0, 0 ,0 ,0],
+            id="Return Zero coins"
+        ),
+        pytest.param(
+            1, [1, 0, 0, 0],
+            id="Return one coin"
+        ),
+        pytest.param(
+            10, [0, 0, 1, 0],
+            id="Return 10 coins"
+        ),
+        pytest.param(
+            17, [2, 1, 1, 0],
+            id="Return 17 coins"
+        ),
+        pytest.param(
+            50, [0, 0, 0, 2],
+            id="Return 50 coins"
+        ),
+        pytest.param(
+            100, [0, 0, 0, 4],
+            id="Return 100 coins"
+        )
+
+    ]
+)
+def test_get_coin_combination(
+        cents: int,
+        coins: list[int],
+)-> None:
+    assert get_coin_combination(cents) == coins
