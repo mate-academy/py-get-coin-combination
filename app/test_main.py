c22319352ac6f28b966c27d47e623a83345dc670
Redum9 <dumaneckijdenis@gmail.com>
2026-01-29 17:35:19 +0200

    Resolve

--- app/test_main.py
+++ app/test_main.py
@@ -10,6 +10,5 @@ from app.main import get_coin_combination
         (17, [2, 1, 1, 0]),
         (50, [0, 0, 0, 2]),
     ])
-
 def test_money_converter(coin: int, expected_result: list) -> None:
     assert get_coin_combination(coin) == expected_result
