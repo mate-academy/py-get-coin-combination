107d9d5d6aff4cb03a155dc0c33dbd059f3852bd
Mafialate <mafialate@ukr.net>
2026-02-01 20:58:21 +0100

    Add tests for get_coin_combination function

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,32 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, expected",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (5, [0, 1, 0, 0]),
+        (10, [0, 0, 1, 0]),
+        (25, [0, 0, 0, 1]),
+        (41, [1, 1, 1, 1]),
+        (45, [0, 0, 2, 1]),
+        (60, [0, 0, 1, 2]),
+        (123, [3, 0, 2, 4])
+    ],
+    ids=[
+        "test_returns_expected_coins_for_0",
+        "test_returns_expected_coins_for_1",
+        "test_returns_expected_coins_for_5",
+        "test_returns_expected_coins_for_10",
+        "test_returns_expected_coins_for_25",
+        "test_returns_expected_coins_for_41",
+        "test_returns_expected_coins_for_45",
+        "test_returns_expected_coins_for_60",
+        "test_returns_expected_coins_for_123",
+    ]
+)
+def test_get_coin_combination(cents: int, expected: list[int]) -> None:
+    assert get_coin_combination(cents) == expected
