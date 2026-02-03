e154aed554b3c4f0b513657d5f71d7797f701bd8
Seroga <ilchuk.cergey@gmail.com>
2026-02-03 19:39:43 +0200

    solition2

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,7 @@
 import pytest
+from typing import Any
 from app.main import get_coin_combination
+
 
 @pytest.mark.parametrize(
     "coin, expected",
@@ -10,7 +12,7 @@ from app.main import get_coin_combination
         (50, [0, 0, 0, 2]),
     ]
 )
-def test_get_coin_combination(coin, expected):
+def test_get_coin_combination(coin: int, expected: list[int]) -> None:
     assert get_coin_combination(coin) == expected
 
 
@@ -21,9 +23,13 @@ def test_get_coin_combination(coin, expected):
         pytest.param("1", TypeError, id="TypeError"),
     ]
 )
-def test_get_coin_combination_error(coin, expected_error):
+def test_get_coin_combination_error(
+        coin: type[Any],
+        expected_error: type[Exception]
+) -> None:
     with pytest.raises(expected_error):
         get_coin_combination(coin)
 
-def test_get_coin_combination_should_return_len_result_4():
+
+def test_get_coin_combination_should_return_len_result_4() -> None:
     assert len(get_coin_combination(1)) == 4
