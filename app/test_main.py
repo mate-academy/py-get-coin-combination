830c2caf75d70dd9011c7337f9c18dbb37331774
Ivan <ivanqqq@ukr.net>
2026-02-18 14:20:42 +0100

    fix 2

--- app/test_main.py
+++ app/test_main.py
@@ -9,13 +9,15 @@ import pytest
         (6, [1, 1, 0, 0]),
         (17, [2, 1, 1, 0]),
         (50, [0, 0, 0, 2]),
+        (41, [1, 1, 1, 1])
     ],
     ids=[
         "1 penny",
         "1 penny + 1 nickel",
         "2 pennies + 1 nickel + 1 dime",
         "2 quarters",
+        "1 penny + 1 nickel + 1 dime + 1 quarter"
     ]
 )
-def test_get_coin_combination(cents, expected):
+def test_get_coin_combination(cents: int, expected: int) -> None:
     assert get_coin_combination(cents) == expected
