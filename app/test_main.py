2184ca5a81b38cdde57d3827cf413a55af6caaf8
Ivan <ivanqqq@ukr.net>
2026-02-18 13:08:39 +0100

    fix 1

--- app/test_main.py
+++ app/test_main.py
@@ -5,10 +5,10 @@ import pytest
 @pytest.mark.parametrize(
     "cents, expected",
     [
-        (1, [1, 0, 0, 0]),   # 1 penny
-        (6, [1, 1, 0, 0]),   # 1 penny + 1 nickel
-        (17, [2, 1, 1, 0]),  # 2 pennies + 1 nickel + 1 dime
-        (50, [0, 0, 0, 2]),  # 2 quarters
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
     ],
     ids=[
         "1 penny",
