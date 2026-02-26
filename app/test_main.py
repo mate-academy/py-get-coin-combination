77a2e68f472ae225716796655a9e62b963bce20c
Oleksandr Simochko <vhsb721@gmail.com>
2026-02-26 11:46:34 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,29 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_zero():
+    assert get_coin_combination(0) == [0, 0, 0, 0]
+
+
+@pytest.mark.parametrize(
+    "cents, expected",
+    [
+        (1, [1, 0, 0, 0]),
+        (4, [4, 0, 0, 0]),  # boundary before nickel
+        (5, [0, 1, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (9, [4, 1, 0, 0]),  # boundary before dime
+        (10, [0, 0, 1, 0]),
+        (14, [4, 0, 1, 0]),
+        (15, [0, 1, 1, 0]),
+        (24, [4, 0, 2, 0]),  # boundary before quarter
+        (25, [0, 0, 0, 1]),
+        (26, [1, 0, 0, 1]),
+        (30, [0, 1, 0, 1]),
+        (50, [0, 0, 0, 2]),
+        (99, [4, 0, 2, 3]),
+    ]
+)
+def test_coin_combinations(cents, expected):
+    assert get_coin_combination(cents) == expected
