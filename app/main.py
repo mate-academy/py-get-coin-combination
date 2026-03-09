2699a37001af516307b5538548487efbf9de81af
Marek Biela <marekbiela26@gmail.com>
2026-03-09 21:00:51 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 def get_coin_combination(cents: int) -> list:
     values = [1, 5, 10, 25]
     coins = [0, 0, 0, 0]
-
+    if not isinstance(cents, int):
+        raise TypeError("Expected an integer")
+    if cents < 0:
+        raise ValueError("Value cannot be less than 0")
     for i in range(3, -1, -1):
         coins[i] = cents // values[i]
         cents -= coins[i] * values[i]
--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,27 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize("value,expected_results", [
+    (0, [0, 0, 0, 0]),
+    (1, [1, 0, 0, 0]),
+    (50, [0, 0, 0, 2]),
+    (6, [1, 1, 0, 0]),
+    (17, [2, 1, 1, 0]),
+    (24, [4, 0, 2, 0]),
+    (1000, [0, 0, 0, 40]),
+    (99, [4, 0, 2, 3])
+])
+def tests(value: int, expected_results: list) -> None:
+    assert get_coin_combination(value) == expected_results
+
+
+def test_negative() -> None:
+    with pytest.raises(ValueError):
+        get_coin_combination(-3)
+
+
+def test_not_integer() -> None:
+    with pytest.raises(TypeError):
+        get_coin_combination(3.45)
