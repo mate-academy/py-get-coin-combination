ae1e5a573a460084f2c3893782dbbaece04bd566
alemonnjy222 <71006485+alemonnjy222@users.noreply.github.com>
2026-04-19 10:31:51 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,21 @@
 from app.main import get_coin_combination
 
 
-def test_should_return_zero():
+def test_should_return_zero() -> None:
     assert get_coin_combination(0) == [0, 0, 0, 0]
 
-def test_return_only_penny():
+
+def test_return_only_penny() -> None:
     assert get_coin_combination(1) == [1, 0, 0, 0]
 
-def test_six_cents():
+
+def test_six_cents() -> None:
     assert get_coin_combination(6) == [1, 1, 0, 0]
 
-def test_seventeen_cents():
+
+def test_seventeen_cents() -> None:
     assert get_coin_combination(17) == [2, 1, 1, 0]
 
-def test_fifty_cents():
+
+def test_fifty_cents() -> None:
     assert get_coin_combination(50) == [0, 0, 0, 2]
