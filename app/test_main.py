308b526bf6045011cb92238c764d153f2eb6d08c
Piotr Ploskonka <pploskonka7@gmail.com>
2026-05-05 21:59:31 +0200

    Fixed Flake 8 error

--- app/test_main.py
+++ app/test_main.py
@@ -10,12 +10,12 @@ from app.main import get_coin_combination
         (5, [0, 1, 0, 0]),
         (10, [0, 0, 1, 0]),
         (25, [0, 0, 0, 1]),
-        (6, [1, 1, 0, 0]),   # 1 penny + 1 nickel
-        # (17, [2, 1, 1, 0]),  # 2 pennies + 1 nickel + 1 dime
-        # (67, [2, 1, 1, 2]),
+        (6, [1, 1, 0, 0]),  # 1 penny + 1 nickel
+        (17, [2, 1, 1, 0]),  # 2 pennies + 1 nickel + 1 dime
+        (67, [2, 1, 1, 2]),
     ]
 )
 def test_get_coin_combination(value: int, coins: list) -> None:
     assert (
-            get_coin_combination(value) == coins
+        get_coin_combination(value) == coins
     ), f"Wrong coin combination: {value}"
