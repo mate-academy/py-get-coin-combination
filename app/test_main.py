21e040cfc95bb5a3d31923dab59b3cb9e20605cf
Alex <alex@example.com>
2026-05-19 16:17:05 +0300

    finish 1

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,30 @@
 from app.main import get_coin_combination
+import pytest
 
 
-# write your tests here
+@pytest.mark.parametrize(
+   "cents,expected",
+   [
+       (1, [1, 0, 0, 0]),
+       (6, [1, 1, 0, 0]),
+       (17, [2, 1, 1, 0]),
+       (50, [0, 0, 0, 2]),
+       (16, [1, 1, 1, 0]),
+       (41, [1, 1, 1, 1]),
+       (11, [1, 0, 1, 0]),
+       (30, [0, 1, 0, 1])
+   ]
+)
+def test_coin(cents, expected):
+    assert get_coin_combination(cents) == expected
+
+
+def test_not_only_pennies():
+    assert get_coin_combination(6) != [6, 0, 0, 0]
+
+
+def test_uses_different_coins():
+    result = get_coin_combination(41)
+    assert result[-1] == 1
+    assert result[1] == 1
+    assert sum(result) < 41
