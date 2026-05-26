86ee8d857d1d74dcdcff88c57deb10a2e85195b2
fanghey <2603Lysenko2603@gmail.com>
2026-05-26 11:55:18 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -2,12 +2,14 @@ from app.your_module import get_coin_combination
 
 
 @pytest.mark.parametrize(
-   "cents","values",
-   [
-      (1, [1, 0, 0, 0]),
-      (6, [1, 1, 0, 0]),
-      (17, [2, 1, 1, 0]),
-      (50, [0, 0, 0, 2])
-      
-   ]
+    "cents,expected",  # один рядок з комою
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2])
+    ]
 )
+def test_get_coin_combination(cents, expected):
+    assert get_coin_combination(cents) == expected
+   
