8c56fed26152c897d915c5790c342f45fdcbd86c
fanghey <2603Lysenko2603@gmail.com>
2026-05-26 11:53:04 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,13 @@
-from app.main import get_coin_combination
+from app.your_module import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+   "cents","values",
+   [
+      (1, [1, 0, 0, 0]),
+      (6, [1, 1, 0, 0]),
+      (17, [2, 1, 1, 0]),
+      (50, [0, 0, 0, 2])
+      
+   ]
+)
