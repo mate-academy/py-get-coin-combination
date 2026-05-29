d44f00cea742b08a1180ae35f4fdfa3263e5a11a
GitJaouad <jlaourayed@gmail.com>
2026-05-29 19:20:56 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -2,8 +2,25 @@ def get_coin_combination(cents: int) -> list:
     values = [1, 5, 10, 25]
     coins = [0, 0, 0, 0]
 
+    if isinstance(cents, str):
+        raise TypeError  
+    if cents < 0:
+        raise ValueError
     for i in range(3, -1, -1):
         coins[i] = cents // values[i]
         cents -= coins[i] * values[i]
 
     return coins
+
+print(get_coin_combination(0))
+print(get_coin_combination(1))
+print(get_coin_combination(5))
+print(get_coin_combination(10))
+print(get_coin_combination(25))
+print(get_coin_combination(26))
+print(get_coin_combination(30))
+print(get_coin_combination(35))
+print(get_coin_combination(40))
+print(get_coin_combination(41))
+print(get_coin_combination(-5))
+print(get_coin_combination("ab"))
--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,27 @@
+import pytest
 from app.main import get_coin_combination
 
+@pytest.mark.parametrize(
+    "cents, expected",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (5, [0, 1, 0, 0]),
+        (10, [0, 0, 1, 0]),
+        (25, [0, 0, 0, 1]),
+        (26, [1, 0, 0, 1]),
+        (30, [0, 1, 0, 1]),
+        (35, [0, 0, 1, 1]),
+        (40, [0, 1, 1, 1]),
+        (41, [1, 1, 1, 1]),
+        (-5, ValueError),
+        ("a", TypeError),
+    ]
+)
 
-# write your tests here
+def test_get_coin_combination(cents, expected) -> None:
+    if isinstance(expected, type) and issubclass(expected, BaseException):
+        with pytest.raises(expected):
+            get_coin_combination(cents)
+    else:
+        assert get_coin_combination(cents) == expected
