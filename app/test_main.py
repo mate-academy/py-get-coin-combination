6b4d50bb10b8b8dd453d830c01d53f5009750805
Pavlo <kanivets.pavel@gmail.com>
2026-06-03 16:43:24 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -15,7 +15,9 @@ from app.main import get_coin_combination
         (50, [0, 0, 0, 2]),
         (100, [0, 0, 0, 4]),
         (99, [4, 0, 2, 3]),
+        (5, [0, 1, 0, 0]),
+        (10, [0, 0, 1, 0]),
     ]
 )
-def test_get_coin_combination(cents, expected):
+def test_get_coin_combination(cents: int, expected: list) -> None:
     assert get_coin_combination(cents) == expected
