5f2e3b82b2dd2dcfe566c231c1fe791fd8f8fb65
maxmlv <max.mlv.k@gmail.com>
2026-06-16 16:07:05 +0300

    implement tests for get_coin_combination and parameter value validation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
+def validate_cents(cents: int) -> None:
+    if not isinstance(cents, int):
+        raise TypeError("Parameter must be an integer.")
+    if cents < 0:
+        raise ValueError("Parameter must be a positive number.")
+
+
 def get_coin_combination(cents: int) -> list:
     values = [1, 5, 10, 25]
     coins = [0, 0, 0, 0]
+
+    validate_cents(cents)
 
     for i in range(3, -1, -1):
         coins[i] = cents // values[i]
--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,51 @@
+from typing import Any
+
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents,expected",
+    [
+        pytest.param(1, [1, 0, 0, 0]),
+        pytest.param(6, [1, 1, 0, 0]),
+        pytest.param(17, [2, 1, 1, 0]),
+        pytest.param(50, [0, 0, 0, 2]),
+    ]
+)
+def test_get_coin_combination(
+        cents: int,
+        expected: list[int],
+) -> None:
+    assert get_coin_combination(cents) == expected
+
+
+@pytest.mark.parametrize(
+    "cents,expected",
+    [
+        pytest.param("1", TypeError),
+        pytest.param(2.5, TypeError),
+    ]
+)
+def test_get_coin_combination_with_type_error(
+        cents: Any,
+        expected: type[TypeError],
+) -> None:
+    with pytest.raises(expected):
+        get_coin_combination(cents)
+
+
+@pytest.mark.parametrize(
+    "cents,expected",
+    [
+        pytest.param(-2, ValueError),
+        pytest.param(-5, ValueError),
+    ]
+)
+def test_get_coin_combination_with_negative_value_error(
+        cents: int,
+        expected: type[ValueError],
+) -> None:
+    with pytest.raises(expected):
+        get_coin_combination(cents)
