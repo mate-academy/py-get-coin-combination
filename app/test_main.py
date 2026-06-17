cfa8eb49c45805b1f59ff1faf4d08af346828e09
Dr-Kate <katia.01.01.2007d@gmail.com>
2026-06-17 14:26:23 +0300

    implemented-coin-combination-tests-task

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,21 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, coin_combination", [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (5, [0, 1, 0, 0]),
+        (10, [0, 0, 1, 0]),
+        (25, [0, 0, 0 , 1]),
+        (30, [0, 1, 0, 1]),
+        (48, [3, 0, 2, 1]),
+        (70, [0, 0, 2, 2]),
+    ]
+)
+def test_some_combinations(
+    cents: int,
+    coin_combination: list
+) -> None:
+    assert get_coin_combination(cents) == coin_combination
