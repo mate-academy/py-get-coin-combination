ff6802838183b42b32b753ea33ee2f204ae71551
Medaka <11Draghost11@gmail.com>
2026-06-17 11:28:33 +0300

    some fixes

--- app/test_main.py
+++ app/test_main.py
@@ -7,10 +7,10 @@ from app.main import get_coin_combination
 @pytest.mark.parametrize(
     "cents, expected",
     [
-        (1, [1, 0, 0, 0]), #1 penny
-        (6, [1, 1, 0, 0]), #1 penny + 1 nickel
-        (17, [2, 1, 1, 0]), #2 penny + 1 nickel + 1 dime
-        (50, [0, 0, 0, 2]) #2 quarters
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2])
     ]
 )
 def test_get_coin_combination(
