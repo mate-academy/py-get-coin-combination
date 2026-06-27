619d5b94c874e4b6368eb3452b95cb292e5596b1
Danylo Pylypenko <p.prince.e777@gmail.com>
2026-06-27 21:29:27 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,33 @@
 from app.main import get_coin_combination
+import pytest
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, expected",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (5, [0, 1, 0, 0]),
+        (10, [0, 0, 1, 0]),
+        (25, [0, 0, 0, 1]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
+        (100, [0, 0, 0, 4])
+    ])
+def test_normal_combination(cents: int, expected: list[int]) -> None:
+    assert get_coin_combination(cents) == expected
+
+
+@pytest.mark.parametrize(
+    "cents",
+    [
+        "0",
+        "one",
+        5.5,
+        None,
+        ""
+    ]
+)
+def test_no_normal_combination(cents: int) -> None:
+    with pytest.raises(TypeError):
+        get_coin_combination(cents)
