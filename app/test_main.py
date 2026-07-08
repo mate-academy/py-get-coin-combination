4e41d26c24e125392004d5a019b7c2199bae3dd7
glor1ee <sashok1764@icloud.com>
2026-07-08 21:23:15 +0300

    implement test to get_coin_combination function

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,17 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_get_coin_combination_1():
+    assert get_coin_combination(1) == [1, 0, 0, 0]  # 1 penny
+
+
+def test_get_coin_combination_6():
+    assert get_coin_combination(6) == [1, 1, 0, 0]  # 1 penny + 1 nickel
+
+
+def test_get_coin_combination_17():
+    assert get_coin_combination(17) == [2, 1, 1, 0]   # 2 pennies + 1 nickel + 1 dime
+
+
+def test_get_coin_combination_50():
+    assert get_coin_combination(50) == [0, 0, 0, 2]
