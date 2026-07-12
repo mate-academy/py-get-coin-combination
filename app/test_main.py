591d9ba4fe11948eac85605404de9b6de987a778
Vitalii Kononets <vitalii.kononets.85@gmail.com>
2026-07-13 00:18:57 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,70 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, coins",
+    [
+        pytest.param(
+            0,
+            [0, 0, 0, 0],
+            id="zero cents"
+        ),
+        pytest.param(
+            1,
+            [1, 0, 0, 0],
+            id="just one penny"
+        ),
+        pytest.param(
+            5,
+            [0, 1, 0, 0],
+            id="just one nickel"
+        ),
+        pytest.param(
+            10,
+            [0, 0, 1, 0],
+            id="just one dime"
+        ),
+        pytest.param(
+            25,
+            [0, 0, 0, 1],
+            id="just one quarter"
+        ),
+        pytest.param(
+            4,
+            [4, 0, 0, 0],
+            id="max before nickel"
+        ),
+        pytest.param(
+            9,
+            [4, 1, 0, 0],
+            id="max before dime"
+        ),
+        pytest.param(
+            24,
+            [4, 0, 2, 0],
+            id="max before quarter"
+        ),
+        pytest.param(
+            104,
+            [4, 0, 0, 4],
+            id="random mix"
+        ),
+        pytest.param(
+            41,
+            [1, 1, 1, 1],
+            id="every coin"
+        ),
+        pytest.param(
+            33343,
+            [3, 1, 1, 1333],
+            id="large number"
+        ),
+    ]
+)
+def test_combine_coins_correctly(
+        cents: int,
+        coins: list[int]
+) -> None:
+    assert get_coin_combination(cents) == coins
