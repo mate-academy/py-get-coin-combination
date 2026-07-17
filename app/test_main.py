bf0f3d82f7550bf87de9e5fb2c7d01d518c83af2
Andrii Zelenskyi <Zelenskiy2013@gmail.com>
2026-07-17 09:32:13 +0300

    fix according linter checks

--- app/test_main.py
+++ app/test_main.py
@@ -3,12 +3,11 @@ import pytest
 from app.main import get_coin_combination
 
 
-@pytest.mark.parametrize("a,result",[
+@pytest.mark.parametrize("data, result", [
     (1, [1, 0, 0, 0]),
     (6, [1, 1, 0, 0]),
     (17, [2, 1, 1, 0]),
     (50, [0, 0, 0, 2])]
-                         )
-def test_list(a: int, result: list[int]) -> None:
-   assert (get_coin_combination(a) == result)
-
+)
+def test_list(data: int, result: list[int]) -> None:
+    assert (get_coin_combination(data) == result)
