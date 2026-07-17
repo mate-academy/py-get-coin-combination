4d91477ab7cf9b5c64f664ae37d3230c778ca9e2
Rafael Carvalho <dannyelrc@hotmail.com>
2026-07-17 16:58:28 -0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,20 @@
 from app.main import get_coin_combination
+import pytest
 
 
-# write your tests here
+@pytest.mark.parametrize("coin_combination, expected", [
+    (get_coin_combination(0), [0, 0, 0, 0]),
+    (get_coin_combination(1), [1, 0, 0, 0]),
+    (get_coin_combination(5), [0, 1, 0, 0]),
+    (get_coin_combination(10), [0, 0, 1, 0]),
+    (get_coin_combination(25), [0, 0, 0, 1]),
+    (get_coin_combination(30), [0, 1, 0, 1]),
+    (get_coin_combination(99), [4, 0, 2, 3]),
+])
+def test_get_coin_combination(coin_combination: list, expected: list) -> None:
+    assert coin_combination == expected
+
+
+def test_get_coin_combination_negative() -> None:
+    with pytest.raises(ValueError):
+        get_coin_combination(-1)
