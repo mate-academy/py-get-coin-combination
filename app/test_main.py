58eee64c8ea591e7fd1479bc6e469c3168ed7e31
ira <iryna.yaroshevska@gmail.com>
2026-07-21 10:46:21 +0800

    Solution Tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,35 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "coins,result",
+    [
+        (1, [1, 0, 0, 0]),
+        (50, [0, 0, 0, 2])
+    ],
+    ids=[
+        "1 penny",
+        "2 quarters"
+    ]
+)
+def test_check_one_type(coins: int, result: list[int]) -> None:
+    assert get_coin_combination(coins) == result
+
+
+@pytest.mark.parametrize(
+    "coins,result",
+    [
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0])
+    ],
+    ids=[
+        "1 penny + 1 nickel",
+        "2 pennies + 1 nickel + 1 dime"
+    ]
+)
+def test_check_multiple_types_at_same_time(
+        coins: int, result: list[int]
+) -> None:
+    assert get_coin_combination(coins) == result
